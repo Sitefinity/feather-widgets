d78f42f7515766d603e5572c80bb03cca5394718
ElenaGaneva <Elena.Ganeva@telerik.com>
2014-05-29 18:30:09 +0300

    Fixed activate/deactivate of Sitefinity module

--- ContentBlock/Initializer.cs
+++ ContentBlock/Initializer.cs
@@ -4,14 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Web.Routing;
 using System.Web.Mvc;
+using Telerik.Sitefinity.Abstractions;
 
 namespace ContentBlock
 {
+    /// <summary>
+    /// This class presents initialzier for the ContentBlock.
+    /// </summary>
     public static class Initializer
     {
+        /// <summary>
+        /// Registers routes for the ContentBlock controller. 
+        /// </summary>
         public static void Initialize()
         {
-            RouteTable.Routes.MapRoute("MvcContentBlock", "ContentBlock/{controller}/{action}", new { controller = "ContentBlock", action = "Index" });
+             Bootstrapper.MVC.MapRoute("MvcContentBlock", "ContentBlock/{controller}/{action}",
+                    new { controller = "ContentBlock", action = "Index" });
         }
     }
 }
