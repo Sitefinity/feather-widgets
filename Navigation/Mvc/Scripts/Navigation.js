936a7845e657fcbde627f613d57e9ff3bde93403
Dimitar Panov <dimitar.panov@telerik.com>
2014-06-17 18:28:13 +0300

    Removed custom menu toggling functionality; Updated default Bootstrap templates for Navigation;

--- Navigation/Mvc/Scripts/Navigation.js
+++ Navigation/Mvc/Scripts/Navigation.js
@@ -3,10 +3,5 @@
         var val = jQuery(this).find('option:selected').val();
         window.location.replace(val);
     });
-
-    jQuery('.nav-toggle').bind('click', function () {
-        jQuery(this).siblings('.nav').toggle();
-        jQuery(this).toggleClass('active');
-    });
 });
 
