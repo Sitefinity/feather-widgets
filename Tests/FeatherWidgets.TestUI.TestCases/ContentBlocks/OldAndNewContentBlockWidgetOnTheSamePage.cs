50a41595098d5d8f4d95492a60a46accb2572383
VenetaBogoeva <veneta.bogoeva@telerik.com>
2014-08-18 16:48:44 +0300

    Added new UI test OldAndNewContentBlockWidgetOnTheSamePage

---
+++ Tests/FeatherWidgets.TestUI.TestCases/ContentBlocks/OldAndNewContentBlockWidgetOnTheSamePage.cs
@@ -0,0 +1,68 @@
+﻿using Feather.Widgets.TestUI.Framework;
+using FeatherWidgets.TestUI.TestCases;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeatherWidgets.TestUI
+{
+    /// <summary>
+    /// This is a sample test class.
+    /// </summary>
+    [TestClass]
+    public class OldAndNewContentBlockWidgetOnTheSamePage_ : FeatherTestCase
+    {
+        // <summary>
+        /// Pefroms Server Setup and prepare the system with needed data.
+        /// </summary>
+        protected override void ServerSetup()
+        {
+            BAT.Macros().User().EnsureAdminLoggedIn();
+            BAT.Arrange(this.TestName).ExecuteSetUp();
+        }
+
+        /// <summary>
+        /// Performs clean up and clears all data created by the test.
+        /// </summary>
+        protected override void ServerCleanup()
+        {
+            BAT.Arrange(this.TestName).ExecuteTearDown();
+        }
+
+        [TestMethod,
+       Microsoft.VisualStudio.TestTools.UnitTesting.Owner("Feather team"),
+       TestCategory(FeatherTestCategories.PagesAndContent)]
+        public void OldAndNewContentBlockWidgetOnTheSamePage()
+        {
+            BAT.Macros().NavigateTo().Pages();
+            BAT.Wrappers().Backend().Pages().PagesWrapper().OpenPageZoneEditor(PageName);
+            BATFeather.Wrappers().Backend().Pages().PageZoneEditorWrapper().EditWidget(NewContentBlockWidget);
+            BATFeather.Wrappers().Backend().ContentBlocks().ContentBlocksWrapper().FillContentToContentBlockWidget(NewContentBlockContent);
+            BATFeather.Wrappers().Backend().ContentBlocks().ContentBlocksWrapper().SaveChanges();
+            BAT.Wrappers().Backend().Pages().PageZoneEditorWrapper().CheckWidgetContent(NewContentBlockWidget, NewContentBlockContent);
+            BAT.Wrappers().Backend().Pages().PageZoneEditorWrapper().EditWidget(OldContentBlockWidget);
+            BAT.Wrappers().Backend().Pages().WidgetDesigners().ContentBlockDesigner().FillContentBlockContent(OldContentBlockContent);
+            BAT.Wrappers().Backend().Pages().WidgetDesigners().ContentBlockDesigner().SaveEditedWidgetChanges();
+            BAT.Wrappers().Frontend().ContentBlock().ContentBlockWrapper().VerifyContentOfContentBlockOnThePageFrontend(OldContentBlockContent);
+            BAT.Wrappers().Backend().Pages().PageZoneEditorWrapper().PublishPage();
+            this.NavigatePageOnTheFrontend(PageName);
+            BATFeather.Wrappers().Frontend().ContentBlock().ContentBlockWrapper().VerifyContentOfContentBlockOnThePageFrontend(NewContentBlockContent);
+            BAT.Wrappers().Frontend().ContentBlock().ContentBlockWrapper().VerifyContentOfContentBlockOnThePageFrontend(OldContentBlockContent);
+        }
+
+        public void NavigatePageOnTheFrontend(string pageName)
+        {
+            BAT.Macros().NavigateTo().CustomPage("~/" + pageName.ToLower());
+            ActiveBrowser.WaitUntilReady();
+        }
+
+        private const string PageName = "ContentBlock";
+        private const string NewContentBlockContent = "New content block widget";
+        private const string NewContentBlockWidget = "ContentBlock";
+        private const string OldContentBlockContent = "Old content block widget";
+        private const string OldContentBlockWidget = "Content block";
+    }
+}
