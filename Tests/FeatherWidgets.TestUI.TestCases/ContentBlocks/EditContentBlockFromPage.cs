d41aba3a377dc193c7b0c486fb24d6af6b312ea6
VenetaBogoeva <veneta.bogoeva@telerik.com>
2014-08-18 14:23:51 +0300

    Added new UI test EditContentBlockFromPage

---
+++ Tests/FeatherWidgets.TestUI.TestCases/ContentBlocks/EditContentBlockFromPage.cs
@@ -0,0 +1,63 @@
+﻿using Feather.Widgets.TestUI.Framework;
+using FeatherWidgets.TestUI.TestCases;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeatherWidgets.TestUI
+{
+    /// <summary>
+    /// This is a sample test class.
+    /// </summary>
+    [TestClass]
+    public class EditContentBlockFromPage_ : FeatherTestCase
+    {
+        // <summary>
+        /// Pefroms Server Setup and prepare the system with needed data.
+        /// </summary>
+        protected override void ServerSetup()
+        {
+            BAT.Macros().User().EnsureAdminLoggedIn();
+            BAT.Arrange(this.TestName).ExecuteSetUp();
+        }
+
+        /// <summary>
+        /// Performs clean up and clears all data created by the test.
+        /// </summary>
+        protected override void ServerCleanup()
+        {
+            BAT.Arrange(this.TestName).ExecuteTearDown();
+        }
+
+        [TestMethod,
+       Microsoft.VisualStudio.TestTools.UnitTesting.Owner("Feather team"),
+       TestCategory(FeatherTestCategories.PagesAndContent)]
+        public void EditContentBlockFromPage()
+        {
+            BAT.Macros().NavigateTo().Pages();
+            BAT.Wrappers().Backend().Pages().PagesWrapper().OpenPageZoneEditor(PageName);
+            BATFeather.Wrappers().Backend().Pages().PageZoneEditorWrapper().EditWidget(WidgetName);
+            BATFeather.Wrappers().Backend().ContentBlocks().ContentBlocksWrapper().FillContentToContentBlockWidget(EditContent);
+            BATFeather.Wrappers().Backend().ContentBlocks().ContentBlocksWrapper().SaveChanges();
+            BAT.Wrappers().Backend().Pages().PageZoneEditorWrapper().PublishPage();
+            this.NavigatePageOnTheFrontend(PageName);
+            BATFeather.Wrappers().Frontend().ContentBlock().ContentBlockWrapper().VerifyContentOfContentBlockOnThePageFrontend(ExpectedContent);
+        }
+
+        public void NavigatePageOnTheFrontend(string pageName)
+        {
+            BAT.Macros().NavigateTo().CustomPage("~/" + pageName.ToLower());
+            ActiveBrowser.WaitUntilReady();
+        }
+
+        private const string PageName = "ContentBlock";
+        private const string WidgetName = "ContentBlock";
+        private const string OperationName = "Duplicate";
+        private const string EditContent = " edited";
+        private const string ExpectedContent = "Test content edited";
+        private const int ExpectedCountOfContentDivEmpty = 2;
+    }
+}
