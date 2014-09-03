e961c0a6211cab793d2c5da8f43985b7024a2986
VenetaBogoeva <veneta.bogoeva@telerik.com>
2014-09-03 17:30:24 +0300

    Added new UI test DuplicateNewsWidgetFromPage

---
+++ Tests/FeatherWidgets.TestUI.TestCases/News/DuplicateNewsWidgetFromPage.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Feather.Widgets.TestUI.Framework;
+using FeatherWidgets.TestUI.TestCases;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FeatherWidgets.TestUI
+{
+    /// <summary>
+    /// DuplicateNewsWidgetFromPage test class.
+    /// </summary>
+    [TestClass]
+    public class DuplicateNewsWidgetFromPage_ : FeatherTestCase
+    {
+        /// <summary>
+        /// UI test DuplicateNewsWidgetFromPage
+        /// </summary>
+        [TestMethod,
+       Microsoft.VisualStudio.TestTools.UnitTesting.Owner("Feather team"),
+       TestCategory(FeatherTestCategories.PagesAndContent)]
+        public void DuplicateNewsWidgetFromPage()
+        {
+            BAT.Macros().NavigateTo().Pages();
+            BAT.Wrappers().Backend().Pages().PagesWrapper().OpenPageZoneEditor(PageName);
+            BAT.Wrappers().Backend().Pages().PageZoneEditorWrapper().SelectExtraOptionForWidget(OperationName);
+            BAT.Wrappers().Backend().Pages().PageZoneEditorWrapper().PublishPage();
+            this.VerifyNewsOnTheFrontend();
+        }
+
+        /// <summary>
+        /// Verify news widget on the frontend
+        /// </summary>
+        public void VerifyNewsOnTheFrontend()
+        {
+            BAT.Macros().NavigateTo().CustomPage("~/" + PageName.ToLower());
+            ActiveBrowser.WaitUntilReady();
+            BATFeather.Wrappers().Frontend().News().NewsWrapper().VerifyContentOfContentBlockOnThePageFrontend(this.newsTitles);
+        }
+
+        /// <summary>
+        /// Performs Server Setup and prepare the system with needed data.
+        /// </summary>
+        protected override void ServerSetup()
+        {
+            BAT.Macros().User().EnsureAdminLoggedIn();
+            BAT.Arrange(this.TestName).ExecuteSetUp();
+        }
+
+        /// <summary>
+        /// Performs clean up and clears all data created by the test.
+        /// </summary>
+        protected override void ServerCleanup()
+        {
+            BAT.Arrange(this.TestName).ExecuteTearDown();
+        }
+
+        private const string PageName = "News";
+        private const string NewsTitle = "NewsTitle";
+        private const string OperationName = "Duplicate";
+        private string[] newsTitles = new string[] { NewsTitle, NewsTitle };
+    }
+}
