373140fd8895611ceb86af4066711ce439773525
VenetaBogoeva <veneta.bogoeva@telerik.com>
2014-09-10 17:49:55 +0300

    Added integration test ContentBlockWidget_EditSharedContent

--- Tests/FeatherWidgets.TestIntegration/ContentBlock/ContentBlockWidget.cs
+++ Tests/FeatherWidgets.TestIntegration/ContentBlock/ContentBlockWidget.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using ContentBlock.Mvc.Controllers;
 using FeatherWidgets.TestUtilities.CommonOperations;
 using MbUnit.Framework;
@@ -14,7 +15,7 @@ namespace FeatherWidgets.TestIntegration.ContentBlock
     /// </summary>
     [TestFixture]
     [Description("This is a class with Content block tests for Advanced settings.")]
-    public class ContentBlockWidgetAdvancedSettings
+    public class ContentBlockWidget
     {
         /// <summary>
         /// Set up method
@@ -67,11 +68,48 @@ namespace FeatherWidgets.TestIntegration.ContentBlock
             Assert.IsTrue(responseContent.Contains(ContentBlockContent), "The content block with this title was not found!");
         }
 
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Maintainability", "CA1506:AvoidExcessiveClassCoupling"), Test]
+        [Category(TestCategories.ContentBlock)]
+        [Author(TestAuthor.Team2)]
+        [Description("Verifies that when edit shared content block the changes are applied in content block widget on the frontend.")]
+        public void ContentBlockWidget_EditSharedContent()
+        {
+            string testName = "ContentBlockWidgetEditSharedContent";
+            string pageNamePrefix = testName + "ContentBlockPage";
+            string pageTitlePrefix = testName + "Content Block";
+            string urlNamePrefix = testName + "content-block";
+            int pageIndex = 1;
+            string url = UrlPath.ResolveAbsoluteUrl("~/" + urlNamePrefix + pageIndex);
+
+            var content = App.WorkWith().ContentItems()
+                           .Where(c => c.Title == ContentBlockTitle && c.Status == Telerik.Sitefinity.GenericContent.Model.ContentLifecycleStatus.Master)
+                           .Get().Single();
+
+            var mvcProxy = new MvcControllerProxy();
+            mvcProxy.ControllerName = typeof(ContentBlockController).FullName;
+            var contentBlockController = new ContentBlockController();
+            contentBlockController.SharedContentID = content.Id;
+            mvcProxy.Settings = new ControllerSettings(contentBlockController);
+
+            this.pageOperations.CreatePageWithControl(mvcProxy, pageNamePrefix, pageTitlePrefix, urlNamePrefix, pageIndex);
+
+            App.WorkWith().ContentItem(content.Id).CheckOut().Do(cI =>
+            {
+                cI.Content = ContentBlockContentEdited;
+                cI.LastModified = DateTime.UtcNow;
+            })
+                .CheckIn().Publish().SaveChanges();
+
+            string responseContent = PageInvoker.ExecuteWebRequest(url);
+            Assert.IsTrue(responseContent.Contains(ContentBlockContentEdited), "The content block with this title was not found!");
+        }
+
         #region Fields and constants
 
         private PagesOperations pageOperations;
         private const string ContentBlockTitle = "This is content block title";
         private const string ContentBlockContent = "This is content block content";
+        private const string ContentBlockContentEdited = "This is content block content edited";
 
         #endregion
     }
