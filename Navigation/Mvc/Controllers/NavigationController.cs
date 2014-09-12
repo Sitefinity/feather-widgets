26e2e1a34940d135abe32151fbb64354153220b1
Boyko-Karadzhov <boyko.karadzhov@telerik.com>
2014-09-12 11:40:00 +0300

    fix disappearing widgets when viewing news details

--- Navigation/Mvc/Controllers/NavigationController.cs
+++ Navigation/Mvc/Controllers/NavigationController.cs
@@ -134,6 +134,19 @@ namespace Navigation.Mvc.Controllers
 
         #endregion
 
+        #region Overridden methods
+
+        /// <summary>
+        /// Called when a request matches this controller, but no method with the specified action name is found in the controller.
+        /// </summary>
+        /// <param name="actionName">The name of the attempted action.</param>
+        protected override void HandleUnknownAction(string actionName)
+        {
+            this.Index().ExecuteResult(this.ControllerContext);
+        }
+
+        #endregion
+
         #region Private methods
 
         /// <summary>
--- SocialShare/Mvc/Controllers/SocialShareController.cs
+++ SocialShare/Mvc/Controllers/SocialShareController.cs
@@ -23,5 +23,18 @@ namespace SocialShare.Mvc.Controllers
         }
 
         #endregion
+
+        #region Overridden methods
+
+        /// <summary>
+        /// Called when a request matches this controller, but no method with the specified action name is found in the controller.
+        /// </summary>
+        /// <param name="actionName">The name of the attempted action.</param>
+        protected override void HandleUnknownAction(string actionName)
+        {
+            this.Index().ExecuteResult(this.ControllerContext);
+        }
+
+        #endregion
     }
 }
