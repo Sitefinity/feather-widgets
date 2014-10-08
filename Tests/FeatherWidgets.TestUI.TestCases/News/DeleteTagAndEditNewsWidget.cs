bca900b4d438c5b2bec1db118b39fc50019bf149
Slaveya Shtereva <slaveya.shtereva@telerik.com>
2014-10-08 14:54:08 +0300

    Add new tests

---
+++ Tests/FeatherWidgets.TestUI.TestCases/News/DeleteTagAndEditNewsWidget.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Feather.Widgets.TestUI.Framework;
+using FeatherWidgets.TestUI.TestCases;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FeatherWidgets.TestUI
+{
+    /// <summary>
+    /// SelectNewsItemInNewsWidgetFromPage_ test class.
+    /// </summary>
+    [TestClass]
+    public class DeleteTagAndEditNewsWidget_ : FeatherTestCase
+    {
+        /// <summary>
+        /// UI test FilterNewsItemWithCustomTaxonomyOnPage
+        /// </summary>
+        [TestMethod,
+       Microsoft.VisualStudio.TestTools.UnitTesting.Owner("Feather team"),
+       TestCategory(FeatherTestCategories.PagesAndContent)]
+        public void DeleteTagAndEditNewsWidget()
+        {
+            BAT.Macros().NavigateTo().Pages();
+            BAT.Wrappers().Backend().Pages().PagesWrapper().OpenPageZoneEditor(PageName);
+            BATFeather.Wrappers().Backend().Pages().PageZoneEditorWrapper().EditWidget(WidgetName);
+            BATFeather.Wrappers().Backend().News().NewsWidgetEditContentScreenWrapper().SelectWhichNewsToDisplay(WhichNewsToDisplay);
+            BATFeather.Wrappers().Backend().News().NewsWidgetEditContentScreenWrapper().SelectTaxonomy(TaxonomyName);
+            BATFeather.Wrappers().Backend().News().NewsWidgetEditContentScreenWrapper().SelectItem(TaxonTitle1);
+            BATFeather.Wrappers().Backend().News().NewsWidgetEditContentScreenWrapper().SaveChanges();
+            BAT.Wrappers().Backend().Pages().PageZoneEditorWrapper().CheckWidgetContent(WidgetName, NewsTitle1);
+            BAT.Wrappers().Backend().Pages().PageZoneEditorWrapper().PublishPage();
+            this.VerifyNewsOnTheFrontend();
+
+            this.DeleteTag();
+
+            BAT.Macros().NavigateTo().Pages();
+            BAT.Wrappers().Backend().Pages().PagesWrapper().OpenPageZoneEditor(PageName);
+            BATFeather.Wrappers().Backend().Pages().PageZoneEditorWrapper().EditWidget(WidgetName);
+            BATFeather.Wrappers().Backend().News().NewsWidgetEditContentScreenWrapper().SelectWhichNewsToDisplay(WhichNewsToDisplay);
+            BATFeather.Wrappers().Backend().News().NewsWidgetEditContentScreenWrapper().SelectItem(TaxonTitle2);
+            BATFeather.Wrappers().Backend().News().NewsWidgetEditContentScreenWrapper().SaveChanges();
+            BAT.Wrappers().Backend().Pages().PageZoneEditorWrapper().CheckWidgetContent(WidgetName, NewsTitle2);
+        }
+
+        /// <summary>
+        /// Verify news widget on the frontend
+        /// </summary>
+        public void VerifyNewsOnTheFrontend()
+        {
+            BAT.Macros().NavigateTo().CustomPage("~/" + PageName.ToLower());
+            ActiveBrowser.WaitUntilReady();
+            BATFeather.Wrappers().Frontend().News().NewsWrapper().VerifyContentOfContentBlockOnThePageFrontend(this.newsTitles);
+        }
+
+        /// <summary>
+        /// Performs Server Setup and prepare the system with needed data.
+        /// </summary>
+        protected override void ServerSetup()
+        {
+            BAT.Macros().User().EnsureAdminLoggedIn();
+            BAT.Arrange(this.TestName).ExecuteSetUp();
+        }
+
+        protected void DeleteTag()
+        {
+            BAT.Arrange(this.TestName).ExecuteArrangement("DeleteTag");
+        }
+
+        /// <summary>
+        /// Performs clean up and clears all data created by the test.
+        /// </summary>
+        protected override void ServerCleanup()
+        {
+           BAT.Arrange(this.TestName).ExecuteTearDown();
+        }
+
+        private const string PageName = "News";
+        private const string TaxonTitle1 = "Tag1";
+        private const string TaxonTitle2 = "Tag2";
+        private const string NewsTitle1 = "NewsTitle1";
+        private const string NewsTitle2 = "NewsTitle2";
+        private const string WidgetName = "News";
+        private const string WhichNewsToDisplay = "Narrow selection by...";
+        private const string TaxonomyName = "Tags";
+        private string[] newsTitles = new string[] { NewsTitle1 };
+    }
+}
