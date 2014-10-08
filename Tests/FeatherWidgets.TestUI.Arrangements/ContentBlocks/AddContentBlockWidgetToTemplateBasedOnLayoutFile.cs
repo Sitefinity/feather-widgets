731eae077281d8c13e0df927fc7d0d88f4a3debc
PetyaRachina <petya.rachina@telerik.com>
2014-10-08 18:34:49 +0300

    added UI test AddContentBlockWidgetToTemplateBasedOnLayoutFile

---
+++ Tests/FeatherWidgets.TestUI.Arrangements/ContentBlocks/AddContentBlockWidgetToTemplateBasedOnLayoutFile.cs
@@ -0,0 +1,65 @@
+﻿using System.IO;
+using System.Threading;
+using Telerik.Sitefinity.Frontend.TestUtilities.CommonOperations;
+using Telerik.Sitefinity.TestUI.Arrangements.Framework;
+using Telerik.Sitefinity.TestUI.Arrangements.Framework.Attributes;
+using Telerik.Sitefinity.TestUtilities.CommonOperations;
+
+namespace FeatherWidgets.TestUI.Arrangements
+{
+    /// <summary>
+    /// AddContentBlockWidgetToTemplateBasedOnLayoutFile arrangement class.
+    /// </summary>
+    public class AddContentBlockWidgetToTemplateBasedOnLayoutFile : ITestArrangement
+    {
+        /// <summary>
+        /// Server side set up.
+        /// </summary>
+        [ServerSetUp]
+        public void SetUp()
+        {
+            string folderPath = Path.Combine(this.SfPath, "MVC", "Views", "Layouts");
+
+            if (!Directory.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+            }
+
+            string filePath = Path.Combine(folderPath, LayoutFileName);
+            FeatherServerOperations.ResourcePackages().AddNewResource(LayoutFileResource, filePath);
+        }
+
+        [ServerArrangement]
+        public void CreatePage()
+        {
+            var templateId = ServerOperations.Templates().GetTemplateIdByTitle(TemplateTitle);
+            ServerOperations.Pages().CreatePage(PageName, templateId);
+        }
+
+        /// <summary>
+        /// Tears down.
+        /// </summary>
+        [ServerTearDown]
+        public void TearDown()
+        {
+            string filePath = Path.Combine(this.SfPath, "MVC", "Views", "Layouts", LayoutFileName);
+
+            ServerOperations.Pages().DeleteAllPages();
+            ServerOperations.Templates().DeletePageTemplate(TemplateTitle);
+            File.Delete(filePath);
+        }
+
+        private const string TemplateTitle = "TestLayout";
+        private const string PageName = "FeatherPage";
+        private const string LayoutFileResource = "Telerik.Sitefinity.Frontend.TestUtilities.Data.TestLayout.cshtml";
+        private const string LayoutFileName = "TestLayout.cshtml";
+
+        private string SfPath
+        {
+            get
+            {
+                return System.Web.Hosting.HostingEnvironment.MapPath("~/");
+            }
+        }
+    }
+}
---
+++ Tests/FeatherWidgets.TestUI.TestCases/ContentBlocks/AddContentBlockToTemplateBasedOnLayoutFile.cs
@@ -0,0 +1,69 @@
+﻿using Feather.Widgets.TestUI.Framework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telerik.Sitefinity.TestUI.Framework.Framework.Wrappers.Backend.PageTemplates;
+
+namespace FeatherWidgets.TestUI.TestCases.ContentBlocks
+{
+    /// <summary>
+    /// AddContentBlockToTemplateBasedOnLayoutFile test class
+    /// </summary>
+    [TestClass]
+    public class AddContentBlockToTemplateBasedOnLayoutFile : FeatherTestCase
+    {
+        /// <summary>
+        /// UI test AddContentBlockWidgetToTemplateBasedOnLayoutFile
+        /// </summary>
+        [TestMethod,
+        Owner("Feather team"),
+        TestCategory(FeatherTestCategories.PagesAndContent)]
+        public void AddContentBlockWidgetToTemplateBasedOnLayoutFile()
+        {
+            BAT.Macros().NavigateTo().Design().PageTemplates();
+            BAT.Wrappers().Backend().PageTemplates().PageTemplateMainScreen().OpenTemplateEditor(TemplateTitle);
+            BATFeather.Wrappers().Backend().Pages().PageZoneEditorWrapper().AddWidgetToSelectedPlaceHolder(WidgetName, PlaceHolder);
+            BATFeather.Wrappers().Backend().Pages().PageZoneEditorWrapper().EditWidget(WidgetName);
+            BATFeather.Wrappers().Backend().ContentBlocks().ContentBlocksWrapper().FillContentToContentBlockWidget(ContentBlockContent);
+            BATFeather.Wrappers().Backend().ContentBlocks().ContentBlocksWrapper().SaveChanges();
+            BAT.Wrappers().Backend().PageTemplates().PageTemplateModifyScreen().PublishTemplate();
+
+            BAT.Arrange(this.TestName).ExecuteArrangement("CreatePage");
+
+            BAT.Macros().NavigateTo().CustomPage("~/" + PageName.ToLower(), false);
+            ActiveBrowser.WaitUntilReady();
+
+            Assert.IsFalse(ActiveBrowser.ContainsText(ServerErrorMessage), "Server error was found on the page");
+            Assert.IsTrue(ActiveBrowser.ContainsText(ContentBlockContent),"Content block content was not found on the page");
+            Assert.IsTrue(ActiveBrowser.ContainsText(LayoutText), "Layout template text was not found");
+        }
+
+        /// <summary>
+        /// Performs Server Setup and prepare the system with needed data.
+        /// </summary>
+        protected override void ServerSetup()
+        {
+            BAT.Macros().User().EnsureAdminLoggedIn();
+            BAT.Arrange(this.TestName).ExecuteSetUp();
+        }
+
+        /// <summary>
+        /// Performs clean up and clears all data created by the test.
+        /// </summary>
+        protected override void ServerCleanup()
+        {
+            BAT.Arrange(this.TestName).ExecuteTearDown();
+        }
+
+        private const string PageName = "FeatherPage";
+        private const string TemplateTitle = "TestLayout";
+        private const string ContentBlockContent = "Test content";
+        private const string WidgetName = "ContentBlock";
+        private const string PlaceHolder = "TestPlaceHolder";
+        private const string LayoutText = "Test Layout";
+        private const string ServerErrorMessage = "Server Error"; 
+    }
+}
