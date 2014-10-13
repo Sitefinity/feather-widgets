63732f7e818a1783727e1f29f98cce438ae711f5
PetyaRachina <petya.rachina@telerik.com>
2014-10-13 19:31:05 +0300

    fixed integration tests for search

--- Tests/FeatherWidgets.TestIntegration/ContentBlock/ContentBlockSearchIndex.cs
+++ Tests/FeatherWidgets.TestIntegration/ContentBlock/ContentBlockSearchIndex.cs
@@ -1,6 +1,10 @@
-﻿using FeatherWidgets.TestUtilities.CommonOperations;
+﻿using System;
+using FeatherWidgets.TestUtilities.CommonOperations;
 using MbUnit.Framework;
 using Telerik.Sitefinity.Frontend.TestUtilities.CommonOperations;
+using Telerik.Sitefinity.Modules.Pages;
+using Telerik.Sitefinity.Publishing.Configuration;
+using Telerik.Sitefinity.Publishing.Web.Services;
 using Telerik.Sitefinity.Services.Search;
 using Telerik.Sitefinity.Services.Search.Data;
 using Telerik.Sitefinity.TestUtilities.CommonOperations;
@@ -20,17 +24,20 @@ namespace FeatherWidgets.TestIntegration.ContentBlock
         [Description("Creates a pure MVC template and page, based on the template, with content block widget, verifies the search result is correct")]
         public void ContentBlock_PureMvcPageWithContentBlockWidget_VerifySearch()
         {
+            string searchIndex = "TestSearchIndex_" + Guid.NewGuid().ToString();
+
             try
             {
+                var searchIndexId = ServerOperations.Search().CreateSearchIndex(searchIndex);
+
                 var templateId = FeatherServerOperations.Pages().CreatePureMvcTemplate(PageTemplate);
                 var pageId = ServerOperations.Pages().CreatePage(PageTitle, templateId);
                 pageId = ServerOperations.Pages().GetPageNodeId(pageId);
                 ServerOperationsFeather.Pages().AddContentBlockWidgetToPage(pageId, ContentBlockText);
 
-                ServerOperations.Search().CreateSearchIndex(SearchIndex);
+                this.Reindex(searchIndexId);
 
-                IResultSet result = this.Search(SearchIndex, ContentBlockText);
-
+                IResultSet result = this.Search(searchIndex, ContentBlockText);
                 Assert.IsTrue(result.HitCount == 1, "Search result is not correct");
             }
             finally
@@ -47,14 +54,18 @@ namespace FeatherWidgets.TestIntegration.ContentBlock
         [Description("Creates normal page (hybrid mode - web forms + MVC) with content block widget and verifies the search result is correct ")]
         public void ContentBlock_PageWithContentBlockWidget_VerifySearch()
         {
+            string searchIndex = "TestSearchIndex_" + Guid.NewGuid().ToString();
+
             try
             {
+                var searchIndexId = ServerOperations.Search().CreateSearchIndex(searchIndex);
+
                 var pageId = ServerOperations.Pages().CreatePage(PageTitle);
                 ServerOperationsFeather.Pages().AddContentBlockWidgetToPage(pageId, ContentBlockText);
 
-                ServerOperations.Search().CreateSearchIndex(SearchIndex);
+                this.Reindex(searchIndexId);
 
-                IResultSet result = this.Search(SearchIndex, ContentBlockText);
+                IResultSet result = this.Search(searchIndex, ContentBlockText);
 
                 Assert.IsTrue(result.HitCount == 1, "Search result is not correct");
             }
@@ -77,9 +88,14 @@ namespace FeatherWidgets.TestIntegration.ContentBlock
             return Telerik.Sitefinity.Services.ServiceBus.ResolveService<ISearchService>();
         }
 
+        private void Reindex(Guid searchIndexId)
+        {
+            PublishingAdminService service = new PublishingAdminService();
+            service.ReindexSearchContent(PublishingConfig.SearchProviderName, searchIndexId.ToString());
+        }
+
         private const string PageTemplate = "TestTemplate";
         private const string PageTitle = "PageWithContentBlock";
         private const string ContentBlockText = "TestSearching";
-        private const string SearchIndex = "TestSearchIndex";
     }
 }
