3094aa83859422069cee6ead20f7c7849d0edd6e
PetyaRachina <petya.rachina@telerik.com>
2014-10-20 16:48:34 +0300

    added new UI test NavigationWidgetBootstrapTemplateVerifyDefaultTransformation

---
+++ Tests/Feather.Widgets.TestUI.Framework/Framework/ElementMap/Navigation/NavigationWidgetFrontend.cs
@@ -0,0 +1,38 @@
+﻿using ArtOfTest.WebAii.Controls.HtmlControls;
+using ArtOfTest.WebAii.Core;
+using ArtOfTest.WebAii.ObjectModel;
+using ArtOfTest.WebAii.TestTemplates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Feather.Widgets.TestUI.Framework.Framework.ElementMap.Navigation
+{
+    /// <summary>
+    /// Elements from Navigation widget on the frontend.
+    /// </summary>
+    public class NavigationWidgetFrontend : HtmlElementContainer
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NavigationWidgetFrontend" /> class.
+        /// </summary>
+        /// <param name="find">The find.</param>
+        public NavigationWidgetFrontend(Find find)
+            : base(find)
+        {
+        }
+
+        /// <summary>
+        /// Gets the Toggle button on the frontend when toggle menu is present
+        /// </summary>
+        public HtmlButton ToggleButton
+        {
+            get
+            {
+                return this.Get<HtmlButton>("class=^navbar-toggle", "InnerText=~Toggle navigation");
+            }
+        }
+    }
+}
