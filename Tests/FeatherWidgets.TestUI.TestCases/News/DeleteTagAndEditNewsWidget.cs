1f4c96dd8597b463253fa9d5f37746699e5a08c8
VenetaBogoeva <veneta.bogoeva@telerik.com>
2014-10-27 17:30:28 +0200

    Removed ignore tag of UI tests

--- Tests/FeatherWidgets.TestUI.TestCases/News/DeleteTagAndEditNewsWidget.cs
+++ Tests/FeatherWidgets.TestUI.TestCases/News/DeleteTagAndEditNewsWidget.cs
@@ -17,7 +17,7 @@ namespace FeatherWidgets.TestUI
         /// </summary>
         [TestMethod,
        Microsoft.VisualStudio.TestTools.UnitTesting.Owner("Feather team"),
-       TestCategory(FeatherTestCategories.PagesAndContent), Ignore]
+       TestCategory(FeatherTestCategories.PagesAndContent)]
         public void DeleteTagAndEditNewsWidget()
         {
             BAT.Macros().NavigateTo().Pages();
--- Tests/FeatherWidgets.TestUI.TestCases/News/FilterNewsItemWithCustomTaxonomyOnPage.cs
+++ Tests/FeatherWidgets.TestUI.TestCases/News/FilterNewsItemWithCustomTaxonomyOnPage.cs
@@ -16,7 +16,7 @@ namespace FeatherWidgets.TestUI
         /// </summary>
         [TestMethod,
        Microsoft.VisualStudio.TestTools.UnitTesting.Owner("Feather team"),
-       TestCategory(FeatherTestCategories.PagesAndContent), Ignore]
+       TestCategory(FeatherTestCategories.PagesAndContent)]
         public void FilterNewsItemWithCustomTaxonomyOnPage()
         {
             BAT.Macros().NavigateTo().Pages();
