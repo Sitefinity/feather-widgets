a65393b42c3127e45478d282068908ffdd58b31f
VenetaBogoeva <veneta.bogoeva@telerik.com>
2014-10-31 15:58:07 +0200

    Ignored UI test

--- Tests/FeatherWidgets.TestUI.TestCases/News/ActivateAndDeactivateNewsModule.cs
+++ Tests/FeatherWidgets.TestUI.TestCases/News/ActivateAndDeactivateNewsModule.cs
@@ -20,7 +20,7 @@ namespace FeatherWidgets.TestUI
         /// </summary>
         [TestMethod,
        Microsoft.VisualStudio.TestTools.UnitTesting.Owner("Feather team"),
-       TestCategory(FeatherTestCategories.PagesAndContent)]
+       TestCategory(FeatherTestCategories.PagesAndContent), Ignore]
         public void ActivateAndDeactivateNewsModule()
         {
             BAT.Arrange(this.TestName).ExecuteArrangement("DeactivateNewsModule");
