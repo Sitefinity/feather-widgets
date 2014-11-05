58baf8fe58d908baf6e59941a7129732a7b17391
PetyaRachina <petya.rachina@telerik.com>
2014-11-05 16:04:14 +0200

    Added UI test for content block widget on different templates from packages

---
+++ Tests/FeatherWidgets.TestUI.Arrangements/ContentBlocks/ContentBlockWidgetOnPageBasedOnPackageTemplate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Telerik.Sitefinity.TestUI.Arrangements.Framework;
+using Telerik.Sitefinity.TestUI.Arrangements.Framework.Attributes;
+using Telerik.Sitefinity.TestUI.Arrangements.Framework.Server;
+using Telerik.Sitefinity.TestUtilities.CommonOperations;
+
+namespace FeatherWidgets.TestUI.Arrangements
+{
+    /// <summary>
+    /// Arrangement methods for ContentBlockWidgetOnPageBasedOnPackageTemplate
+    /// </summary>
+    public class ContentBlockWidgetOnPageBasedOnPackageTemplate : ITestArrangement
+    {
+        /// <summary>
+        /// Server side set up.
+        /// </summary>
+        [ServerSetUp]
+        public void SetUp()
+        {
+            string templateName = ServerArrangementContext.GetCurrent().Values["templateName"];
+
+            Guid templateId = ServerOperations.Templates().GetTemplateIdByTitle(templateName);
+            ServerOperations.Pages().CreatePage(PageName, templateId);
+        }
+
+        /// <summary>
+        /// Tears down.
+        /// </summary>
+        [ServerTearDown]
+        public void TearDown()
+        {
+            ServerOperations.Pages().DeleteAllPages();
+        }
+
+        private const string PageName = "FeatherPage";
+    }
+}
