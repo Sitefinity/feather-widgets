8787e8b6fd52421f530058a7f1e734e291a408b1
Boyko Karadzhov <bokovskii@gmail.com>
2014-11-20 12:52:13 +0200

    added ability to filter by multiple parent types

--- DynamicContent/Mvc/Models/DesignerModelHelper.cs
+++ DynamicContent/Mvc/Models/DesignerModelHelper.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web.Script.Serialization;
 using System.Web.UI;
+
 using DynamicContent.Mvc.Controllers;
+using DynamicContent.Mvc.StringResources;
 using Telerik.Sitefinity.DynamicModules.Builder.Model;
 using Telerik.Sitefinity.DynamicModules.Builder.Web.UI;
 using Telerik.Sitefinity.Frontend.Mvc.Infrastructure.Controllers;
 using Telerik.Sitefinity.Frontend.Mvc.Models;
+using Telerik.Sitefinity.Localization;
 using Telerik.Sitefinity.Utilities.TypeConverters;
 
 namespace DynamicContent.Mvc.Models
@@ -77,7 +80,9 @@ namespace DynamicContent.Mvc.Models
         public static string SerializedParentTypes(this Control control)
         {
             var parentTypes = control.ParentModuleTypes();
-            var parentTypeInfos = parentTypes.Select(p => new { TypeName = p.GetFullTypeName(), DisplayName = PluralsResolver.Instance.ToPlural(p.DisplayName).ToLower() });
+            var parentTypeInfos = parentTypes.Select(p => new { TypeName = p.GetFullTypeName(), DisplayName = p.DisplayName.ToLower() }).ToList();
+            if (parentTypeInfos.Count > 1)
+                parentTypeInfos.Add(new { TypeName = DynamicContentController.AnyParentValue, DisplayName = Res.Get<DynamicContentResources>().AnyParentContentType });
 
             return new JavaScriptSerializer().Serialize(parentTypeInfos);
         }
