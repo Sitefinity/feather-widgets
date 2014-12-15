7cc6b11660af38645a2dfab5b523b5257a1fb106
Boyko Karadzhov <bokovskii@gmail.com>
2014-12-15 18:27:22 +0200

    updated file versions

--- Telerik.Sitefinity.Frontend.DynamicContent/Properties/AssemblyInfo.cs
+++ Telerik.Sitefinity.Frontend.DynamicContent/Properties/AssemblyInfo.cs
@@ -30,7 +30,7 @@ using Telerik.Sitefinity.Frontend.Mvc.Infrastructure.Controllers.Attributes;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.20.3")]
+[assembly: AssemblyFileVersion("1.1.20.3")]
 
 [assembly: ControllerContainer(typeof(Telerik.Sitefinity.Frontend.DynamicContent.DynamicWidgetInitializer), "Initialize")]
