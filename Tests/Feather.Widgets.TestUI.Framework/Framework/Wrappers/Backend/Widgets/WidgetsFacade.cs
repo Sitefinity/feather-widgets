0e7864f6ef38e8a7d6457d6e9dd24fb82df68f03
Slaveya Shtereva <slaveya.shtereva@telerik.com>
2015-01-07 14:11:39 +0200

    Add missing files

---
+++ Tests/Feather.Widgets.TestUI.Framework/Framework/Wrappers/Backend/Widgets/WidgetsFacade.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Feather.Widgets.TestUI.Framework.Framework.Wrappers.Backend
+{
+    /// <summary>
+    /// Facade for Widgets wrapper
+    /// </summary>
+    public class WidgetsFacade
+    {
+        /// <summary>
+        /// Widgets content screen wrapper.
+        /// </summary>
+        /// <returns></returns>
+        public WidgetDesignerContentScreenWrapper WidgetDesignerContentScreenWrapper()
+        {
+            return new WidgetDesignerContentScreenWrapper();
+        }
+    }
+}
