d84579877ea779235c996987ca9ed8fefcb4755b
ElenaGaneva <Elena.Ganeva@telerik.com>
2015-01-07 14:36:38 +0200

    Added client test project

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,25 +9,85 @@ module.exports = function (grunt) {
 		jshint: {
 			//define the files to lint
 			files: ['gruntfile.js',
-					'ContentBlock/Mvc/Scripts/ContentBlock/*.js'
+					'**/*.js',
+					'!node_modules/**/*.js',
+					'!Tests/**/*.js'
 			]
 		},
 		
 		jasmine: {
 			unit:{
-				src: 'Mvc/Scripts/**/*.js',
+				src: [
+					'**/*.js',
+					'!node_modules/**/*.js',
+					'!Tests/**/*.js'
+				],
 				options: {
-					specs: 'JsTest/**/*.js'
+					vendor:[
+					'Tests/FeatherWidgets.ClientTest/helpers/jquery-1.8.3.min.js',
+					'Tests/FeatherWidgets.ClientTest/helpers/angular.js',
+					'Tests/FeatherWidgets.ClientTest/helpers/angular-resource.js',
+					'Tests/FeatherWidgets.ClientTesthelpers/angular-route.js',
+					'Tests/FeatherWidgets.ClientTest/helpers/angular-mocks.js',
+					'Tests/FeatherWidgets.ClientTest/helpers/kendo.web.min.js',
+					'Tests/FeatherWidgets.ClientTest/helpers/angular-kendo.js',
+					'Tests/FeatherWidgets.ClientTest/helpers/Telerik.Sitefinity.Frontend/MVC/Scripts/Bootstrap/js/*.js',
+					'Tests/FeatherWidgets.ClientTest/templates.js',
+					'Tests/FeatherWidgets.ClientTest/helpers/Telerik.Sitefinity.Frontend/Designers/Scripts/*.js',
+					'Tests/FeatherWidgets.ClientTest/helpers/Telerik.Sitefinity.Frontend/MVC/Scripts/Designer/*.js',
+					'Tests/FeatherWidgets.ClientTest/helpers/Telerik.Sitefinity.Frontend/MVC/Scripts/*.js',
+					'Tests/FeatherWidgets.ClientTest/helpers/Telerik.Sitefinity.Frontend/client-components/selectors/common/sf-services.js',
+					'Tests/FeatherWidgets.ClientTest/helpers/Telerik.Sitefinity.Frontend/client-components/selectors/common/sf-selectors.js',
+					'Tests/FeatherWidgets.ClientTest/helpers/Telerik.Sitefinity.Frontend/client-components/selectors/common/sf-list-selector.js',
+					'Tests/FeatherWidgets.ClientTest/helpers/Telerik.Sitefinity.Frontend/client-components/selectors/common/sf-items-tree.js',
+					'Tests/FeatherWidgets.ClientTest/helpers/Telerik.Sitefinity.Frontend/client-components/selectors/news/sf-news-item-service.js',
+					'Tests/FeatherWidgets.ClientTest/helpers/Telerik.Sitefinity.Frontend/client-components/selectors/news/sf-news-selector.js',
+					'Tests/FeatherWidgets.ClientTest/helpers/Telerik.Sitefinity.Frontend/client-components/selectors/dynamic-modules/sf-data-service.js',
+					'Tests/FeatherWidgets.ClientTest/helpers/Telerik.Sitefinity.Frontend/client-components/selectors/dynamic-modules/sf-dynamic-items-selector.js',
+					'Tests/FeatherWidgets.ClientTest/helpers/Telerik.Sitefinity.Frontend/client-components/selectors/taxonomies/sf-flat-taxon-service.js',
+					'Tests/FeatherWidgets.ClientTest/helpers/Telerik.Sitefinity.Frontend/client-components/selectors/taxonomies/sf-taxon-selector.js',
+					'Tests/FeatherWidgets.ClientTest/helpers/Telerik.Sitefinity.Frontend/client-components/selectors/date-time/sf-timespan-selector.js',
+					'Tests/FeatherWidgets.ClientTest/helpers/Telerik.Sitefinity.Frontend/client-components/selectors/pages/sf-page-service.js',
+					'Tests/FeatherWidgets.ClientTest/helpers/Telerik.Sitefinity.Frontend/client-components/selectors/pages/sf-page-selector.js',
+					'Tests/FeatherWidgets.ClientTest/helpers/mocks/*.js'
+					],
+					specs: ['Tests/FeatherWidgets.ClientTest/unit/**/*.js'],
+					junit: {
+						path: 'Tests/FeatherWidgets.ClientTest/TestResults'
+						},
+					template: require('grunt-template-jasmine-istanbul'),
+					templateOptions: {
+						coverage: 'Tests/FeatherWidgets.ClientTest/coverage/coverage.json',
+						report: [
+							{type: 'html', options: {dir: 'Tests/FeatherWidgets.ClientTest/coverage'}},
+							{type: 'cobertura', options: {dir: 'Tests/FeatherWidgets.ClientTest/coverage/cobertura'}},
+							{type: 'text-summary'}
+						]
+					}
 				}
-			}			
-		}
+			}
+		},
+		
+		//Converts directive's external html templates into javascript strings and stores them in the Angular's $templateCache service.
+		html2js: {
+			options: {
+			  singleModule: true,
+			  module: 'templates',
+			  base: 'Telerik.Sitefinity.Frontend'
+			},
+			main: {
+			  src: ['Tests/FeatherWidgets.ClientTest/helpers/Telerik.Sitefinity.Frontend/client-components/selectors/**/*.html'],
+			  dest: 'Tests/FeatherWidgets.ClientTest/templates.js'
+			},
+		},
 	});
 	
 	//Load the needed plugins
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-jasmine');
+	grunt.loadNpmTasks('grunt-html2js');
 	
 	//Default task(s)
-	grunt.registerTask('default', ['jshint']);
+	grunt.registerTask('default', ['jshint','html2js', 'jasmine']);
 	
 };
---
+++ Tests/FeatherWidgets.ClientTest/helpers/Telerik.Sitefinity.Frontend/Mvc/Scripts/Designer/designerview-propertygrid.js
@@ -0,0 +1,74 @@
+﻿(function () {
+    var propertyGridModule = angular.module('PropertyGridModule', ['designer', 'pageEditorServices', 'breadcrumb']);
+    angular.module('designer').requires.push('PropertyGridModule');
+
+    //basic controller for the advanced designer view
+    propertyGridModule.controller('PropertyGridCtrl', ['$scope', 'propertyService',
+        function ($scope, propertyService) {
+
+            var onGetPropertiesSuccess = function (data) {
+                if (data.Items)
+                    $scope.items = data.Items;
+            };
+
+            var onGetError = function (data) {
+                $scope.feedback.showError = true;
+                if (data)
+                    $scope.feedback.errorMessage = data.Detail;
+            };
+
+            propertyService.get().then(onGetPropertiesSuccess, onGetError)
+                .finally(function () {
+                    $scope.feedback.showLoadingIndicator = false;
+                });
+
+            $scope.feedback.showLoadingIndicator = true;
+
+            $scope.drillDownPropertyHierarchy = function (propertyPath, propertyName) {
+                $scope.propertyPath = propertyPath;
+                $scope.propertyName = propertyName;
+            };
+        }]);
+
+    //filters property hierarchy
+    propertyGridModule.filter('propertyHierarchy', function () {
+
+        return function (inputArray, propertyName, propertyPath) {
+            var currentLevel = 0;
+            if (propertyPath)
+                currentLevel = propertyPath.split('/').length - 1;
+
+            var levelFilter = function (property) {
+                var level = property.PropertyPath.split('/').length - 2;
+                if (!propertyName || propertyName == 'Home') {
+                    return level <= currentLevel;
+                } else {
+                    if (property.PropertyPath.indexOf(propertyPath) >= 0) {
+                        return currentLevel == level;
+                    } else {
+                        return false;
+                    }
+                }
+            };
+
+            var proxyFilter = function (property) {
+                if (property.IsProxy)
+                    return false;
+                else {
+                    if (!currentLevel)
+                        currentLevel = 1;
+
+                    return levelFilter(property);
+                }
+            };
+
+            var result = inputArray;
+            if (inputArray && inputArray[0].IsProxy)
+                result = inputArray.filter(proxyFilter);
+            else if (inputArray)
+                result = inputArray.filter(levelFilter);
+
+            return result;
+        };
+    });
+})();
