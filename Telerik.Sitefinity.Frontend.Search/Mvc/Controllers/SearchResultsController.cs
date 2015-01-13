5646c818d9aaeae36b74d7c51c6e23218c4cb3b0
GeorgiMateev <georgi.mateev@telerik.com>
2015-01-13 19:03:21 +0200

    Completely removed kendo from the search results template

--- Telerik.Sitefinity.Frontend.Search/Mvc/Controllers/SearchResultsController.cs
+++ Telerik.Sitefinity.Frontend.Search/Mvc/Controllers/SearchResultsController.cs
@@ -76,17 +76,17 @@ namespace Telerik.Sitefinity.Frontend.Search.Mvc.Controllers
 
                 if (isValid)
                 {
-                    var queryStringFormat = "?indexCatalogue={0}&searchQuery={1}&wordsMode={2}";
+                    var queryStringFormat = "?indexCatalogue={0}&searchQuery={1}&wordsMode={2}&orderBy={3}";
                     var languageParamFormat = "&language={0}";
 
-                    var queryString = string.Format(queryStringFormat, indexCatalogue, searchQuery, wordsMode);
+                    var queryString = string.Format(queryStringFormat, indexCatalogue, searchQuery, wordsMode, orderBy);
                     var languageParam = String.IsNullOrEmpty(language) ? String.Empty : String.Format(languageParamFormat, language);
                     var currentPageUrl = this.GetCurrentPageUrl();
 
                     this.ViewBag.LanguageSearchUrlTemplate = String.Concat(currentPageUrl, queryString, languageParamFormat);
                     this.ViewBag.RedirectPageUrlTemplate = String.Concat(this.GetCurrentPageUrl(), "/{0}", queryString, languageParam);
 
-                        this.InitializeOrderByEnum(orderBy);
+                    this.InitializeOrderByEnum(orderBy);
                     this.Model.IndexCatalogue = indexCatalogue;
 
                     if (page == null || page < 1)
