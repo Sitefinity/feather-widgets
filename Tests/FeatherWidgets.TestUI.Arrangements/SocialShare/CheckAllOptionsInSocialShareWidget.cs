875e6f0fe21740248ad292fb072f1cb3a6cd63b6
Slaveya Shtereva <slaveya.shtereva@telerik.com>
2015-01-13 16:38:02 +0200

    Social share test file

---
+++ Tests/FeatherWidgets.TestUI.Arrangements/SocialShare/CheckAllOptionsInSocialShareWidget.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using FeatherWidgets.TestUtilities.CommonOperations;
+using Telerik.Sitefinity.TestUI.Arrangements.Framework;
+using Telerik.Sitefinity.TestUI.Arrangements.Framework.Attributes;
+using Telerik.Sitefinity.TestUtilities.CommonOperations;
+
+namespace FeatherWidgets.TestUI.Arrangements
+{
+    /// <summary>
+    /// CheckAllOptionsInSocialnShareWidget arragement.
+    /// </summary>
+    public class CheckAllOptionsInSocialShareWidget : ITestArrangement
+    {
+        /// <summary>
+        /// Server side set up.
+        /// </summary>
+        [ServerSetUp]
+        public void SetUp()
+        {
+            Guid pageId = ServerOperations.Pages().CreatePage(PageName);
+            ServerOperationsFeather.Pages().AddSocialShareWidgetToPage(pageId);
+        }
+
+        /// <summary>
+        /// Tears down.
+        /// </summary>
+        [ServerTearDown]
+        public void TearDown()
+        {
+            ServerOperations.Pages().DeleteAllPages();
+        }
+
+        private const string PageName = "SocialShare";
+    }
+}
