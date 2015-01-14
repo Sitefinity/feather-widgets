72f247bfbee8212e1d561de6cf3de0a4c077694d
ElenaGaneva <Elena.Ganeva@telerik.com>
2015-01-14 18:23:41 +0200

    Add Test for searchResults in multilingual

--- Tests/FeatherWidgets.TestIntegration/SearchResults/SearchResultsWidgetTests.cs
+++ Tests/FeatherWidgets.TestIntegration/SearchResults/SearchResultsWidgetTests.cs
@@ -1,18 +1,22 @@
 ﻿using System;
+using System.Globalization;
 using System.Linq;
+using System.Threading;
 using FeatherWidgets.TestUtilities.CommonOperations;
 using MbUnit.Framework;
 using Telerik.Sitefinity;
+using Telerik.Sitefinity.Abstractions;
 using Telerik.Sitefinity.Data;
-using Telerik.Sitefinity.Frontend.ContentBlock.Mvc.Controllers;
 using Telerik.Sitefinity.Frontend.News.Mvc.Controllers;
 using Telerik.Sitefinity.Frontend.Search.Mvc.Controllers;
+using Telerik.Sitefinity.Model;
 using Telerik.Sitefinity.Mvc.Proxy;
+using Telerik.Sitefinity.News.Model;
 using Telerik.Sitefinity.Publishing;
 using Telerik.Sitefinity.Publishing.Configuration;
 using Telerik.Sitefinity.Services;
 using Telerik.Sitefinity.Services.Search;
-using Telerik.Sitefinity.Web;
+using Telerik.WebTestRunner.Server.Attributes;
 using SitefinityOperations = Telerik.Sitefinity.TestUtilities.CommonOperations;
 
 namespace FeatherWidgets.TestIntegration.SearchResults
@@ -35,6 +39,8 @@ namespace FeatherWidgets.TestIntegration.SearchResults
 
             for (int i = 1; i <= SearchResultsWidgetTests.NewsCount; i++)
                 Telerik.Sitefinity.TestUtilities.CommonOperations.ServerOperations.News().CreateNewsItem(SearchResultsWidgetTests.NewsTitle + i);
+
+            this.CreateNewsInSecondLanguage();
         }
 
         /// <summary>
@@ -49,6 +55,7 @@ namespace FeatherWidgets.TestIntegration.SearchResults
         }
 
         [Test]
+        [Multilingual]
         [Category(TestCategories.SearchResults)]
         [Author("FeatherTeam")]
         [Description("Verifies that all search results are returned correctly for all languages.")]
@@ -76,14 +83,83 @@ namespace FeatherWidgets.TestIntegration.SearchResults
             Assert.AreEqual(SearchResultsWidgetTests.NewsCount, searchResultsController.Model.Results.TotalCount);
         }
 
+        [Test]
+        [Multilingual]
+        [Category(TestCategories.SearchResults)]
+        [Author("FeatherTeam")]
+        [Description("Verifies that all search results are returned correctly for particular languages.")]
+        public void SearchResultsWidget_SpecificLanguages_ResultsFound()
+        {
+            int index = 1;
+            string testName = System.Reflection.MethodInfo.GetCurrentMethod().Name;
+            string pageNamePrefix = testName + "NewsPage" + index;
+            string pageTitlePrefix = testName + "NewsPage" + index;
+            string urlNamePrefix = testName + "news-page" + index;
+
+            var frontEndLanguages = AppSettings.CurrentSettings.DefinedFrontendLanguages;
+            var language = frontEndLanguages[2].Name;
+            string orderBy = "Oldest";
+            var searchResultsController = new SearchResultsController();
+
+            var mvcProxy = new MvcControllerProxy();
+            mvcProxy.ControllerName = typeof(NewsController).FullName;
+            var newsController = new NewsController();
+            newsController.Model.EnableSocialSharing = true;
+            mvcProxy.Settings = new ControllerSettings(newsController);
+
+            this.pageOperations.CreatePageWithControl(mvcProxy, pageNamePrefix, pageTitlePrefix, urlNamePrefix, index);
+
+            searchResultsController.Index(null, SearchResultsWidgetTests.NewsTitle, SearchResultsWidgetTests.SearchIndexName, null, language, orderBy);
+
+            Assert.AreEqual(1, searchResultsController.Model.Results.TotalCount);
+        }
+
         #region Helper methods
+
+        private void CreateNewsInSecondLanguage()
+        {
+            this.AssertIsMultilingual();
+
+            var frontEndLanguages = AppSettings.CurrentSettings.DefinedFrontendLanguages;
+            var secondLanguage = frontEndLanguages[2].Name;
+
+            var currentCulture = Thread.CurrentThread.CurrentUICulture;
+            try
+            {
+                System.Threading.Thread.CurrentThread.CurrentUICulture = new CultureInfo(secondLanguage);
+
+                App.Prepare()
+                   .SetRunPublishingSystemAsynchronous(false)
+                   .WorkWith()
+                   .AnyContentItem<NewsItem>()
+                   .CreateNew()
+                   .Do(item =>
+                   {
+                       item.Title[secondLanguage] = SearchResultsWidgetTests.NewsTitle + "20";
+                       ((NewsItem)item).GetString("Content")[secondLanguage] = "TestContent20";
+                   })
+                   .Publish()
+                   .Done()
+                   .SaveChanges();
+            }
+            finally
+            {
+                Thread.CurrentThread.CurrentUICulture = currentCulture;
+            }
+        }
+
+        private void AssertIsMultilingual()
+        {
+            Assert.IsTrue(AppSettings.CurrentSettings.Multilingual);
+            Assert.IsTrue(AppSettings.CurrentSettings.DefinedFrontendLanguages.Count() >= 2, "At least 2 frontend languages should be configured.");
+        }
 
         /// <summary>
         /// Deletes the index of the search.
         /// </summary>
         /// <param name="searchIndexName">Name of the search index.</param>
         /// <param name="publishingPointId">The publishing point identifier.</param>
-        protected void DeleteSearchIndex(string searchIndexName, Guid publishingPointId)
+        private void DeleteSearchIndex(string searchIndexName, Guid publishingPointId)
         {
             // delete the publishing point
             var transaction = Guid.NewGuid().ToString();
