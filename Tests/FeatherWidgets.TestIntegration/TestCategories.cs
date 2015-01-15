f9114449d90176560f54f6197bc98c0aab09b4af
GeorgiMateev <georgi.mateev@telerik.com>
2015-01-15 18:22:48 +0200

    More files from the merge

---
+++ Tests/FeatherWidgets.TestIntegration/TestCategories.cs
@@ -0,0 +1,18 @@
+﻿namespace FeatherWidgets.TestIntegration
+{
+    /// <summary>
+    /// Defines the integration test category names.
+    /// </summary>
+    public static class TestCategories
+    {
+        /// <summary>
+        /// Specifies the name of the category of tests related to Feather modules.
+        /// </summary>
+        public const string Samples = "Samples";
+        public const string Navigation = "Navigation";
+        public const string News = "News";
+        public const string ContentBlock = "ContentBlock";
+        public const string DynamicWidgets = "DynamicWidgets";
+        public const string SearchResults = "SearchResults";
+    }
+}
