1cd604f8695143190c83e87fc78438a874922536
PetyaRachina <petya.rachina@telerik.com>
2015-02-11 17:25:28 +0200

    fixed style cop error

--- Tests/Feather.Widgets.TestUI.Framework/Framework/Wrappers/Backend/Pages/PageZoneEditorWrapper.cs
+++ Tests/Feather.Widgets.TestUI.Framework/Framework/Wrappers/Backend/Pages/PageZoneEditorWrapper.cs
@@ -100,7 +100,7 @@ namespace Feather.Widgets.TestUI.Framework.Framework.Wrappers.Backend
         /// Adds Mvc widget to selected placeholder.
         /// </summary>
         /// <param name="placeHolder">The placeholder id.</param>
-        public void AddMvcWidgetToSelectedPlaceHolder(string widgetName, string placeHolder="Body")
+        public void AddMvcWidgetToSelectedPlaceHolder(string widgetName, string placeHolder = "Body")
         {
             HtmlDiv radDockZone = ActiveBrowser.Find.ByExpression<HtmlDiv>("id=?" + placeHolder)
                .AssertIsPresent<HtmlDiv>(placeHolder);
