f4a1582adb6a101758d6482da928bb2cff312b9f
Slaveya Shtereva <slaveya.shtereva@telerik.com>
2015-02-12 18:00:13 +0200

    Add new tests for link selector, pages option

---
+++ Tests/FeatherWidgets.TestUI.Arrangements/ContentBlocks/LinkSelectorInsertLinkToPageFromThisSiteOverSelectedText.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using FeatherWidgets.TestUtilities.CommonOperations;
+using Telerik.Sitefinity.Services;
+using Telerik.Sitefinity.TestUI.Arrangements.Framework;
+using Telerik.Sitefinity.TestUI.Arrangements.Framework.Attributes;
+using Telerik.Sitefinity.TestUI.Arrangements.Framework.Server;
+using Telerik.Sitefinity.TestUtilities.CommonOperations;
+
+namespace FeatherWidgets.TestUI.Arrangements
+{
+    /// <summary>
+    /// This is a test class with server site methods for Link selector and page from this site option over selected text.
+    /// </summary>
+    public class LinkSelectorInsertLinkToPageFromThisSiteOverSelectedText : ITestArrangement
+    {
+        /// <summary>
+        /// Server side set up.
+        /// </summary>
+        [ServerSetUp]
+        public void SetUp()
+        {
+            Guid selectorPageId = ServerOperations.Pages().CreatePage(LinkSelectorPageName);
+            ServerOperationsFeather.Pages().AddContentBlockWidgetToPage(selectorPageId, ContentBlockHtml);
+
+            Guid parentPageId = ServerOperations.Pages().CreatePage(ParentPageName);
+
+            Guid childPageId = Guid.NewGuid();
+            ServerOperations.Pages().CreatePage(ChildPageName, childPageId, parentPageId);    
+        }
+
+        [ServerArrangement]
+        public void GetSiteMapRootNodeId()
+        {
+            // We need the siteMapRootNodeId in order to form the sfref later in the UI test.
+            Guid siteMapRootNodeId = SystemManager.CurrentContext.CurrentSite.SiteMapRootNodeId;
+
+            ServerArrangementContext.GetCurrent().Values.Add("siteMapRootNodeId", siteMapRootNodeId.ToString());
+        }
+
+        [ServerArrangement]
+        public void GetChildPageId()
+        {
+            // We need the child page Id in order to form the sfref later in the UI test.
+            Guid childPageId = ServerOperations.Pages().GetPageId(ChildPageName);
+
+            ServerArrangementContext.GetCurrent().Values.Add("childPageId", childPageId.ToString());
+        }
+
+        /// <summary>
+        /// Tears down.
+        /// </summary>
+        [ServerTearDown]
+        public void TearDown()
+        {
+            ServerOperations.Pages().DeleteAllPages();
+        }
+
+        private const string LinkSelectorPageName = "PageWithContentBlockAndLinkSelector";
+        private const string ParentPageName = "ParentPage";
+        private const string ChildPageName = "ChildPage";
+        private const string ContentBlockHtml = "Test content";
+    }
+}
---
+++ Tests/FeatherWidgets.TestUI.TestCases/ContentBlocks/LinkSelectorInsertLinkToPageFromThisSiteOverSelectedText.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Feather.Widgets.TestUI.Framework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FeatherWidgets.TestUI.TestCases.ContentBlocks
+{
+    /// <summary>
+    /// This is a test class with tests related to Link selector Page from this site option over selected text.
+    /// </summary>
+    [TestClass]
+    public class LinkSelectorInsertLinkToPageFromThisSiteOverSelectedText_ : FeatherTestCase
+    {
+        /// <summary>
+        /// UI test LinkSelectorInsertLinkToPageFromThisSiteOverSelectedText
+        /// </summary>
+        [TestMethod,
+        Owner("Feather Team"),
+        TestCategory(FeatherTestCategories.PagesAndContent)]
+        public void LinkSelectorInsertLinkToPageFromThisSiteOverSelectedText()
+        {
+            BAT.Macros().NavigateTo().Pages();
+            BAT.Wrappers().Backend().Pages().PagesWrapper().OpenPageZoneEditor(LinkSelectorPageName);
+            BATFeather.Wrappers().Backend().Pages().PageZoneEditorWrapper().EditWidget(WidgetName);
+            BATFeather.Wrappers().Backend().ContentBlocks().ContentBlocksWrapper().SelectContentInEditableArea();
+            BATFeather.Wrappers().Backend().ContentBlocks().ContentBlocksWrapper().OpenLinkSelector();
+            BATFeather.Wrappers().Backend().ContentBlocks().LinkSelectorWrapper().SwitchToSelectedTab(SelectedTabName);
+
+            Assert.IsFalse(BATFeather.Wrappers().Backend().ContentBlocks().LinkSelectorWrapper().IsInsertLinkEnabled());
+
+            BATFeather.Wrappers().Backend().Widgets().SelectorsWrapper().SearchItemByTitle(SearchText);
+            BATFeather.Wrappers().Backend().Widgets().SelectorsWrapper().WaitForItemsToAppear(SearchResultsCount);
+            BATFeather.Wrappers().Backend().Widgets().SelectorsWrapper().VerifyNoItemsFound();
+
+            BATFeather.Wrappers().Backend().Widgets().SelectorsWrapper().SearchItemByTitle(NewSearchText);
+            BATFeather.Wrappers().Backend().Widgets().SelectorsWrapper().WaitForItemsToAppear(NewSearchResultsCount);
+            BATFeather.Wrappers().Backend().Widgets().SelectorsWrapper().CheckBreadcrumbAfterSearchInFlatSelector(NewSearchText, ResultPageBreadcrumb);
+            BATFeather.Wrappers().Backend().Widgets().SelectorsWrapper().SelectItemsInFlatSelector(NewSearchText);
+
+            BATFeather.Wrappers().Backend().ContentBlocks().LinkSelectorWrapper().VerifyCorrectTextToDisplay(TextToDisplay, TabIndex);
+            BATFeather.Wrappers().Backend().ContentBlocks().LinkSelectorWrapper().InsertLink();
+
+            BATFeather.Wrappers().Backend().ContentBlocks().ContentBlocksWrapper().VerifyContentBlockTextDesignMode(TextToDisplay);
+            BATFeather.Wrappers().Backend().ContentBlocks().ContentBlocksWrapper().SwitchToHtmlView();
+            BATFeather.Wrappers().Backend().ContentBlocks().ContentBlocksWrapper().VerifyContentInHtmlEditableArea(this.HtmlContent());
+            BATFeather.Wrappers().Backend().ContentBlocks().ContentBlocksWrapper().SaveChanges();
+            BAT.Wrappers().Backend().Pages().PageZoneEditorWrapper().PublishPage();
+            BAT.Macros().NavigateTo().CustomPage("~/" + LinkSelectorPageName.ToLower());
+            BATFeather.Wrappers().Frontend().CommonWrapper().VerifySelectedAnchorLink(TextToDisplay, ExpectedUrl);
+        }
+
+        /// <summary>
+        /// Login admin user and creates page with content block widget.
+        /// Creates a parent and child page in order to be used in the link selector.
+        /// </summary>
+        protected override void ServerSetup()
+        {
+            BAT.Macros().User().EnsureAdminLoggedIn();
+            BAT.Arrange(this.TestName).ExecuteSetUp();
+        }
+
+        /// <summary>
+        /// Deletes all pages.
+        /// </summary>
+        protected override void ServerCleanup()
+        {
+            BAT.Arrange(this.TestName).ExecuteTearDown();
+        }
+
+        private string HtmlContent()
+        {
+            string siteMapRootNodeId = BAT.Arrange(this.TestName).ExecuteArrangement("GetSiteMapRootNodeId").Result.Values["siteMapRootNodeId"];
+            string childPageId = BAT.Arrange(this.TestName).ExecuteArrangement("GetChildPageId").Result.Values["childPageId"];
+
+            string content = "<a href=\"/" + ParentPageName.ToLower() + 
+                "/" + InsertedPageName.ToLower() + 
+                "\" sfref=\"[" + siteMapRootNodeId + "]" + childPageId +
+                "\">" + TextToDisplay + "</a>";
+
+            return content;
+        }
+
+        private const string LinkSelectorPageName = "PageWithContentBlockAndLinkSelector";
+        private const string WidgetName = "ContentBlock";
+        private const string SelectedTabName = "Page from this site";
+        private const string InsertedPageName = "ChildPage";
+        private const string ParentPageName = "ParentPage";
+        private const int TabIndex = 2;
+        private const string ExpectedUrl = "parentpage/childpage";
+        private const string TextToDisplay = "Test content";
+        private const string SearchText = "FeatherPage";
+        private const int SearchResultsCount = 0;
+        private const string NewSearchText = "ChildPage";
+        private const string ResultPageBreadcrumb = "Under ParentPage";
+        private const int NewSearchResultsCount = 1;
+    }
+}
