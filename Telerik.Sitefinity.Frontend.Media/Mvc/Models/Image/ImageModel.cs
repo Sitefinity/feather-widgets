df476d0e08925e86e81d2b4306701b5a4380596c
manev <kaloyan.manev@telerik.com>
2015-02-18 16:58:54 +0200

    Changed folder structure

---
+++ Telerik.Sitefinity.Frontend.Media/Mvc/Models/Image/ImageModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Telerik.Sitefinity.Frontend.Media.Mvc.Models
+{
+    /// <summary>
+    ///     This class is used as a model for the image controller.
+    /// </summary>
+    public class ImageModel : IImageModel
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ImageModel" /> class.
+        /// </summary>
+        public ImageModel()
+        {
+
+        }
+    }
+}
