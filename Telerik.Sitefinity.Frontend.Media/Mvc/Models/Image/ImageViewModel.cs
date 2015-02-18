71dfc5977f92d9ed213e27dfd206ac9ffb33a7ff
dzhenko <dzhenko@gmail.com>
2015-02-18 17:15:15 +0200

    View model

---
+++ Telerik.Sitefinity.Frontend.Media/Mvc/Models/Image/ImageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Telerik.Sitefinity.Model;
+
+namespace Telerik.Sitefinity.Frontend.Media.Mvc.Models.Image
+{
+    public class ImageViewModel
+    {
+        public Lstring Title { get; set; }
+    }
+}
