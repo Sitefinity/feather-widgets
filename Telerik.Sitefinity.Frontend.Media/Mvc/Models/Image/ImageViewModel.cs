a10e9aed3a9c85576bdf5aa9eed715dbecf426f4
ElenaGaneva <Elena.Ganeva@telerik.com>
2015-02-18 17:19:59 +0200

    Added alternative text

--- Telerik.Sitefinity.Frontend.Media/Mvc/Models/Image/ImageViewModel.cs
+++ Telerik.Sitefinity.Frontend.Media/Mvc/Models/Image/ImageViewModel.cs
@@ -2,12 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Telerik.Sitefinity.Frontend.Mvc.Models;
 using Telerik.Sitefinity.Model;
 
 namespace Telerik.Sitefinity.Frontend.Media.Mvc.Models.Image
 {
+    /// <summary>
+    /// This class represents view model for Image content.
+    /// </summary>
     public class ImageViewModel
     {
+        /// <summary>
+        /// Gets or sets the title.
+        /// </summary>
+        /// <value>
+        /// The title.
+        /// </value>
         public Lstring Title { get; set; }
+
+        /// <summary>
+        /// Gets or sets the alternative text.
+        /// </summary>
+        /// <value>
+        /// The alternative text.
+        /// </value>
+        public Lstring AlternativeText { get; set; }
+
+        /// <summary>
+        /// Gets or sets the item.
+        /// </summary>
+        /// <value>
+        /// The item.
+        /// </value>
+        public ItemViewModel Item { get; set; }
     }
 }
