edb07d59828f6fc934f716073f733c606b8e15d7
ElenaGaneva <Elena.Ganeva@telerik.com>
2015-02-18 16:28:56 +0200

    Added simple designer view script

---
+++ Telerik.Sitefinity.Frontend.Media/Mvc/Scripts/Image/designerview-simple.js
@@ -0,0 +1,6 @@
+﻿(function ($) {
+
+    var simpleViewModule = angular.module('simpleViewModule', ['designer', 'kendo.directives', 'sfFields', 'sfSelectors']);
+    angular.module('designer').requires.push('simpleViewModule');
+
+})(jQuery);
