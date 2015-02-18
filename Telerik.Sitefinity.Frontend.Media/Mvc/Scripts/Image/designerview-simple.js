55a688d5bc6c9114d4d877bc9c6853f97162c7be
ElenaGaneva <Elena.Ganeva@telerik.com>
2015-02-18 18:15:16 +0200

    Added angular cotnroller

--- Telerik.Sitefinity.Frontend.Media/Mvc/Scripts/Image/designerview-simple.js
+++ Telerik.Sitefinity.Frontend.Media/Mvc/Scripts/Image/designerview-simple.js
@@ -2,5 +2,29 @@
 
     var simpleViewModule = angular.module('simpleViewModule', ['designer', 'kendo.directives', 'sfFields', 'sfSelectors']);
     angular.module('designer').requires.push('simpleViewModule');
+    designerModule.controller('SimpleCtrl', ['$scope', 'propertyService', 'sfMediaMarkupService', function ($scope, propertyService, mediaMarkupService) {
+        $scope.feedback.showLoadingIndicator = true;
 
+        $scope.$watch('model', function (newVal, oldVal) {
+            if (newVal !== oldVal && newVal && newVal[0]) {
+                $scope.properties.Item.PropertyValue = newVal.item;
+            }
+        });
+
+        propertyService.get()
+            .then(function (data) {
+                if (data) {
+                    $scope.properties = propertyService.toAssociativeArray(data.Items);
+                    $scope.model = $scope.properties.Item.PropertyValue;
+                }
+            },
+            function (data) {
+                $scope.feedback.showError = true;
+                if (data)
+                    $scope.feedback.errorMessage = data.Detail;
+            })
+            .finally(function () {
+                $scope.feedback.showLoadingIndicator = false;
+            });
+    }]);
 })(jQuery);
