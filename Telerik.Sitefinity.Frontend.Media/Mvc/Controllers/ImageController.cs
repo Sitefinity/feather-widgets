19b57a259bd8e4cfdc8d8cb9ffa163dd2830a629
Tihomir Petrov <tihomir.petrov@telerik.com>
2015-02-18 16:09:03 +0200

    Empty ImageController is added.

---
+++ Telerik.Sitefinity.Frontend.Media/Mvc/Controllers/ImageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Telerik.Sitefinity.Mvc;
+
+namespace Telerik.Sitefinity.Frontend.Media.Mvc.Controllers
+{
+    /// <summary>
+    /// This class represents the controller of the Image widget.
+    /// </summary>
+    [ControllerToolboxItem(Name = "Image", Title = "Image", SectionName = "MvcWidgets")]
+    public class ImageController: Controller
+    {
+    }
+}
---
+++ Telerik.Sitefinity.Frontend.Media/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1398071a-5c84-40e5-bd34-94209a033504")]
+
+[assembly: AssemblyTitle("Telerik.Sitefinity.Frontend.DynamicContent")]
