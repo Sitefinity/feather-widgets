6945c142da334039f5a50d1a6b9da2694924d7b2
ElenaGaneva <Elena.Ganeva@telerik.com>
2015-02-18 23:05:02 +0200

    Change designer model according to widget model

--- Telerik.Sitefinity.Frontend.Media/Mvc/Scripts/Image/designerview-simple.js
+++ Telerik.Sitefinity.Frontend.Media/Mvc/Scripts/Image/designerview-simple.js
@@ -2,14 +2,15 @@
 
     var simpleViewModule = angular.module('simpleViewModule', ['designer', 'kendo.directives', 'sfFields', 'sfSelectors']);
     angular.module('designer').requires.push('simpleViewModule');
-    simpleViewModule.controller('SimpleCtrl', ['$scope', 'propertyService', 'sfMediaMarkupService', function ($scope, propertyService, mediaMarkupService) {
+    simpleViewModule.controller('SimpleCtrl', ['$scope', 'propertyService', 'serverContext', 'sfMediaMarkupService', function ($scope, propertyService, serverContext, mediaMarkupService) {
         $scope.feedback.showLoadingIndicator = true;
+        $scope.thumbnailSizeTempalteUrl = serverContext.getEmbeddedResourceUrl('Telerik.Sitefinity.Frontend', 'client-components/selectors/media/sf-thumbnail-size-selection.html');
 
         $scope.$watch('model', function (newVal, oldVal) {
             if (newVal !== oldVal) {
-                $scope.properties.Item.PropertyValue = newVal.item;
-                $scope.properties.Title.PropertyValue = newVal.item.Title;
-                $scope.properties.AlternativeText.PropertyValue = newVal.item.AlternativeText;
+                $scope.properties.Id.PropertyValue = newVal.item.Id;
+                $scope.properties.Title.PropertyValue = newVal.title;
+                $scope.properties.AlternativeText.PropertyValue = newVal.alternativeText;
             }
         });
 
@@ -17,7 +18,8 @@
             .then(function (data) {
                 if (data) {
                     $scope.properties = propertyService.toAssociativeArray(data.Items);
-                    $scope.model = $scope.properties.Item.PropertyValue;
+                    var markup = $scope.properties.Markup.PropertyValue || '';
+                    $scope.model = mediaMarkupService.image.properties(markup);
                 }
             },
             function (data) {
