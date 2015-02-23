bbd8bd2fb73a5fc77caebcba0e41b5142318a79a
Boyko Karadzhov <bokovskii@gmail.com>
2015-02-23 11:40:24 +0200

    image gallery model

---
+++ Telerik.Sitefinity.Frontend.Media/Mvc/Models/ParentFilterMode.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Telerik.Sitefinity.Frontend.Media.Mvc.Models
+{
+    /// <summary>
+    /// Mode for filtering dynamic content items by parent.
+    /// </summary>
+    public enum ParentFilterMode
+    {
+        All,
+        Selected,
+        CurrentlyOpen,
+        NotApplicable
+    }
+}
