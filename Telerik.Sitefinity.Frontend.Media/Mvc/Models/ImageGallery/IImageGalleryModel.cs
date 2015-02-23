29c4e5a010dcbc97a575bb3ab689823a9beff5e6
Boyko Karadzhov <bokovskii@gmail.com>
2015-02-23 11:51:07 +0200

    updated image gallery model interface

--- Telerik.Sitefinity.Frontend.Media/Mvc/Models/ImageGallery/IImageGalleryModel.cs
+++ Telerik.Sitefinity.Frontend.Media/Mvc/Models/ImageGallery/IImageGalleryModel.cs
@@ -9,8 +9,118 @@ using Telerik.Sitefinity.Taxonomies.Model;
 
 namespace Telerik.Sitefinity.Frontend.Media.Mvc.Models.ImageGallery
 {
+    /// <summary>
+    /// Classes that implement this interface provide business logic for getting Image items.
+    /// </summary>
     public interface IImageGalleryModel
     {
+        /// <summary>
+        /// Gets or sets the CSS class that will be applied on the wrapping element of the widget when it is in List view.
+        /// </summary>
+        /// <value>
+        /// The CSS class.
+        /// </value>
+        string ListCssClass { get; set; }
+
+        /// <summary>
+        /// Gets or sets the CSS class that will be applied on the wrapper div of the widget when it is in Details view.
+        /// </summary>
+        /// <value>
+        /// The CSS class.
+        /// </value>
+        string DetailCssClass { get; set; }
+
+        /// <summary>
+        /// Gets the list of items to be displayed inside the widget when option "Selected items" is enabled.
+        /// </summary>
+        /// <value>
+        /// The selected item ids.
+        /// </value>
+        string SerializedSelectedItemsIds { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether to enable social sharing.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if should enable social sharing; otherwise, <c>false</c>.
+        /// </value>
+        bool EnableSocialSharing { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the provider.
+        /// </summary>
+        /// <value>The name of the provider.</value>
+        string ProviderName { get; set; }
+
+        /// <summary>
+        /// Gets or sets which items to be displayed in the list view.
+        /// </summary>
+        /// <value>The page display mode.</value>
+        SelectionMode SelectionMode { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether to divide items in the list.
+        /// </summary>
+        /// <value>
+        /// The display mode.
+        /// </value>
+        ListDisplayMode DisplayMode { get; set; }
+
+        /// <summary>
+        /// Gets or sets the items count per page.
+        /// </summary>
+        /// <value>
+        /// The items per page.
+        /// </value>
+        int? ItemsPerPage { get; set; }
+
+        /// <summary>
+        /// Gets or sets the sort expression.
+        /// </summary>
+        /// <value>
+        /// The sort expression.
+        /// </value>
+        string SortExpression { get; set; }
+
+        /// <summary>
+        /// Gets or sets the additional filter expression.
+        /// </summary>
+        /// <value>
+        /// The filter expression.
+        /// </value>
+        string FilterExpression { get; set; }
+
+        /// <summary>
+        /// Gets or sets the serialized additional filters.
+        /// </summary>
+        /// <value>
+        /// The serialized additional filters.
+        /// </value>
+        string SerializedAdditionalFilters { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the canonical URL tag should be added to the page when the canonical meta tag should be added to the page.
+        /// If the value is not set, the settings from SystemConfig -> ContentLocationsSettings -> DisableCanonicalURLs will be used. 
+        /// </summary>
+        /// <value>The disable canonical URLs.</value>
+        bool? DisableCanonicalUrlMetaTag { get; set; }
+
+        /// <summary>
+        /// Gets or sets the parent filtering mode.
+        /// </summary>
+        /// <value>
+        /// The parent filtering mode.
+        /// </value>
+        ParentFilterMode ParentFilterMode { get; set; }
+
+        /// <summary>
+        /// Gets or sets the serialized selected parent ids.
+        /// </summary>
+        /// <value>
+        /// The serialized selected parents ids.
+        /// </value>
+        string SerializedSelectedParentsIds { get; set; }
+
         /// <summary>
         /// Gets the information for all of the content types that a control is able to show.
         /// </summary>
