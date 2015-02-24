51312898ba4cd06834739d47e3c2f47efa1f89a0
dzhenko <dzhenko@gmail.com>
2015-02-24 11:14:56 +0200

    Altered cshtml

--- Telerik.Sitefinity.Frontend.Identity/Mvc/Models/LoginStatus/LoginStatusModel.cs
+++ Telerik.Sitefinity.Frontend.Identity/Mvc/Models/LoginStatus/LoginStatusModel.cs
@@ -1,14 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Web;
 using Telerik.Sitefinity.Modules.Pages;
 using Telerik.Sitefinity.Security;
-using Telerik.Sitefinity.Security.Claims;
 using Telerik.Sitefinity.Security.Model;
-using Telerik.Sitefinity.Services;
-using Telerik.Sitefinity.Web;
 
 namespace Telerik.Sitefinity.Frontend.Identity.Mvc.Models.LoginStatus
 {
@@ -56,12 +50,10 @@ namespace Telerik.Sitefinity.Frontend.Identity.Mvc.Models.LoginStatus
         }
 
         /// <inheritdoc />
-        public StatusViewModel GetStatus()
+        public StatusViewModel GetStatusViewModel()
         {
             var response = new StatusViewModel() { IsLoggedIn = false };
 
-            // TODO: Decide
-            //var user = SecurityManager.GetUser(ClaimsManager.GetCurrentIdentity().UserId);
             var user = SecurityManager.GetUser(SecurityManager.GetCurrentUserId());
 
             if (user != null)
