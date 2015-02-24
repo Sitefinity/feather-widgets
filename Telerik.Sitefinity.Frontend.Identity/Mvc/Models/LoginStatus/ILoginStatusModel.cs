b8e78b9a29a176163defd6e28d3e222355557f3d
pivanova <penka.ivanova@telerik.com>
2015-02-24 10:00:30 +0200

    Added model properties for login, logout, registration and profile pafes

--- Telerik.Sitefinity.Frontend.Identity/Mvc/Models/LoginStatus/ILoginStatusModel.cs
+++ Telerik.Sitefinity.Frontend.Identity/Mvc/Models/LoginStatus/ILoginStatusModel.cs
@@ -8,5 +8,30 @@ namespace Telerik.Sitefinity.Frontend.Identity.Mvc.Models.LoginStatus
     public interface ILoginStatusModel
     {
         LoginStatusViewModel GetViewModel();
+
+        /// <summary>
+        /// Gets or sets the css class that will be applied on the wrapping element of the widget.
+        /// </summary>
+        string CssClass { get; set; }
+
+        /// <summary>
+        /// Gets or sets id of the page where the user will be redirected after logout
+        /// </summary>
+        Guid LoginPageId { get; set; }
+
+        /// <summary>
+        /// Gets or sets id of the page where you have dropped Login form widget
+        /// </summary>
+        Guid LogoutPageId { get; set; }
+
+        /// <summary>
+        /// Gets or sets id of the page where you have dropped Profile widget
+        /// </summary>
+        Guid ProfilePageId { get; set; }
+
+        /// <summary>
+        /// Gets or sets id of the page where you have dropped Registration widget
+        /// </summary>
+        Guid RegistrationPageId { get; set; }
     }
 }
--- Telerik.Sitefinity.Frontend.Identity/Mvc/Models/LoginStatus/LoginStatusModel.cs
+++ Telerik.Sitefinity.Frontend.Identity/Mvc/Models/LoginStatus/LoginStatusModel.cs
@@ -11,5 +11,20 @@ namespace Telerik.Sitefinity.Frontend.Identity.Mvc.Models.LoginStatus
         {
             throw new NotImplementedException();
         }
+
+        /// <inheritdoc />
+        public string CssClass { get; set; }
+
+        /// <inheritdoc />
+        public Guid LoginPageId { get; set; }
+
+        /// <inheritdoc />
+        public Guid LogoutPageId { get; set; }
+
+        /// <inheritdoc />
+        public Guid ProfilePageId { get; set; }
+
+        /// <inheritdoc />
+        public Guid RegistrationPageId { get; set; }
     }
 }
