8f37219585b3dc98d1abca3ea42153c53cf4fe85
ElenaGaneva <Elena.Ganeva@telerik.com>
2015-02-25 16:39:57 +0200

    Modify tests to make request to page

--- Tests/FeatherWidgets.TestIntegration/Identity/LoginStatus/LoginTests.cs
+++ Tests/FeatherWidgets.TestIntegration/Identity/LoginStatus/LoginTests.cs
@@ -1,12 +1,11 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Globalization;
 using System.Web;
-using System.Web.Mvc;
+using FeatherWidgets.TestUtilities.CommonOperations;
 using MbUnit.Framework;
 using Telerik.Sitefinity.Frontend.Identity.Mvc.Controllers;
-using Telerik.Sitefinity.Frontend.Identity.Mvc.Models.LoginStatus;
 using Telerik.Sitefinity.Frontend.TestUtilities;
 using Telerik.Sitefinity.Mvc.Proxy;
-using Telerik.Sitefinity.TestUtilities.CommonOperations;
 using Telerik.Sitefinity.Web;
 
 namespace FeatherWidgets.TestIntegration.Identity.LoginStatus
@@ -18,12 +17,31 @@ namespace FeatherWidgets.TestIntegration.Identity.LoginStatus
     [Description("This is a test class with tests related to login functionality of LoginStatus widget.")]
     public class LoginTests
     {
+        /// <summary>
+        /// Set up method
+        /// </summary>
+        [SetUp]
+        public void Setup()
+        {
+            this.pageOperations = new PagesOperations();
+        }
+
+        /// <summary>
+        /// Tears down.
+        /// </summary>
+        [TearDown]
+        public void TearDown()
+        {
+            this.pageOperations.DeletePages();
+        }
+
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1726:UsePreferredTerms", MessageId = "Login"), Test]
         [Category(TestCategories.Identity)]
         [Author(FeatherTeams.Team2)]
         [Description("Verify when External login page url is provided, redirect link is constructed correctly.")]
         public void Login_RedirectToExternalPage_VerifyLoginRedirectUrlIsCorrect()
         {
+            string loginStatusPageUrl = UrlPath.ResolveAbsoluteUrl("~/" + this.urlNamePrefix + this.pageIndex);
             string expectedLoginUrl = "www.telerik.com";
 
             var mvcProxy = new MvcControllerProxy();
@@ -32,9 +50,9 @@ namespace FeatherWidgets.TestIntegration.Identity.LoginStatus
             loginStatusController.Model.ExternalLoginUrl = expectedLoginUrl;
             mvcProxy.Settings = new ControllerSettings(loginStatusController);
 
-            var actionResult = (ViewResult)loginStatusController.Index();
-            var loginStatusViewModel = actionResult.Model as LoginStatusViewModel;
-            Assert.AreEqual(expectedLoginUrl, loginStatusViewModel.LoginPageUrl, "Login redirect url is not as expected");
+            this.pageOperations.CreatePageWithControl(mvcProxy, this.pageNamePrefix, this.pageTitlePrefix, this.urlNamePrefix, this.pageIndex);
+            var responseContent = PageInvoker.ExecuteWebRequest(loginStatusPageUrl);
+            Assert.IsTrue(responseContent.Contains(expectedLoginUrl), "Login redirect url is not as expected");
         }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1726:UsePreferredTerms", MessageId = "Login"), Test]
@@ -43,14 +61,14 @@ namespace FeatherWidgets.TestIntegration.Identity.LoginStatus
         [Description("Verify when Page id is provided, redirect link is constructed correctly.")]
         public void Login_WithPageId_VerifyLoginRedirectUrlIsCorrect()
         {
-            var pageOperations = new PagesOperations();
+            string loginStatusPageUrl = UrlPath.ResolveAbsoluteUrl("~/" + this.urlNamePrefix + this.pageIndex);
+            var basicPageOperations = new Telerik.Sitefinity.TestUtilities.CommonOperations.PagesOperations();
             var pageTitle = "Page Title1";
             var pageUrl = "PageTitle1";
             var absoluteUrl = UrlPath.GetDomainUrl() + "/" + pageUrl;
-
             try
             {
-                var loginPageId = pageOperations.CreatePage(pageTitle, pageUrl);
+                var loginPageId = basicPageOperations.CreatePage(pageTitle, pageUrl);
 
                 var mvcProxy = new MvcControllerProxy();
                 mvcProxy.ControllerName = typeof(LoginStatusController).FullName;
@@ -58,13 +76,13 @@ namespace FeatherWidgets.TestIntegration.Identity.LoginStatus
                 loginStatusController.Model.LoginPageId = loginPageId;
                 mvcProxy.Settings = new ControllerSettings(loginStatusController);
 
-                var actionResult = (ViewResult)loginStatusController.Index();
-                var loginStatusViewModel = actionResult.Model as LoginStatusViewModel;
-                Assert.AreEqual(this.GetExpectedUrlWithParams(absoluteUrl), loginStatusViewModel.LoginPageUrl, "Login redirect url is not as expected");
+                this.pageOperations.CreatePageWithControl(mvcProxy, this.pageNamePrefix, this.pageTitlePrefix, this.urlNamePrefix, this.pageIndex);
+                var responseContent = PageInvoker.ExecuteWebRequest(loginStatusPageUrl);
+                Assert.IsTrue(responseContent.Contains(this.GetExpectedUrlWithParams(absoluteUrl)), "Login redirect url is not as expected");
             }
-            finally
+            finally 
             {
-                pageOperations.DeleteAllPages();
+                basicPageOperations.DeleteAllPages();
             }
         }
 
@@ -74,6 +92,7 @@ namespace FeatherWidgets.TestIntegration.Identity.LoginStatus
         [Description("Verify when AllowInstantLogin is set to true, redirect link is constructed correctly.")]
         public void Login_WithInstantLogin_VerifyLoginRedirectUrlIsCorrect()
         {
+            string loginStatusPageUrl = UrlPath.ResolveAbsoluteUrl("~/" + this.urlNamePrefix + this.pageIndex);
             var absoluteUrl = UrlPath.GetDomainUrl() + "/Sitefinity/Authenticate/SWT";
             var mvcProxy = new MvcControllerProxy();
             mvcProxy.ControllerName = typeof(LoginStatusController).FullName;
@@ -81,10 +100,12 @@ namespace FeatherWidgets.TestIntegration.Identity.LoginStatus
             loginStatusController.Model.AllowInstantLogin = true;
             mvcProxy.Settings = new ControllerSettings(loginStatusController);
 
-            var actionResult = (ViewResult)loginStatusController.Index();
-            var loginStatusViewModel = actionResult.Model as LoginStatusViewModel;
-            Assert.AreEqual(this.GetExpectedUrlWithParams(absoluteUrl), loginStatusViewModel.LoginPageUrl, "Login redirect url is not as expected");
+            this.pageOperations.CreatePageWithControl(mvcProxy, this.pageNamePrefix, this.pageTitlePrefix, this.urlNamePrefix, this.pageIndex);
+            var responseContent = PageInvoker.ExecuteWebRequest(loginStatusPageUrl);
+            Assert.IsTrue(responseContent.Contains(this.GetExpectedUrlWithParams(absoluteUrl)), "Login redirect url is not as expected");
         }
+
+        #region Private members
 
         /// <summary>
         /// Gets the expected URL with parameters.
@@ -93,12 +114,20 @@ namespace FeatherWidgets.TestIntegration.Identity.LoginStatus
         /// <returns></returns>
         private string GetExpectedUrlWithParams(string redirectUrl)
         {
-            var currentUrl = HttpContext.Current.Request.RawUrl + "&sf-hru=true";
+            var currentUrl = string.Format(CultureInfo.InvariantCulture, "/{0}?sf-hru=true", this.urlNamePrefix + this.pageIndex);
             var realm = UrlPath.GetDomainUrl() + "/";
-            var urlFormat = "{0}?realm={1}&redirect_uri={2}&deflate=true";
-            var urlWithParams = string.Format(CultureInfo.InvariantCulture, urlFormat, redirectUrl, realm, HttpUtility.UrlEncode(currentUrl));
+            string urlFormat = "{0}?realm={1}&amp;redirect_uri={2}&amp;deflate=true";
+            var urlWithParams = urlFormat.Arrange(redirectUrl, realm, HttpUtility.UrlEncode(currentUrl));
 
             return urlWithParams;
         }
+
+        private PagesOperations pageOperations;
+        private string pageNamePrefix = "LoginStatusPage";
+        private string pageTitlePrefix = "Login Status";
+        private string urlNamePrefix = "login-status";
+        private int pageIndex = 1;
+
+        #endregion
     }
 }
