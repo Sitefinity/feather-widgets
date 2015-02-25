f737a43d08f8041ee3897f1340d1fda6377ab949
dzhenko <dzhenko@gmail.com>
2015-02-25 11:43:03 +0200

    First test and algered template

--- Tests/FeatherWidgets.TestIntegration/Identity/LoginStatus/LogoutTests.cs
+++ Tests/FeatherWidgets.TestIntegration/Identity/LoginStatus/LogoutTests.cs
@@ -1,43 +1,43 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Web.Mvc;
-using FeatherWidgets.TestUtilities.CommonOperations;
 using MbUnit.Framework;
 using Telerik.Sitefinity.Frontend.Identity.Mvc.Controllers;
 using Telerik.Sitefinity.Frontend.Identity.Mvc.Models.LoginStatus;
 using Telerik.Sitefinity.Frontend.TestUtilities;
 using Telerik.Sitefinity.Mvc.Proxy;
-using System;
-using Telerik.Sitefinity.Modules.Pages;
 
 namespace FeatherWidgets.TestIntegration.Identity.LoginStatus
 {
     /// <summary>
     /// This is a test class with tests related to logout functionality of LoginStatus widget.
     /// </summary>
-    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1726:UsePreferredTerms", MessageId = "Login")]
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1726:UsePreferredTerms", MessageId = "Logout")]
     [Description("This is a test class with tests related to logout functionality of LoginStatus widget.")]
     [TestFixture]
     public class LogoutTests
     {
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1726:UsePreferredTerms", MessageId = "Login")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1726:UsePreferredTerms", MessageId = "Logout")]
         [Author(FeatherTeams.Team2)]
         [Category(TestCategories.Identity)]
-        [Description("Verify when External login page url is provided, redirect link is constructed correctly.")]
+        [Description("Verify when External logout page url is provided, redirect link is constructed correctly.")]
         [Test]
         public void Logout_RedirectToExternalPage_VerifyLogoutRedirectUrlIsCorrect()
         {
-            var somePage = PageManager.GetManager().GetPageDataList();
-            var expectedLogoutPageId = Guid.NewGuid();
+            string expectedLogoutUrl = "www.telerik.com";
 
             var mvcProxy = new MvcControllerProxy();
             mvcProxy.ControllerName = typeof(LoginStatusController).FullName;
-            var loginStatusController = new LoginStatusController();
-            loginStatusController.Model.LogoutPageId = expectedLogoutPageId;
-            mvcProxy.Settings = new ControllerSettings(loginStatusController);
+            var controller = new LoginStatusController();
+            controller.Model.ExternalLogoutUrl = expectedLogoutUrl;
+            mvcProxy.Settings = new ControllerSettings(controller);
 
-            var actionResult = (ViewResult)loginStatusController.Index();
-            var loginStatusViewModel = actionResult.Model as LoginStatusViewModel;
-            //Assert.AreEqual(expectedLogoutUrl, loginStatusViewModel.LogoutPageUrl, "Login redirect url is not as expected");
+            var actionResult = (ViewResult)controller.Index();
+            var viewModel = actionResult.Model as LoginStatusViewModel;
+
+            Assert.AreEqual(expectedLogoutUrl, viewModel.LogoutPageUrl, "Logout redirect url is not as expected");
         }
+
+
     }
 }
