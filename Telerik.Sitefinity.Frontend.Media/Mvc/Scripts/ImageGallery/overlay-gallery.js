4286b7e1275a01eddbf967b8404069d931cd17f8
Boyko Karadzhov <bokovskii@gmail.com>
2015-02-26 17:52:42 +0200

    thumbnail dynamic url fixes

--- Telerik.Sitefinity.Frontend.Media/Mvc/Scripts/ImageGallery/overlay-gallery.js
+++ Telerik.Sitefinity.Frontend.Media/Mvc/Scripts/ImageGallery/overlay-gallery.js
@@ -1,5 +1,7 @@
-﻿(function ($) {
+﻿; (function ($) {
     $(function () {
+        var pageHref = location.href;
+        var pageTitle = document.title;
         $('.image-link').magnificPopup({
             type: 'image',
             gallery: {
@@ -18,11 +20,27 @@
                 change: function () {
                     var img = this.currItem.el.is('img') ? this.currItem.el : this.currItem.el.find('img');
                     var detailUrl = img.attr('data-detail-url');
-                    if (detailUrl) {
-                        history.pushState('data', img.attr('title'), detailUrl);
+                    if (detailUrl && history.state !== this.currItem.index) {
+                        history.pushState(this.currItem.index, img.attr('alt'), detailUrl);
+                    }
+                },
+                close: function () {
+                    if (pageHref !== location.href) {
+                        history.pushState(null, pageTitle, pageHref);
                     }
                 }
             }
         });
+    });
+
+    window.addEventListener('popstate', function (e) {
+        if (e.state !== undefined && e.state !== null && typeof e.state === 'number') {
+            if (e.state >= 0 && e.state < $.magnificPopup.instance.items.length) {
+                $('.image-link').magnificPopup('open', e.state);
+            }
+        }
+        else {
+            $('.image-link').magnificPopup('close');
+        }
     });
 })(jQuery);
