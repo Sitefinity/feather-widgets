6ce3d0802deb5145411567bea9b9200ad9fa9fa9
ElenaGaneva <Elena.Ganeva@telerik.com>
2015-02-27 18:25:42 +0200

    Added firstName and LastName to viewModel

--- Telerik.Sitefinity.Frontend.Identity/Mvc/Models/Registration/RegistrationViewModel.cs
+++ Telerik.Sitefinity.Frontend.Identity/Mvc/Models/Registration/RegistrationViewModel.cs
@@ -65,5 +65,21 @@ namespace Telerik.Sitefinity.Frontend.Identity.Mvc.Models.Registration
         /// The email.
         /// </value>
         public string Email { get; set; }
+
+        /// <summary>
+        /// Gets or sets the first name.
+        /// </summary>
+        /// <value>
+        /// The first name.
+        /// </value>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the last name.
+        /// </summary>
+        /// <value>
+        /// The last name.
+        /// </value>
+        public string LastName { get; set; }
     }
 }
