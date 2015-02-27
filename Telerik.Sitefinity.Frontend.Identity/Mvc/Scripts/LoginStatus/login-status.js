8894cc10ac86671e4e2e4b9034354e887cde7d6f
ElenaGaneva <Elena.Ganeva@telerik.com>
2015-02-27 11:56:03 +0200

    Fixed recirect

--- Telerik.Sitefinity.Frontend.Identity/Mvc/Scripts/LoginStatus/login-status.js
+++ Telerik.Sitefinity.Frontend.Identity/Mvc/Scripts/LoginStatus/login-status.js
@@ -8,9 +8,12 @@
                     loggedInView.find('.sf-logged-in-name').html(statusViewModel.DisplayName);
                     loggedInView.find('.sf-logged-in-email').html(statusViewModel.Email);
                     loggedInView.find('.sf-logged-in-log-out-btn').on('click', function () {
-                        $.get('Sitefinity/SignOut?sts_signout=true', function (data) {
-                            window.location.href = $('.sf-logout-redirect-url').val() || window.location.href;
-                        });
+                        var redirectUrl = $('.sf-logout-redirect-url').val();
+                        if (redirectUrl.indexOf('&redirect-uri=') < 0){
+                            redirectUrl += '&redirect-uri=' + encodeURIComponent(window.location.href);
+                        }
+                        window.location.href = redirectUrl;
+                        return false;
                     });
 
                     loggedInView.show();
