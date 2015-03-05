e91e9283f172fd4ae4ff9e70c0a27253334f23ce
Boyko Karadzhov <bokovskii@gmail.com>
2015-03-05 17:18:53 +0200

    added a unit test for profile controller

--- Tests/FeatherWidgets.TestUnit/TestInterfaceMappings.cs
+++ Tests/FeatherWidgets.TestUnit/TestInterfaceMappings.cs
@@ -5,6 +5,7 @@ using FeatherWidgets.TestUnit.DummyClasses.SearchResults;
 using Ninject.Modules;
 using Telerik.Sitefinity.Frontend.ContentBlock.Mvc.Models;
 using Telerik.Sitefinity.Frontend.Identity.Mvc.Models.AccountActivation;
+using Telerik.Sitefinity.Frontend.Identity.Mvc.Models.Profile;
 using Telerik.Sitefinity.Frontend.Navigation.Mvc.Models;
 using Telerik.Sitefinity.Frontend.Search.Mvc.Models;
 
@@ -24,6 +25,7 @@ namespace FeatherWidgets.TestUnit
             Bind<INavigationModel>().To<DummyNavigationModel>().When(request => true);
             Bind<ISearchResultsModel>().To<DummySearchResultsModel>().When(request => true);
             Bind<IAccountActivationModel>().To<DummyAccountActivationModel>().When(request => true);
+            Bind<IProfileModel>().To<DummyProfileModel>();
         }
     }
 }
