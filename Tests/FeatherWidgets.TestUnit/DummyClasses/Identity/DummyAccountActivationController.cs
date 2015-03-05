fd627b4247f4356e02478459bcc63a87a4c27c9e
manev <kaloyan.manev@telerik.com>
2015-03-05 13:31:18 +0200

    added missing tests files

---
+++ Tests/FeatherWidgets.TestUnit/DummyClasses/Identity/DummyAccountActivationController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telerik.Sitefinity.Frontend.Identity.Mvc.Controllers;
+
+namespace FeatherWidgets.TestUnit.DummyClasses.Identity
+{
+    public class DummyAccountActivationController : AccountActivationController
+    {
+    }
+}
