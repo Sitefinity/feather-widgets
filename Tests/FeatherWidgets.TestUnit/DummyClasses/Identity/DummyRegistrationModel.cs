cd7b8bcd07a4ed88f79f8a4c3bc04f188516f1f9
Boyko Karadzhov <bokovskii@gmail.com>
2015-03-05 13:56:46 +0200

    added registration controller unit tests

---
+++ Tests/FeatherWidgets.TestUnit/DummyClasses/Identity/DummyRegistrationModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Web.Security;
+using Telerik.Sitefinity.Frontend.Identity.Mvc.Models.Registration;
+
+namespace FeatherWidgets.TestUnit.DummyClasses.Identity
+{
+    /// <summary>
+    /// This class is used for testing the Registration controller.
+    /// </summary>
+    public class DummyRegistrationModel : IRegistrationModel
+    {
+        public Guid? LoginPageId { get; set; }
+
+        public string CssClass { get; set; }
+
+        public string MembershipProviderName { get; set; }
+
+        public string SerializedSelectedRoles { get; set; }
+
+        public SuccessfulRegistrationAction SuccessfulRegistrationAction { get; set; }
+
+        public bool SendEmailOnSuccess { get; set; }
+
+        public string SuccessEmailSubject { get; set; }
+
+        public Guid? SuccessEmailTemplateId { get; set; }
+
+        public string EmailSenderName { get; set; }
+
+        public ActivationMethod ActivationMethod { get; set; }
+
+        public string SuccessfulRegistrationMsg { get; set; }
+
+        public Guid? SuccessfulRegistrationPageId { get; set; }
+
+        public bool SendRegistrationEmail { get; set; }
+
+        public RegistrationViewModel GetViewModel()
+        {
+            return new RegistrationViewModel();
+        }
+
+        public MembershipCreateStatus RegisterUser(RegistrationViewModel model)
+        {
+            if (model.UserName != "Fail")
+                return MembershipCreateStatus.Success;
+            else
+                return MembershipCreateStatus.InvalidUserName;
+        }
+
+        public string ErrorMessage(MembershipCreateStatus status)
+        {
+            return status.ToString();
+        }
+
+        public string GetPageUrl(Guid? pageId)
+        {
+            if (pageId.HasValue)
+            {
+                return "http://" + pageId.Value.ToString("D");
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
