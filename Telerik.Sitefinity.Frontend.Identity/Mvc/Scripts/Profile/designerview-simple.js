a89c85f2af8a07eb579a6de5c5c06164f8572203
manev <kaloyan.manev@telerik.com>
2015-03-09 12:04:06 +0200

    Fixed - Profile: opening designer throws js error #494

--- Telerik.Sitefinity.Frontend.Identity/Mvc/Scripts/Profile/designerview-simple.js
+++ Telerik.Sitefinity.Frontend.Identity/Mvc/Scripts/Profile/designerview-simple.js
@@ -1,3 +1,37 @@
 ﻿(function () {
-    angular.module('designer').requires.push('expander', 'kendo.directives', 'sfSelectors');
+
+    var simpleViewModule = angular.module('simpleViewModule', ['designer']);
+
+    angular.module('designer').requires.push('expander', 'kendo.directives', 'sfSelectors', 'simpleViewModule');
+
+    simpleViewModule.controller('SimpleCtrl', ['$scope', 'propertyService', function ($scope, propertyService) {
+
+        // ------------------------------------------------------------------------
+        // scope variables and set up
+        // ------------------------------------------------------------------------
+
+        $scope.feedback.showLoadingIndicator = true;
+
+        var onGetPropertiesSuccess = function (data) {
+            if (data) {
+                $scope.properties = propertyService.toAssociativeArray(data.Items);
+
+                if ($scope.properties.Mode.PropertyValue === 'EditOnly' &&
+                    $scope.properties.SaveChangesAction.PropertyValue === 'SwitchToReadMode') {
+                    $scope.properties.SaveChangesAction.PropertyValue = 'ShowMessage';
+                }
+            }
+        };
+
+        propertyService.get()
+            .then(onGetPropertiesSuccess)
+            .catch(function (data) {
+                $scope.feedback.showError = true;
+                if (data)
+                    $scope.feedback.errorMessage = data.Detail;
+            })
+            .finally(function () {
+                $scope.feedback.showLoadingIndicator = false;
+            });
+    }]);
 })();
