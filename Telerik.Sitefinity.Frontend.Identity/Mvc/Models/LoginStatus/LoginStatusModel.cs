53f42d2c443ccabb90d9e4a6138de7c836e0701b
Boyko Karadzhov <bokovskii@gmail.com>
2015-03-12 16:06:32 +0200

    workaround issue with app path

--- Telerik.Sitefinity.Frontend.Identity/Mvc/Models/LoginStatus/LoginStatusModel.cs
+++ Telerik.Sitefinity.Frontend.Identity/Mvc/Models/LoginStatus/LoginStatusModel.cs
@@ -100,7 +100,10 @@ namespace Telerik.Sitefinity.Frontend.Identity.Mvc.Models.LoginStatus
                 logoutRedirectUrl = HyperLinkHelpers.GetFullPageUrl(this.LogoutPageId.Value);
             }
 
-            string fullLogoutUrl = UrlPath.ResolveUrl(ClaimsManager.GetLogoutUrl(logoutRedirectUrl), true, true);
+            string fullLogoutUrl = RouteHelper.ResolveUrl(ClaimsManager.GetLogoutUrl(logoutRedirectUrl), UrlResolveOptions.Rooted);
+
+            // Workaround an issue when the application is hosted under an application path.
+            fullLogoutUrl = fullLogoutUrl.Replace("sts_signout=true&", "");
 
             return fullLogoutUrl;
         }
