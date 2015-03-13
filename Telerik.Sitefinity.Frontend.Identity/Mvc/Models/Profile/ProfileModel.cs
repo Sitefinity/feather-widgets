f49d96762fd61b8503a86756a4bb5af5a2aa61f8
dzhenko <dzhenko@gmail.com>
2015-03-13 14:57:29 +0200

    Added validation for password length in validate model method.

--- Telerik.Sitefinity.Frontend.Identity/Mvc/Models/Profile/ProfileModel.cs
+++ Telerik.Sitefinity.Frontend.Identity/Mvc/Models/Profile/ProfileModel.cs
@@ -262,6 +262,20 @@ namespace Telerik.Sitefinity.Frontend.Identity.Mvc.Models.Profile
                     }
                 }
             }
+
+            var minPassLength = UserManager.GetManager(this.MembershipProvider).MinRequiredPasswordLength;
+            if (!string.IsNullOrEmpty(viewModel.OldPassword) && !string.IsNullOrEmpty(viewModel.NewPassword) && !string.IsNullOrEmpty(viewModel.RepeatPassword))
+            {
+                if (viewModel.NewPassword.Length < minPassLength)
+                {
+                    modelState.AddModelError("NewPassword", string.Format(Res.Get<ProfileResources>().MinimumPasswordLength, minPassLength));
+                }
+
+                if (viewModel.RepeatPassword.Length < minPassLength)
+                {
+                    modelState.AddModelError("RepeatPassword", string.Format(Res.Get<ProfileResources>().MinimumPasswordLength, minPassLength));
+                }
+            }
         }
         
         #endregion
