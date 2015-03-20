571cce07816b6bf25ba9fe727206541880049458
VenetaBogoeva <veneta.bogoeva@telerik.com>
2015-03-20 14:50:11 +0200

    Added identity structure for UI tests

---
+++ Tests/Feather.Widgets.TestUI.Framework/Framework/Wrappers/Frontend/Identity/LoginFormWrapper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Feather.Widgets.TestUI.Framework.Framework.Wrappers.Frontend.Identity
+{
+    /// <summary>
+    /// This is the entry point class for login form on the frontend.
+    /// </summary>
+    public class LoginFormWrapper : BaseWrapper
+    {
+    }
+}
