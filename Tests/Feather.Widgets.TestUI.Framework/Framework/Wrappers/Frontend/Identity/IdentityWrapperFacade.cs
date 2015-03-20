e40e76638783aa3795f82a06915c9897b71f9d94
VenetaBogoeva <veneta.bogoeva@telerik.com>
2015-03-20 15:23:26 +0200

    Identity structure for UI tests

--- Tests/Feather.Widgets.TestUI.Framework/Framework/Wrappers/Frontend/Identity/IdentityWrapperFacade.cs
+++ Tests/Feather.Widgets.TestUI.Framework/Framework/Wrappers/Frontend/Identity/IdentityWrapperFacade.cs
@@ -11,5 +11,40 @@ namespace Feather.Widgets.TestUI.Framework.Framework.Wrappers.Frontend.Identity
     /// </summary>
     public class IdentityWrapperFacade
     {
+        /// <summary>
+        /// Provides unified access to the LoginFormWrapper
+        /// </summary>
+        /// <returns>Returns the LoginFormWrapper</returns>
+        public LoginFormWrapper LoginFormWrapper()
+        {
+            return new LoginFormWrapper();
+        }
+
+        /// <summary>
+        /// Provides unified access to the LoginStatusWrapper
+        /// </summary>
+        /// <returns>Returns the LoginStatusWrapper</returns>
+        public LoginStatusWrapper LoginStatusWrapper()
+        {
+            return new LoginStatusWrapper();
+        }
+
+        /// <summary>
+        /// Provides unified access to the ProfileWrapper
+        /// </summary>
+        /// <returns>Returns the ProfileWrapper</returns>
+        public ProfileWrapper ProfileWrapper()
+        {
+            return new ProfileWrapper();
+        }
+
+        /// <summary>
+        /// Provides unified access to the RegistrationWrapper
+        /// </summary>
+        /// <returns>Returns the RegistrationWrapper</returns>
+        public RegistrationWrapper RegistrationWrapper()
+        {
+            return new RegistrationWrapper();
+        }
     }
 }
---
+++ Tests/Feather.Widgets.TestUI.Framework/Framework/Wrappers/Frontend/Identity/RegistrationWrapper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Feather.Widgets.TestUI.Framework.Framework.Wrappers.Frontend.Identity
+{
+    /// <summary>
+    /// This is the entry point class for registration on the frontend.
+    /// </summary>
+    public class RegistrationWrapper : BaseWrapper
+    {
+    }
+}
