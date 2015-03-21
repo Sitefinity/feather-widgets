17ee520809351a5fce738060e9fd3bdeeeaeb681
VenetaBogoeva <veneta.bogoeva@telerik.com>
2015-03-21 16:31:08 +0200

    Added new UI test register new user with administrator role

--- Tests/Feather.Widgets.TestUI.Framework/Framework/ElementMap/Identity/RegistrationFrontend.cs
+++ Tests/Feather.Widgets.TestUI.Framework/Framework/ElementMap/Identity/RegistrationFrontend.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ArtOfTest.WebAii.Controls.HtmlControls;
 using ArtOfTest.WebAii.Core;
 using ArtOfTest.WebAii.TestTemplates;
 
@@ -20,6 +21,83 @@ namespace Feather.Widgets.TestUI.Framework.Framework.ElementMap.Identity
         public RegistrationFrontend(Find find)
             : base(find)
         {
+        }
+
+        /// <summary>
+        /// Gets first name field.
+        /// </summary>
+        public HtmlInputText FirstName
+        {
+            get
+            {
+                return this.Get<HtmlInputText>("tagname=input", "id=Profile_FirstName_");
+            }
+        }
+
+        /// <summary>
+        /// Gets last name field.
+        /// </summary>
+        public HtmlInputText LastName
+        {
+            get
+            {
+                return this.Get<HtmlInputText>("tagname=input", "id=Profile_LastName_");
+            }
+        }
+
+        /// <summary>
+        /// Gets email field.
+        /// </summary>
+        public HtmlInputText Email
+        {
+            get
+            {
+                return this.Get<HtmlInputText>("tagname=input", "id=Email");
+            }
+        }
+
+        /// <summary>
+        /// Gets username field.
+        /// </summary>
+        public HtmlInputText Username
+        {
+            get
+            {
+                return this.Get<HtmlInputText>("tagname=input", "id=UserName");
+            }
+        }
+
+        /// <summary>
+        /// Gets password field.
+        /// </summary>
+        public HtmlInputPassword Password
+        {
+            get
+            {
+                return this.Get<HtmlInputPassword>("tagname=input", "id=Password");
+            }
+        }
+
+        /// <summary>
+        /// Gets re type password field.
+        /// </summary>
+        public HtmlInputPassword ReTypePassword
+        {
+            get
+            {
+                return this.Get<HtmlInputPassword>("tagname=input", "id=ReTypePassword");
+            }
+        }
+
+        /// <summary>
+        /// Gets register button.
+        /// </summary>
+        public HtmlButton RegisterButton
+        {
+            get
+            {
+                return this.Get<HtmlButton>("tagname=button", "InnerText=Register");
+            }
         }
     }
 }
