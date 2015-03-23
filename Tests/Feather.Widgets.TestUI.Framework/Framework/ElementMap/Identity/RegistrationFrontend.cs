45dbc0ca522e5c8ee0f023835d7fd5b93b331f79
VenetaBogoeva <veneta.bogoeva@telerik.com>
2015-03-23 14:12:11 +0200

    Added UI test for validation field - registration widget

--- Tests/Feather.Widgets.TestUI.Framework/Framework/ElementMap/Identity/RegistrationFrontend.cs
+++ Tests/Feather.Widgets.TestUI.Framework/Framework/ElementMap/Identity/RegistrationFrontend.cs
@@ -99,5 +99,82 @@ namespace Feather.Widgets.TestUI.Framework.Framework.ElementMap.Identity
                 return this.Get<HtmlButton>("tagname=button", "InnerText=Register");
             }
         }
+
+        /// <summary>
+        /// Email required field message span.
+        /// </summary>
+        public HtmlSpan EmailRequiredMessage
+        {
+            get
+            {
+                return this.Get<HtmlSpan>("TagName=span", "InnerText=The Email field is required.");
+            }
+        }
+
+        /// <summary>
+        /// Username required field message span.
+        /// </summary>
+        public HtmlSpan UserNameRequiredMessage
+        {
+            get
+            {
+                return this.Get<HtmlSpan>("TagName=span", "InnerText=The Username field is required.");
+            }
+        }
+
+        /// <summary>
+        /// Password required field message span.
+        /// </summary>
+        public HtmlSpan PasswordRequiredMessage
+        {
+            get
+            {
+                return this.Get<HtmlSpan>("TagName=span", "InnerText=The Password field is required.");
+            }
+        }
+
+        /// <summary>
+        /// Password retype field message span.
+        /// </summary>
+        public HtmlSpan PasswordRetypeMessage
+        {
+            get
+            {
+                return this.Get<HtmlSpan>("TagName=span", "InnerText=~'Re-type password' and 'Password' do not match.");
+            }
+        }
+
+        /// <summary>
+        /// Password length message div.
+        /// </summary>
+        public HtmlDiv PasswordLengthMessage
+        {
+            get
+            {
+                return this.Get<HtmlDiv>("TagName=div", "InnerText=Password length minimum: 7. Non-alphanumeric characters required: 0.");
+            }
+        }
+
+        /// <summary>
+        /// Duplicate user name message div.
+        /// </summary>
+        public HtmlDiv DuplicateUserNameMessage
+        {
+            get
+            {
+                return this.Get<HtmlDiv>("TagName=div", "InnerText=Please enter a different user name.");
+            }
+        }
+
+        /// <summary>
+        /// Duplicate email message div.
+        /// </summary>
+        public HtmlDiv DuplicateEmailMessage
+        {
+            get
+            {
+                return this.Get<HtmlDiv>("TagName=div", "InnerText=The email address that you entered is already in use. Please enter a different email address.");
+            }
+        }
     }
 }
