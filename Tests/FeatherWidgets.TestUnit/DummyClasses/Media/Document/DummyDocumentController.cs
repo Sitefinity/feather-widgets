a5a1b42d7020b2d2596f619071efb9344fe5f990
GeorgiMateev <georgi.mateev@telerik.com>
2015-03-23 18:49:43 +0200

    Added unit tests for the DocumentController

---
+++ Tests/FeatherWidgets.TestUnit/DummyClasses/Media/Document/DummyDocumentController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using Telerik.Sitefinity.Frontend.Media.Mvc.Controllers;
+using Telerik.Sitefinity.Frontend.Media.Mvc.Models.Document;
+
+namespace FeatherWidgets.TestUnit.DummyClasses.Media.Document
+{
+    public class DummyDocumentController : DocumentController
+    {
+        private readonly IDocumentModel model;
+
+        public DummyDocumentController(IDocumentModel model)
+        {
+            this.model = model;
+        }
+
+        public override IDocumentModel Model
+        {
+            get
+            {
+                return this.model ?? base.Model;
+            }
+        }
+
+        protected override bool IsDesignMode
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        protected override string DocumentWasNotSelectedOrHasBeenDeletedMessage
+        {
+            get
+            {
+                return "DocumentWasNotSelectedOrHasBeenDeletedMessage";
+            }
+        }
+    }
+}
