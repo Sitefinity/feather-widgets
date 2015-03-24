5d9344f77676a5aea9fae14d97b09d13179e532a
manev <kaloyan.manev@telerik.com>
2015-03-24 15:24:00 +0200

    DocumentsListController tests added

---
+++ Tests/FeatherWidgets.TestUnit/DummyClasses/Media/DocumentsList/DummyDocument.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using Telerik.Sitefinity.Model;
+
+namespace FeatherWidgets.TestUnit.DummyClasses.Media.DocumentsList
+{
+    public class DummyDocument : Telerik.Sitefinity.Libraries.Model.Document
+    {
+        public DummyDocument(string appName, Guid id)
+            : base(appName, id)
+        {
+        }
+
+        public override Lstring Title
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ Tests/FeatherWidgets.TestUnit/DummyClasses/Media/DocumentsList/DummyDocumentsListModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Telerik.Sitefinity.Frontend.Media.Mvc.Models;
+using Telerik.Sitefinity.Frontend.Media.Mvc.Models.DocumentsList;
+using Telerik.Sitefinity.Frontend.Mvc.Models;
+using SfDocument = Telerik.Sitefinity.Libraries.Model.Document;
+
+namespace FeatherWidgets.TestUnit.DummyClasses.Media
+{
+    public class DummyDocumentsListModel : DocumentsListModel, IDocumentsListModel
+    {
+        public ParentFilterMode ParentFilterMode { get; set; }
+
+        public string SerializedSelectedParentsIds { get; set; }
+
+        public override ContentListViewModel CreateListViewModel(Telerik.Sitefinity.Taxonomies.Model.ITaxon taxonFilter, int page)
+        {
+            return new ContentListViewModel();
+        }
+
+        public ContentListViewModel CreateListViewModelByParent(Telerik.Sitefinity.IFolder parentItem, int page)
+        {
+            return new ContentListViewModel();
+        }
+
+        protected override IQueryable<Telerik.Sitefinity.Model.IDataItem> GetItemsQuery()
+        {
+            return new List<SfDocument>().AsQueryable();
+        }
+    }
+}
