657053121ad8b7f0f37ec76e48542f85c2900aca
ElenaGaneva <Elena.Ganeva@telerik.com>
2015-03-25 19:47:58 +0200

    Extracted SearchResults script out of the view

---
+++ Telerik.Sitefinity.Frontend.Search/Mvc/Scripts/SearchResults/Search-results.js
@@ -0,0 +1,29 @@
+﻿$(document).ready(function () {
+
+    //Dropdownlist Selectedchange event
+    $(".userSortDropdown").change(function (value) {
+        var selectedValue = $(value.currentTarget).val();
+        var url = getResultsUrl(selectedValue);
+        window.location.search = url;
+    });
+
+    // Returns url with all needed parameters
+    function getResultsUrl(orderBy, language) {
+        var orderByFieldValue = $('[data-sf-role="searchResOrderBy"]').First().val();
+        var orderByValue = orderBy || orderByFieldValue;
+        var languageFieldValue = $('[data-sf-role="searchResLanguage"]').First().val();
+        var languageValue = language || languageFieldValue;
+
+        var orderByParam = orderByValue ? '&orderBy=' + orderByValue : '';
+        var languageParam = languageValue ? '&language=' + languageValue : '';
+
+        var indexCatalogueParam = $('[data-sf-role="searchResIndexCatalogue"]').First().val();
+        var searchQueryParam = $('[data-sf-role="searchResQuery"]').First().val();
+        var wordsModeParam = $('[data-sf-role="searchResWordsMode"]').First().val();
+        return '?indexCatalogue=' + indexCatalogueParam + '&' +
+            'searchQuery=' + searchQueryParam + '&' +
+            'wordsMode=' + wordsModeParam +
+            orderByParam +
+            languageParam;
+    }
+});
