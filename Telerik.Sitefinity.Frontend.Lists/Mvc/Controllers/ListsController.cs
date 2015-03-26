6661ce959b384d0c66be4665ec7b8cb8826d5b13
manev <kaloyan.manev@telerik.com>
2015-03-26 11:47:28 +0200

    Added lists widget empty project

---
+++ Telerik.Sitefinity.Frontend.Lists/Mvc/Controllers/ListsController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Web.Mvc;
+using Telerik.Sitefinity.Frontend.Lists.Mvc.Models;
+using Telerik.Sitefinity.Frontend.Mvc.Infrastructure.Controllers;
+
+namespace Telerik.Sitefinity.Frontend.Lists.Mvc.Controllers
+{
+    public class ListsController : Controller
+    {
+        #region Properties
+        /// <summary>
+        /// Gets the Lists widget model.
+        /// </summary>
+        /// <value>
+        /// The model.
+        /// </value>
+        [TypeConverter(typeof(ExpandableObjectConverter))]
+        public virtual IListsModel Model
+        {
+            get
+            {
+                if (this.model == null)
+                    this.model = this.InitializeModel();
+
+                return this.model;
+            }
+        }
+
+        #endregion
+
+        #region Private methods
+        private IListsModel InitializeModel()
+        {
+            return ControllerModelFactory.GetModel<IListsModel>(this.GetType());
+        }
+        #endregion
+
+        #region Private fields and constants
+        private IListsModel model;
+        #endregion
+    }
+}
---
+++ Telerik.Sitefinity.Frontend.Lists/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Telerik.Sitefinity.Frontend.Mvc.Infrastructure.Controllers.Attributes;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Telerik.Sitefinity.Frontend.Lists")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6c7eda54-d4cd-4418-bdf9-47a9114686ef")]
+
+[assembly: ControllerContainer]
+
+[assembly: InternalsVisibleTo("FeatherWidgets.TestUnit")]
