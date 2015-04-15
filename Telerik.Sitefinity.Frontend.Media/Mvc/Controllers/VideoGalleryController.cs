f659d92aac993b323b16fcdc6b4b40d84a5a7654
ElenaGaneva <Elena.Ganeva@telerik.com>
2015-04-15 16:45:33 +0300

    Added public properties to the controller

--- Telerik.Sitefinity.Frontend.Media/Mvc/Controllers/VideoGalleryController.cs
+++ Telerik.Sitefinity.Frontend.Media/Mvc/Controllers/VideoGalleryController.cs
@@ -4,10 +4,12 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Web.Mvc;
+using Telerik.Sitefinity.ContentLocations;
 using Telerik.Sitefinity.Frontend.Media.Mvc.Models.VideoGallery;
 using Telerik.Sitefinity.Frontend.Media.Mvc.StringResources;
 using Telerik.Sitefinity.Frontend.Mvc.Infrastructure.Controllers;
 using Telerik.Sitefinity.Frontend.Mvc.Infrastructure.Controllers.Attributes;
+using Telerik.Sitefinity.Libraries.Model;
 using Telerik.Sitefinity.Mvc;
 
 namespace Telerik.Sitefinity.Frontend.Media.Mvc.Controllers
@@ -20,6 +22,84 @@ namespace Telerik.Sitefinity.Frontend.Media.Mvc.Controllers
     public class VideoGalleryController: Controller
     {
         #region Public properties
+
+        /// <summary>
+        /// Gets or sets the name of the template that will be displayed when widget is in List view.
+        /// </summary>
+        /// <value></value>
+        public string ListTemplateName
+        {
+            get
+            {
+                return this.listTemplateName;
+            }
+
+            set
+            {
+                this.listTemplateName = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the name of the template that will be displayed when widget is in Detail view.
+        /// </summary>
+        /// <value></value>
+        public string DetailTemplateName
+        {
+            get
+            {
+                return this.detailTemplateName;
+            }
+
+            set
+            {
+                this.detailTemplateName = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the id of the page where will be displayed details view for selected item.
+        /// </summary>
+        /// <value>The details page id.</value>
+        public Guid DetailsPageId { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the canonical URL tag should be added to the page when the canonical meta tag should be added to the page.
+        /// If the value is not set, the settings from SystemConfig -> ContentLocationsSettings -> DisableCanonicalURLs will be used. 
+        /// </summary>
+        /// <value>The disable canonical URLs.</value>
+        [Browsable(false)]
+        public bool? DisableCanonicalUrlMetaTag
+        {
+            get
+            {
+                return this.disableCanonicalUrlMetaTag;
+            }
+
+            set
+            {
+                this.disableCanonicalUrlMetaTag = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether detail view for video should be opened in the same page.
+        /// </summary>
+        /// <value>
+        /// <c>true</c> if details link should be opened in the same page; otherwise, (if should redirect to custom selected page)<c>false</c>.
+        /// </value>
+        public bool OpenInSamePage
+        {
+            get
+            {
+                return this.openInSamePage;
+            }
+
+            set
+            {
+                this.openInSamePage = value;
+            }
+        }
 
         /// <summary>
         /// Gets the Video gallery widget model.
@@ -42,6 +122,7 @@ namespace Telerik.Sitefinity.Frontend.Media.Mvc.Controllers
         #endregion
 
         #region Actions
+
         #endregion
 
         #region Private methods
@@ -61,7 +142,13 @@ namespace Telerik.Sitefinity.Frontend.Media.Mvc.Controllers
 
         #region Private fields and constants
 
-        private IVideoGalleryModel model;
+        private IVideoGalleryModel model; 
+        private string listTemplateName = "VideoGallery";
+        private string listTemplateNamePrefix = "List.";
+        private string detailTemplateName = "DetailPage";
+        private string detailTemplateNamePrefix = "Detail.";
+        private bool? disableCanonicalUrlMetaTag;
+        private bool openInSamePage = true;
 
         #endregion
     }
