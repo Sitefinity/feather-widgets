cc06e609a98ae884ac35755e1be1717c0b1456fe
dzhenko <dzhenko@gmail.com>
2015-04-15 11:07:18 +0300

    IIFE wrapper in search-results.js added

--- Telerik.Sitefinity.Frontend.Search/Mvc/Scripts/SearchResults/Search-results.js
+++ Telerik.Sitefinity.Frontend.Search/Mvc/Scripts/SearchResults/Search-results.js
@@ -1,29 +1,31 @@
-﻿$(document).ready(function () {
+﻿(function($){
+    $(document).ready(function () {
 
-    //Dropdownlist Selectedchange event
-    $(".userSortDropdown").change(function (value) {
-        var selectedValue = $(value.currentTarget).val();
-        var url = getResultsUrl(selectedValue);
-        window.location.search = url;
+        //Dropdownlist Selectedchange event
+        $(".userSortDropdown").change(function (value) {
+            var selectedValue = $(value.currentTarget).val();
+            var url = getResultsUrl(selectedValue);
+            window.location.search = url;
+        });
+
+        // Returns url with all needed parameters
+        function getResultsUrl(orderBy, language) {
+            var orderByFieldValue = $('[data-sf-role="searchResOrderBy"]').val();
+            var orderByValue = orderBy || orderByFieldValue;
+            var languageFieldValue = $('[data-sf-role="searchResLanguage"]').val();
+            var languageValue = language || languageFieldValue;
+
+            var orderByParam = orderByValue ? '&orderBy=' + orderByValue : '';
+            var languageParam = languageValue ? '&language=' + languageValue : '';
+
+            var indexCatalogueParam = $('[data-sf-role="searchResIndexCatalogue"]').val();
+            var searchQueryParam = $('[data-sf-role="searchResQuery"]').val();
+            var wordsModeParam = $('[data-sf-role="searchResWordsMode"]').val();
+            return '?indexCatalogue=' + indexCatalogueParam + '&' +
+                'searchQuery=' + searchQueryParam + '&' +
+                'wordsMode=' + wordsModeParam +
+                orderByParam +
+                languageParam;
+        }
     });
-
-    // Returns url with all needed parameters
-    function getResultsUrl(orderBy, language) {
-        var orderByFieldValue = $('[data-sf-role="searchResOrderBy"]').val();
-        var orderByValue = orderBy || orderByFieldValue;
-        var languageFieldValue = $('[data-sf-role="searchResLanguage"]').val();
-        var languageValue = language || languageFieldValue;
-
-        var orderByParam = orderByValue ? '&orderBy=' + orderByValue : '';
-        var languageParam = languageValue ? '&language=' + languageValue : '';
-
-        var indexCatalogueParam = $('[data-sf-role="searchResIndexCatalogue"]').val();
-        var searchQueryParam = $('[data-sf-role="searchResQuery"]').val();
-        var wordsModeParam = $('[data-sf-role="searchResWordsMode"]').val();
-        return '?indexCatalogue=' + indexCatalogueParam + '&' +
-            'searchQuery=' + searchQueryParam + '&' +
-            'wordsMode=' + wordsModeParam +
-            orderByParam +
-            languageParam;
-    }
-});
+}(jQuery));
