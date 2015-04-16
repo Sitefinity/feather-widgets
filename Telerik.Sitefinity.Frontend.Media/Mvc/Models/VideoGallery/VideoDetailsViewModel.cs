2b9ae5c6aea32e2a8e1b6bde125a9634f22169ae
ElenaGaneva <Elena.Ganeva@telerik.com>
2015-04-16 11:33:24 +0300

    Added initial Video gallery model

---
+++ Telerik.Sitefinity.Frontend.Media/Mvc/Models/VideoGallery/VideoDetailsViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Telerik.Sitefinity.Frontend.Mvc.Models;
+
+namespace Telerik.Sitefinity.Frontend.Media.Mvc.Models.VideoGallery
+{
+    /// <summary>
+    /// This class represents view model for Video gallery content.
+    /// </summary>
+    public class VideoDetailsViewModel : ContentDetailsViewModel
+    {
+        /// <summary>
+        /// Gets or sets the previous item.
+        /// </summary>
+        /// <value>The previous item.</value>
+        public ItemViewModel PreviousItem { get; set; }
+
+        /// <summary>
+        /// Gets or sets the next item.
+        /// </summary>
+        /// <value>The next item.</value>
+        public ItemViewModel NextItem { get; set; }
+
+        /// <summary>
+        /// Gets or sets the total items count.
+        /// </summary>
+        /// <value>The total items count.</value>
+        public int TotalItemsCount { get; set; }
+
+        /// <summary>
+        /// Gets or sets the URL of the video.
+        /// </summary>
+        /// <value>The thumbnail URL.</value>
+        public string MediaUrl { get; set; }
+    }
+}
--- Telerik.Sitefinity.Frontend.Media/Mvc/Models/VideoGallery/VideoGalleryModel.cs
+++ Telerik.Sitefinity.Frontend.Media/Mvc/Models/VideoGallery/VideoGalleryModel.cs
@@ -2,6 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Telerik.Sitefinity.Configuration;
+using Telerik.Sitefinity.Frontend.Mvc.Models;
+using Telerik.Sitefinity.Model;
+using Telerik.Sitefinity.Modules.Libraries;
+using Telerik.Sitefinity.Services;
 using SfVideo = Telerik.Sitefinity.Libraries.Model.Video;
 
 namespace Telerik.Sitefinity.Frontend.Media.Mvc.Models.VideoGallery
@@ -11,10 +16,83 @@ namespace Telerik.Sitefinity.Frontend.Media.Mvc.Models.VideoGallery
     /// </summary>
     public class VideoGalleryModel : MediaGalleryModelBase<SfVideo>, IVideoGalleryModel
     {
-        protected override IQueryable<Model.IDataItem> GetItemsQuery()
+            #region Initialization
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="VideoGalleryModel"/> class.
+        /// </summary>
+        public VideoGalleryModel() : base()
         {
-            throw new NotImplementedException();
         }
+
+        #endregion
+
+        #region Protected overriden methods
+        /// <inheritdoc />
+        protected override IQueryable<IDataItem> GetItemsQuery()
+        {
+            var manager = (LibrariesManager)this.GetManager();
+            return manager.GetVideos();
+        }
+
+
+        /// <inheritdoc />
+        protected override Frontend.Mvc.Models.ContentDetailsViewModel CreateDetailsViewModelInstance()
+        {
+            return new VideoDetailsViewModel();
+        }
+
+        /// <inheritdoc />
+        public Frontend.Mvc.Models.ContentDetailsViewModel CreateDetailsViewModel(IDataItem item, int? itemIndex)
+        {
+            var viewModel = (VideoDetailsViewModel) base.CreateDetailsViewModel(item);
+
+            int? totalCount = 0;
+            IDataItem next = null;
+            IDataItem prev = null;
+
+            if (itemIndex != null)
+            {
+                var query = this.GetItemsQuery();
+                int? take = this.DisplayMode == ListDisplayMode.Limit ? this.ItemsPerPage : null;
+                
+                query = this.UpdateExpression(query, null, take, ref totalCount);
+
+                if (this.DisplayMode == ListDisplayMode.Limit && this.ItemsPerPage < totalCount)
+                {
+                    totalCount = this.ItemsPerPage;
+                }
+
+                if (totalCount > 1)
+                {
+                    if (itemIndex == 1)
+                    {
+                        next = query.Skip(itemIndex.Value).FirstOrDefault();
+                        prev = query.LastOrDefault();
+                    }
+                    else if (itemIndex == totalCount)
+                    {
+                        next = query.FirstOrDefault();
+                        prev = query.Skip(itemIndex.Value - 2).FirstOrDefault();
+                    }
+                    else
+                    {
+                        next = query.Skip(itemIndex.Value).FirstOrDefault();
+                        prev = query.Skip(itemIndex.Value - 2).FirstOrDefault();
+                    }
+                }
+            }
+
+            viewModel.PreviousItem = prev != null ? new ItemViewModel(prev) : null;
+            viewModel.NextItem = next != null ? new ItemViewModel(next) : null;
+            viewModel.TotalItemsCount = totalCount.Value;
+
+            //viewModel.MediaUrl = this.GetSelectedSizeUrl((SfVideo)item, this.VideoSizeModel);
+
+            return viewModel;
+        }
+
+        #endregion
     }
 }
 
