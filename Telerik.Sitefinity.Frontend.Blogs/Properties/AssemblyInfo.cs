f25da11d346e1dded6b0b8f955a18a95374d22f1
ElenaGaneva <Elena.Ganeva@telerik.com>
2015-04-20 18:18:38 +0300

    Fixed wrong assembly named

--- Telerik.Sitefinity.Frontend.Blogs/Properties/AssemblyInfo.cs
+++ Telerik.Sitefinity.Frontend.Blogs/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using Telerik.Sitefinity.Frontend.Mvc.Infrastructure.Controllers.Attributes;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Telerik.Sitefinity.Frontend.ContentBlock")]
+[assembly: AssemblyTitle("Telerik.Sitefinity.Frontend.Blogs")]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("a6c78f16-8226-49bd-ac07-91c917209d89")]
