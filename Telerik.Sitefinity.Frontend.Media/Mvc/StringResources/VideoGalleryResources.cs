0e9141fd48deb9336c4a376ba4b6f5d9bea34dab
ElenaGaneva <Elena.Ganeva@telerik.com>
2015-04-20 15:00:12 +0300

    Added string resources for list settings view

--- Telerik.Sitefinity.Frontend.Media/Mvc/StringResources/VideoGalleryResources.cs
+++ Telerik.Sitefinity.Frontend.Media/Mvc/StringResources/VideoGalleryResources.cs
@@ -175,5 +175,224 @@ namespace Telerik.Sitefinity.Frontend.Media.Mvc.StringResources
                 return this["CssClasses"];
             }
         }
+
+        /// <summary>
+        /// phrase: List settings
+        /// </summary>
+        /// <value>List settings</value>
+        [ResourceEntry("ListSettings",
+            Value = "List settings",
+            Description = "phrase: List settings",
+            LastModified = "2015/04/20")]
+        public string ListSettings
+        {
+            get
+            {
+                return this["ListSettings"];
+            }
+        }
+
+        /// <summary>
+        /// Phrase: Use paging
+        /// </summary>
+        /// <value>Use paging</value>
+        [ResourceEntry("UsePaging",
+            Value = "Use paging",
+            Description = "Phrase: Use paging",
+            LastModified = "2015/04/20")]
+        public string UsePaging
+        {
+            get
+            {
+                return this["UsePaging"];
+            }
+        }
+
+        /// <summary>
+        /// Phrase: Divide the list on pages up to {0} items per page
+        /// </summary>
+        /// <value>Divide the list on pages up to {0} items per page</value>
+        [ResourceEntry("DivideToItemPerPage",
+            Value = "Divide the list on pages up to {0} items per page",
+            Description = "Phrase: Divide the list on pages up to {0} items per page",
+            LastModified = "2015/04/20")]
+        public string DivideToItemPerPage
+        {
+            get
+            {
+                return this["DivideToItemPerPage"];
+            }
+        }
+
+        /// <summary>
+        /// Phrase: Show only limited number of items {0} items in total
+        /// </summary>
+        /// <value>Show only limited number of items {0} items in total</value>
+        [ResourceEntry("ShowLimitedItems",
+            Value = "Show only limited number of items {0} items in total",
+            Description = "Phrase: Show only limited number of items {0} items in total",
+            LastModified = "2015/04/20")]
+        public string ShowLimitedItems
+        {
+            get
+            {
+                return this["ShowLimitedItems"];
+            }
+        }
+
+        /// <summary>
+        /// Phrase: No limit and paging
+        /// </summary>
+        /// <value>No limit and paging</value>
+        [ResourceEntry("NoLimitPaging",
+            Value = "No limit and paging",
+            Description = "Phrase: No limit and paging",
+            LastModified = "2015/04/20")]
+        public string NoLimitPaging
+        {
+            get
+            {
+                return this["NoLimitPaging"];
+            }
+        }
+
+        /// <summary>
+        /// Phrase: Show all published items at once
+        /// </summary>
+        /// <value>Show all published items at once</value>
+        [ResourceEntry("ShowAllItems",
+            Value = "Show all published items at once",
+            Description = "Phrase: Show all published items at once",
+            LastModified = "2015/04/20")]
+        public string ShowAllItems
+        {
+            get
+            {
+                return this["ShowAllItems"];
+            }
+        }
+
+        /// <summary>
+        /// Phrase: Sort videos
+        /// </summary>
+        /// <value>Sort videos</value>
+        [ResourceEntry("SortItems",
+            Value = "Sort videos",
+            Description = "Phrase: Sort videos",
+            LastModified = "2015/04/20")]
+        public string SortItems
+        {
+            get
+            {
+                return this["SortItems"];
+            }
+        }
+
+        /// <summary>
+        /// Gets phrase : Last published
+        /// </summary>
+        [ResourceEntry("LastPublished",
+            Value = "Last published",
+            Description = "phrase : Last published",
+            LastModified = "2015/04/20")]
+        public string LastPublished
+        {
+            get
+            {
+                return this["LastPublished"];
+            }
+        }
+
+        /// <summary>
+        /// Gets phrase : Last modified
+        /// </summary>
+        [ResourceEntry("LastModified",
+            Value = "Last modified",
+            Description = "phrase : Last modified",
+            LastModified = "2015/04/20")]
+        public string LastModified
+        {
+            get
+            {
+                return this["LastModified"];
+            }
+        }
+
+        /// <summary>
+        /// Gets phrase : By Title (A-Z)
+        /// </summary>
+        [ResourceEntry("ByTitleAZ",
+            Value = "By Title (A-Z)",
+            Description = "phrase : By Title (A-Z)",
+            LastModified = "2015/04/20")]
+        public string ByTitleAZ
+        {
+            get
+            {
+                return this["ByTitleAZ"];
+            }
+        }
+
+        /// <summary>
+        /// Gets phrase : By Title (Z-A)
+        /// </summary>
+        [ResourceEntry("ByTitleZA",
+            Value = "By Title (Z-A)",
+            Description = "phrase : By Title (Z-A)",
+            LastModified = "2015/04/20")]
+        public string ByTitleZA
+        {
+            get
+            {
+                return this["ByTitleZA"];
+            }
+        }
+
+        /// <summary>
+        /// phrase: As set manually
+        /// </summary>
+        /// <value>As set manually</value>
+        [ResourceEntry("AsSetManually",
+            Value = "As set manually",
+            Description = "phrase: As set manually",
+            LastModified = "2015/04/20")]
+        public string AsSetManually
+        {
+            get
+            {
+                return this["AsSetManually"];
+            }
+        }
+
+        /// <summary>
+        /// phrase: As set in Advanced mode
+        /// </summary>
+        /// <value>As set in Advanced mode</value>
+        [ResourceEntry("AsSetInAdvancedMode",
+            Value = "As set in Advanced mode",
+            Description = "phrase: As set in Advanced mode",
+            LastModified = "2015/04/20")]
+        public string AsSetInAdvancedMode
+        {
+            get
+            {
+                return this["AsSetInAdvancedMode"];
+            }
+        }
+
+        /// <summary>
+        /// Gets phrase : List template
+        /// </summary>
+        [ResourceEntry("ListTemplate",
+            Value = "List template",
+            Description = "phrase : List template",
+            LastModified = "2015/04/20")]
+        public string ListTemplate
+        {
+            get
+            {
+                return this["ListTemplate"];
+            }
+        }
     }
 }
