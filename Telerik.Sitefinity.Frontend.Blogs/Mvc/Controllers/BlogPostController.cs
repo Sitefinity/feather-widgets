f12e2a038c13eff0e7283ec92f040f2a1bb0bff5
Tihomir Petrov <tihomir.petrov@telerik.com>
2015-04-20 16:47:46 +0300

    Initial commit of BlogPostController.

---
+++ Telerik.Sitefinity.Frontend.Blogs/Mvc/Controllers/BlogPostController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using Telerik.Sitefinity.Mvc;
+
+namespace Telerik.Sitefinity.Frontend.Blogs.Mvc.Controllers
+{
+    /// <summary>
+    /// This class represents the controller of the Blog post widget.
+    /// </summary>
+    [ControllerToolboxItem(Name = "BlogPost", Title = "Blog post", SectionName = "MvcWidgets", ModuleName = "Blogs", CssClass = BlogPostController.WidgetIconCssClass)]
+    public class BlogPostController: Controller
+    {
+        internal const string WidgetIconCssClass = "sfBlogsViewIcn";
+    }
+}
