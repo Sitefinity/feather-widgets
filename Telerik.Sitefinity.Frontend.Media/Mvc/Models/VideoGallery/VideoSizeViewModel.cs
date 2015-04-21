c822c8f38072031e4b303bea08cebbea91dc3dc0
dzhenko <dzhenko@gmail.com>
2015-04-21 11:13:33 +0300

    Done.

---
+++ Telerik.Sitefinity.Frontend.Media/Mvc/Models/VideoGallery/VideoSizeViewModel.cs
@@ -0,0 +1,32 @@
+﻿namespace Telerik.Sitefinity.Frontend.Media.Mvc.Models.VideoGallery
+{
+    /// <summary>
+    /// This class represents the selected size and aspect ratio of a single selected video in the video gallery widget.
+    /// </summary>
+    public class VideoSizeViewModel
+    {
+        /// <summary>
+        /// Gets or sets the width.
+        /// </summary>
+        /// <value>
+        /// The width.
+        /// </value>
+        public int Width { get; set; }
+
+        /// <summary>
+        /// Gets or sets the height.
+        /// </summary>
+        /// <value>
+        /// The height.
+        /// </value>
+        public int Height { get; set; }
+
+        /// <summary>
+        /// Gets or sets the aspect ratio.
+        /// </summary>
+        /// <value>
+        /// The aspect ratio.
+        /// </value>
+        public string AspectRatio { get; set; }
+    }
+}
