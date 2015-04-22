2fcab4ac5812154f52ebeff6a07c054133e403b3
Tihomir Petrov <tihomir.petrov@telerik.com>
2015-04-22 10:19:32 +0300

    Initial commit of model classes/interfaces.

---
+++ Telerik.Sitefinity.Frontend.Blogs/Mvc/Models/BlogModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Telerik.Sitefinity.Frontend.Blogs.Mvc.Models
+{
+    /// <summary>
+    /// Provides API for working with <see cref="Telerik.Sitefinity.Blogs.Model.Blog"/> items.
+    /// </summary>
+    public class BlogModel: IBlogModel
+    {
+    }
+}
---
+++ Telerik.Sitefinity.Frontend.Blogs/Mvc/Models/IBlogModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Telerik.Sitefinity.Frontend.Blogs.Mvc.Models
+{
+    /// <summary>
+    /// This interface defines API for working with <see cref="Telerik.Sitefinity.Blogs.Model.Blog"/> items.
+    /// </summary>
+    public interface IBlogModel
+    {
+    }
+}
