7292b2f3407d4a5f62cfc731c0442eecf41aa92a
ElenaGaneva <Elena.Ganeva@telerik.com>
2015-04-23 17:04:04 +0300

    Added Last Post date field to Blog widget frontend

---
+++ Telerik.Sitefinity.Frontend.Blogs/Mvc/Helpers/BlogPostHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Telerik.Sitefinity.Blogs.Model;
+using Telerik.Sitefinity.Frontend.Mvc.Models;
+using Telerik.Sitefinity.Modules.Blogs;
+
+namespace Telerik.Sitefinity.Frontend.Blogs.Mvc.Helpers
+{
+    /// <summary>
+    /// Provides helpers for working with blogs.
+    /// </summary>
+    public static class BlogPostHelper
+    {
+        /// <summary>
+        /// Gets the last post date.
+        /// </summary>
+        /// <param name="item">The item.</param>
+        /// <returns></returns>
+        public static DateTime? GetLastPostDate(this ItemViewModel item)
+        {
+            var blog = item.DataItem as Blog;
+
+            if(blog == null)
+                return null;
+
+            var lastPostDate = blog.BlogPosts().Max(bp => bp.PublicationDate);
+
+            return lastPostDate;
+        }
+    }
+}
