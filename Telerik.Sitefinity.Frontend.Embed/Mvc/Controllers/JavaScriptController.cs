4c9c2a4befb7ad9ae8d9c922c318d0feadefc373
GeorgiMateev <georgi.mateev@telerik.com>
2015-04-27 15:13:53 +0300

    Added InlineClientAssets project

---
+++ Telerik.Sitefinity.Frontend.Embed/Mvc/Controllers/JavaScriptController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Telerik.Sitefinity.Frontend.InlineClientAssets.Mvc.Controllers
+{
+    public class JavaScriptController : Controller
+    {
+
+    }
+}
