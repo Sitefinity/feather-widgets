1502650b9f66739ab11573c882e7c761c8a59bf4
Miroslava <miroslava.n.nikolova@gmail.com>
2015-04-28 18:16:37 +0300

    remove test steps

--- Tests/FeatherWidgets.TestUI.Arrangements/Lists/SimpleListTemplate.cs
+++ Tests/FeatherWidgets.TestUI.Arrangements/Lists/SimpleListTemplate.cs
@@ -28,10 +28,6 @@ namespace FeatherWidgets.TestUI.Arrangements
             ServerOperationsFeather.ListsOperations().CreateListItem(Guid.NewGuid(), this.listId, ListItem1Title, ListItem1Content);
             ServerOperationsFeather.ListsOperations().CreateListItem(Guid.NewGuid(), this.listId, ListItem2Title, ListItem2Content);
 
-            ServerOperationsFeather.ListsOperations().CreateList(this.secondListId, SecondListTitle, SecondListDescription);
-            ServerOperationsFeather.ListsOperations().CreateListItem(Guid.NewGuid(), this.secondListId, SecondListItem1Title, SecondListItem1Content);
-            ServerOperationsFeather.ListsOperations().CreateListItem(Guid.NewGuid(), this.secondListId, SecondListItem2Title, SecondListItem2Content);
-
             Guid pageId = ServerOperations.Pages().CreatePage(PageName);
             ServerOperationsFeather.Pages().AddListsWidgetToPage(pageId);
         }
@@ -44,7 +40,6 @@ namespace FeatherWidgets.TestUI.Arrangements
         {
             ServerOperations.Pages().DeleteAllPages();
             ServerOperationsFeather.ListsOperations().DeleteList(this.listId);
-            ServerOperationsFeather.ListsOperations().DeleteList(this.secondListId);
         }
 
         private const string PageName = "TestPage";
@@ -56,13 +51,5 @@ namespace FeatherWidgets.TestUI.Arrangements
         private const string ListItem2Title = "list item 2";
         private const string ListItem2Content = "list content 2";
         private readonly Guid listId = new Guid("0D3937D3-A690-4F19-9DA4-53F0880F5B62");
-
-        private const string SecondListTitle = "Second list";
-        private const string SecondListDescription = "second list description";
-        private const string SecondListItem1Title = "second list item 1";
-        private const string SecondListItem1Content = "second list content 1";
-        private const string SecondListItem2Title = "second list item 2";
-        private const string SecondListItem2Content = "second list content 2";
-        private readonly Guid secondListId = new Guid("FE7F956E-FE4F-49E2-897E-6145D432C318");
     }
 }
