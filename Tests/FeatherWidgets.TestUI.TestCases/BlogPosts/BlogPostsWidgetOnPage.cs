4e52dc7c287f9b4614bc249cccc56463c4012739
PetyaRachina <petya.rachina@telerik.com>
2015-05-05 11:30:35 +0300

    Added UI test for Blog posts widget
    UI test : AddBlogPostsWidgetToPageOpenSingleItemInExistingPage

---
+++ Tests/FeatherWidgets.TestUI.TestCases/BlogPosts/BlogPostsWidgetOnPage.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Feather.Widgets.TestUI.Framework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Telerik.Sitefinity.Frontend.TestUtilities;
+
+namespace FeatherWidgets.TestUI.TestCases.BlogPosts
+{
+    /// <summary>
+    /// Test class with tests related to blog posts widget on page.
+    /// </summary>
+    [TestClass]
+    public class BlogPostsWidgetOnPage : FeatherTestCase
+    {
+        /// <summary>
+        /// Drags and drops blog posts widget to page, edits the widget, select to open single item in existing page
+        /// and verifies on the frontend.
+        /// </summary>
+        [TestMethod,
+        Owner(FeatherTeams.Team2),
+        TestCategory(FeatherTestCategories.PagesAndContent),
+        TestCategory(FeatherTestCategories.BlogPosts)]
+        public void AddBlogPostsWidgetToPageOpenSingleItemInExistingPage()
+        {
+            BAT.Macros().NavigateTo().Pages();
+            BAT.Wrappers().Backend().Pages().PagesWrapper().OpenPageZoneEditor(PageTitle);
+            BATFeather.Wrappers().Backend().Pages().PageZoneEditorWrapper().AddWidgetToPlaceHolderPureMvcMode(WidgetName);
+            BATFeather.Wrappers().Backend().Pages().PageZoneEditorWrapper().EditWidget(WidgetName);
+
+            BATFeather.Wrappers().Backend().Widgets().WidgetDesignerWrapper().SwitchToSingleItemSettingsTab();
+            BATFeather.Wrappers().Backend().Widgets().WidgetDesignerWrapper().SelectExistingPage();
+            BATFeather.Wrappers().Backend().Widgets().WidgetDesignerWrapper().ClickSelectButton();
+            BATFeather.Wrappers().Backend().Widgets().SelectorsWrapper().SelectItemsInHierarchicalSelector(SingleItemPage);
+            BATFeather.Wrappers().Backend().Widgets().SelectorsWrapper().DoneSelecting();
+            BATFeather.Wrappers().Backend().Widgets().WidgetDesignerWrapper().VerifySelectedItemsFromHierarchicalSelector(this.selectedItemsFromPageSelector);
+            BATFeather.Wrappers().Backend().Widgets().WidgetDesignerWrapper().SaveChanges();
+
+            BAT.Wrappers().Backend().Pages().PageZoneEditorWrapper().PublishPage();
+            BAT.Macros().NavigateTo().CustomPage("~/" + PageTitle.ToLower());
+
+            BATFeather.Wrappers().Frontend().CommonWrapper().VerifySelectedAnchorLink(PostTitle, this.expectedUrl);
+        }
+
+        /// <summary>
+        /// Performs Server Setup and prepare the system with needed data.
+        /// </summary>
+        protected override void ServerSetup()
+        {
+            BAT.Macros().User().EnsureAdminLoggedIn();
+            BAT.Arrange(this.TestName).ExecuteSetUp();
+        }
+
+        /// <summary>
+        /// Performs clean up and clears all data created by the test.
+        /// </summary>
+        protected override void ServerCleanup()
+        {
+            BAT.Arrange(this.TestName).ExecuteTearDown();
+        }
+
+        private const string PageTitle = "TestPageWithBlogPostsWidget";
+        private const string WidgetName = "Blog posts";
+        private const string SingleItemPage = "DetailPage";
+        private readonly string[] selectedItemsFromPageSelector = new string[] { "DetailPage" };
+        private const string PostTitle = "post1";
+        private readonly string expectedUrl = string.Format("/DetailPage/TestBlog/{0}/{1:00}/{2:00}/post1", DateTime.UtcNow.Year, DateTime.UtcNow.Month, DateTime.UtcNow.Day);
+    }
+}
