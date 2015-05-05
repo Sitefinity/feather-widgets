f4db9d66e2bc3aaa108de5dbf28ae834092ca6ab
dzhenko <dzhenko@gmail.com>
2015-05-05 11:43:16 +0300

    Multiple widgets on page handled

--- Telerik.Sitefinity.Frontend.Comments/Mvc/Scritps/Comments/comments.js
+++ Telerik.Sitefinity.Frontend.Comments/Mvc/Scritps/Comments/comments.js
@@ -1,20 +1,5 @@
 ﻿; (function ($) {
     $(function () {
-        var template = $($('[data-sf-role="single-comment-template"]').html());
-        var commentsContainer = $('[data-sf-role="comments-holder"]');
-
-        // Initially hide new comment form
-        var newCommentForm = $('[data-sf-role="comments-new-holder"]').hide();
-
-        var commentsThreadKey = $('[data-sf-role="comments-thread-key"]').val();
-        var commentsPerPage = $('[data-sf-role="comments-page-size"]').val();
-        var commentsTakenSoFar = 0;
-        var commentsSortedDescending = false;
-
-        /*
-            Service calls
-        */
-
         var makeAjax = function (url, type) {
             type = type || 'GET';
 
@@ -37,8 +22,8 @@
             return makeAjax('/RestApi/session/is-authenticated?_=' + getRandom());
         };
 
-        var getComments = function (skip, take, sortDescending) {
-            var urlParams = 'ThreadKey=' + commentsThreadKey + '&Take=' + take;
+        var getComments = function (threadKey, skip, take, sortDescending) {
+            var urlParams = 'ThreadKey=' + threadKey + '&Take=' + take;
 
             if (skip && skip > 0) {
                 urlParams = urlParams + '&Skip=' + skip;
@@ -55,91 +40,133 @@
             // Service call
         };
 
-        /*
-            Load comments
-        */
-
-        var clearComments = function () {
-            commentsContainer.html('');
-        };
-
-        var createComment = function (comment) {
-            var newComment = template.clone(true);
-
-            newComment.find('[data-sf-role="comment-name"]').text(comment.Name);
-            newComment.find('[data-sf-role="comment-date"]').text(comment.Date);
-            newComment.find('[data-sf-role="comment-text"]').text(comment.Text);
-
-            commentsContainer.append(newComment);
-        };
-
-        var showComments = function (comments) {
-            if (comments && comments.length) {
-                $('[data-sf-role="comments-header"]').text(comments.length + $('[data-sf-role="comments-header-text"]').val());
-                comments.forEach(createComment);
-            }
-            else {
-                $('[data-sf-role="comments-header"]').text($('[data-sf-role="comments-form"]').hide().text());
-                newCommentForm.show();
-            }
-        };
-
-        var loadComments = function (skip, take, sortDescending) {
-            take = take || commentsPerPage;
-
-            getComments(commentsTakenSoFar, take).then(function (response) {
-                response = response || {};
-                response.Items = response.Items || [];
-
-                commentsTakenSoFar = commentsTakenSoFar + response.Items.length;
-                showComments(response.Items);
-            });
-        };
-
-        $('[data-sf-role="comments-load-more"]').click(function () {
-            loadComments(commentsTakenSoFar);
-        });
-        
-        // Initial loading of comments
-        loadComments();
-
-        /*
-            Sort comments
-        */
-
-        $('[data-sf-role="comments-sort-new"]').click(function () {
-            if (commentsSortedDescending === false) {
-                commentsSortedDescending = true;
-                clearComments();
-                loadComments(0, commentsTakenSoFar, true);
-            }
-        });
-
-        $('[data-sf-role="comments-sort-old"]').click(function () {
-            if (commentsSortedDescending === true) {
-                commentsSortedDescending = false;
-                clearComments();
-                loadComments(0, commentsTakenSoFar, false);
-            }
-        });
-
-        /*
-            New comment
-        */
-
-        // Remove unneeded fields in the form if user is logged in
+        // Remove unneeded fields from all forms if user is logged in
         getIsUserLoggedIn().then(function (response) {
             if (!(response && response.IsAuthenticated)) {
                 $('[data-sf-role="comments-new-logged-out-view"]').hide();
-            }
+            };
         });
 
-        $('[data-sf-role="comments-form"]').click(function () {
-            newCommentForm.toggle('slow');
-        });
+        var createWidget = function () {
+            var $this = $(this);
 
-        $('[data-sf-role="comments-new-submit"]').click(function () {
-            // service call
-        });
+            var template = $($this.find('[data-sf-role="single-comment-template"]').html());
+            var commentsContainer = $this.find('[data-sf-role="comments-holder"]');
+
+            var commentsHeader = $this.find('[data-sf-role="comments-header"]');
+            var commentsHeaderText = $this.find('[data-sf-role="comments-header-text"]').val();
+            var commentsFormButton = $this.find('[data-sf-role="comments-form-button"]');
+
+            // Initially hide new comment form
+            var newCommentForm = $this.find('[data-sf-role="comments-new-holder"]').hide();
+
+            var commentsThreadKey = $this.find('[data-sf-role="comments-thread-key"]').val();
+            var commentsPerPage = $this.find('[data-sf-role="comments-page-size"]').val();
+            var commentsTakenSoFar = 0;
+            var commentsSortedDescending = false;
+
+            var commentsTextMaxLength = $this.find('[data-sf-role="comments-text-max-length"]').val();
+            var commentsReadMoreText = $this.find('[data-sf-role="comments-read-full-comment"]').val();
+
+            /*
+                Load comments
+            */
+
+            var attachText = function (element, text) {
+                if (element && text) {
+                    if (text.length < commentsTextMaxLength) {
+                        element.text(text);
+                    }
+                    else {
+                        element.text(text.substr(0, commentsTextMaxLength));
+                        element.append($('<span />').hide().text(text.substr(commentsTextMaxLength)));
+                        element.append($('<button data-sf-role="comments-read-full-comment-button" />').text(commentsReadMoreText));
+                    }
+                }
+            };
+
+            var createComment = function (comment) {
+                var newComment = template.clone(true);
+
+                newComment.find('[data-sf-role="comment-name"]').text(comment.Name);
+                newComment.find('[data-sf-role="comment-date"]').text(comment.Date);
+
+                attachText(newComment.find('[data-sf-role="comment-text"]'), comment.Text);
+
+                commentsContainer.append(newComment);
+            };
+
+            var showComments = function (comments) {
+                if (comments && comments.length) {
+                    commentsHeader.text(comments.length + commentsHeaderText);
+                    comments.forEach(createComment);
+                }
+                else {
+                    commentsFormButton.hide();
+                    commentsHeader.text(commentsFormButton.text());
+                    newCommentForm.show();
+                }
+            };
+
+            var loadComments = function (skip, take, sortDescending) {
+                take = take || commentsPerPage;
+                skip = skip || commentsTakenSoFar;
+
+                getComments(commentsThreadKey, commentsTakenSoFar, take).then(function (response) {
+                    response = response || {};
+                    response.Items = response.Items || [];
+
+                    commentsTakenSoFar = commentsTakenSoFar + response.Items.length;
+                    showComments(response.Items);
+                });
+            };
+
+            // Initial loading of comments
+            loadComments();
+
+            $this.find('[data-sf-role="comments-load-more-button"]').click(loadComments);
+
+            // Read more comments
+            commentsContainer.on('click', '[data-sf-role="comments-read-full-comment-button"]', function (e) {
+                if (e && e.target) {
+                    $(e.target).hide().siblings().show();
+                }
+            });
+
+            /*
+                Sort comments
+            */
+
+            var sortComments = function (descending) {
+                if (descending !== commentsSortedDescending) {
+                    commentsSortedDescending = !commentsSortedDescending;
+                    commentsContainer.html('');
+                    loadComments(0, commentsTakenSoFar, descending);
+                }
+            };
+
+            $this.find('[data-sf-role="comments-sort-new-button"]').click(function () {
+                sortComments(true);
+            });
+
+            $this.find('[data-sf-role="comments-sort-old-button"]').click(function () {
+                sortComments(false);
+            });
+
+            /*
+                New comment
+            */
+
+            commentsFormButton.click(function () {
+                newCommentForm.toggle();
+            });
+
+            $this.find('[data-sf-role="comments-new-submit-button"]').click(function () {
+                // service call
+            });
+        };
+
+        // Widgets initialization
+        $('[data-sf-role="comments-wrapper"]').each(createWidget);
     });
 }(jQuery));
