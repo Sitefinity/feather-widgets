18d44c4490c5f1506e9f9e36b184ece2be8d4724
NadezhdaPetrova <nadezhda.petrova@hotmail.com>
2015-05-07 18:37:13 +0300

    Removed unnecessary code from LanguageSelector widget designer

--- Telerik.Sitefinity.Frontend.Navigation/Mvc/Scripts/LanguageSelector/designerview-simple.js
+++ Telerik.Sitefinity.Frontend.Navigation/Mvc/Scripts/LanguageSelector/designerview-simple.js
@@ -1,23 +1,3 @@
 ﻿(function () {
-    var designer = angular.module('designer');
-    designer.requires.push('expander');
-
-    designer.controller('SimpleCtrl', ['$scope', 'propertyService', function ($scope, propertyService) {
-        $scope.feedback.showLoadingIndicator = true;
-
-        propertyService.get()
-            .then(function (data) {
-                if (data) {
-                    $scope.properties = propertyService.toAssociativeArray(data.Items);
-                }
-            },
-            function (data) {
-                $scope.feedback.showError = true;
-                if (data)
-                    $scope.feedback.errorMessage = data.Detail;
-            })
-            .finally(function () {
-                $scope.feedback.showLoadingIndicator = false;
-            });
-    }]);
+    angular.module('designer').requires.push('expander');
 })();
