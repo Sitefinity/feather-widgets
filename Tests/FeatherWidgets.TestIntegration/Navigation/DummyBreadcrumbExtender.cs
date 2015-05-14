fe9a06d329536cb2f223ef0a13f657a7c42e2aa9
GeorgiMateev <georgi.mateev@telerik.com>
2015-05-14 19:05:27 +0300

    Added integration tests for the breadcrumb model

---
+++ Tests/FeatherWidgets.TestIntegration/Navigation/DummyBreadcrumbExtender.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Telerik.Sitefinity.Web;
+
+namespace FeatherWidgets.TestIntegration.Navigation
+{
+    public class DummyBreadcrumbExtender : IBreadcrumExtender
+    {
+        public IEnumerable<SiteMapNode> GetVirtualNodes(SiteMapProvider provider)
+        {
+            List<SiteMapNode> list = new List<SiteMapNode>();            
+               
+            var siteMapNode = new SiteMapNode(
+                provider, Guid.NewGuid().ToString(), string.Empty, DummySiteMapNodeTitle, "description");
+            list.Add(siteMapNode);                
+            
+            return list;
+        }
+
+        public const string DummySiteMapNodeTitle = "dummySiteMapNode";
+    }
+}
