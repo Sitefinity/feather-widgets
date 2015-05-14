2b666bff1312ed090d26ba8bcb4683dbc6aefd2d
Slaveya Shtereva <slaveya.shtereva@telerik.com>
2015-05-14 14:45:14 +0300

    Add video integration tests, pull all media test inder MediaGalleryWidgets folder

--- Tests/FeatherWidgets.TestIntegration/MediaGalleryWidgets/ImageGalleryWidgetPagingLimitTests.cs
+++ Tests/FeatherWidgets.TestIntegration/MediaGalleryWidgets/ImageGalleryWidgetPagingLimitTests.cs
@@ -8,7 +8,7 @@ using Telerik.Sitefinity.Modules.Libraries;
 using Telerik.Sitefinity.Mvc.Proxy;
 using Telerik.Sitefinity.TestUtilities.CommonOperations;
 
-namespace FeatherWidgets.TestIntegration.ImageGallery
+namespace FeatherWidgets.TestIntegration.MediaGalleryWidgets
 {
     /// <summary>
     /// This is a class with images gallery tests for paging and limit options.
