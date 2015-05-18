ba8e5af3a14b98eadb0b7050b4f4df1d389de229
GeorgiMateev <georgi.mateev@telerik.com>
2015-05-18 12:58:12 +0300

    Added taxonomy model

--- Telerik.Sitefinity.Frontend.Taxonomies/Mvc/Models/ITaxonomyModel.cs
+++ Telerik.Sitefinity.Frontend.Taxonomies/Mvc/Models/ITaxonomyModel.cs
@@ -43,23 +43,29 @@ namespace Telerik.Sitefinity.Frontend.Taxonomies.Mvc.Models
         /// </summary>
         /// <value>The taxonomy provider.</value>
         string TaxonomyProvider { get; set; }
-        
+
         /// <summary>
         /// Gets or sets the taxonomy id.
         /// </summary>
         /// <value>The taxonomy id.</value>
         Guid TaxonomyId { get; set; }
-        
+
         /// <summary>
         /// Gets or sets the Id of the content item for which the control should display the taxa.
         /// </summary>
         /// <value>The content id.</value>
         Guid ContentId { get; set; }
-        
+
         /// <summary>
         /// Gets or sets the name of the property that contains the taxonomy.
         /// </summary>
         /// <value>The name of the field.</value>
         string FieldName { get; set; }
+
+        /// <summary>
+        /// Creates the view model.
+        /// </summary>
+        /// <returns></returns>
+        TaxonomyViewModel CreateViewModel();
     }
 }
---
+++ Telerik.Sitefinity.Frontend.Taxonomies/Mvc/Models/TaxonomyModel.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Linq;
+using Telerik.Sitefinity.Taxonomies;
+using Telerik.Sitefinity.Taxonomies.Model;
+using Telerik.Sitefinity.Utilities.TypeConverters;
+
+namespace Telerik.Sitefinity.Frontend.Taxonomies.Mvc.Models
+{
+    /// <summary>
+    /// The model of the taxonomies widgets.
+    /// </summary>
+    public class TaxonomyModel : ITaxonomyModel
+    {
+        #region ITaxonomyModel implementation
+        /// <summary>
+        /// Gets or sets the full name of the static type that taxons associated to will be displayed.
+        /// </summary>
+        /// <value>The full name of the content type.</value>
+        public string ContentTypeName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the full name of the dynamic type that taxons associated to will be displayed.
+        /// </summary>
+        /// <value>The full name of the dynamic content type.</value>
+        public string DynamicContentTypeName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the provider of the content type that filters the displayed taxa.
+        /// </summary>
+        /// <value>The name of the content provider.</value>
+        public string ContentProviderName { get; set; }
+
+        /// <summary>
+        /// Determiens whether to display the count of the items associated with every taxon.
+        /// </summary>
+        /// <value>Show item count.</value>
+        public string ShowItemCount { get; set; }
+
+        /// <summary>
+        /// Gets or sets the URL of the page where content will be filtered by selected taxon.
+        /// </summary>
+        /// <value>The base URL.</value>
+        public string BaseUrl { get; set; }
+
+        /// <summary>
+        /// Gets or sets the taxonomy provider.
+        /// </summary>
+        /// <value>The taxonomy provider.</value>
+        public string TaxonomyProvider { get; set; }
+
+        /// <summary>
+        /// Gets or sets the taxonomy id.
+        /// </summary>
+        /// <value>The taxonomy id.</value>
+        public Guid TaxonomyId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Id of the content item for which the control should display the taxa.
+        /// </summary>
+        /// <value>The content id.</value>
+        public Guid ContentId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the property that contains the taxonomy.
+        /// </summary>
+        /// <value>The name of the field.</value>
+        public string FieldName { get; set; }
+
+        /// <summary>
+        /// Creates the view model.
+        /// </summary>
+        /// <returns></returns>
+        public TaxonomyViewModel CreateViewModel()
+        {
+            return new TaxonomyViewModel();
+        }
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Gets the taxonomy manager based on the specified provider.
+        /// </summary>
+        protected virtual TaxonomyManager CurrentTaxonomyManager
+        {
+            get
+            {
+                if (this.taxonomyManager == null)
+                {
+                    this.taxonomyManager = TaxonomyManager.GetManager(); 
+                }
+                return this.taxonomyManager;
+            }
+            set { this.taxonomyManager = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the type of the content that taxonomy control is going to display.
+        /// If ContentTypeName is set returns it otherwise try to resolve DynamicContentTypeName.
+        /// </summary>
+        /// <value>The type of the content.</value>
+        protected virtual Type TaxonomyContentType
+        {
+            get
+            {
+                if (this.taxonomyContentType == null)
+                {
+                    if (!this.ContentTypeName.IsNullOrWhitespace())
+                    {
+                        this.taxonomyContentType = Type.GetType(this.ContentTypeName);
+                    }
+                    else if (!this.DynamicContentTypeName.IsNullOrWhitespace())
+                    {
+                        this.taxonomyContentType = TypeResolutionService.ResolveType(this.DynamicContentTypeName, false);
+                    }
+                }
+
+                return this.taxonomyContentType;
+            }
+        }
+        /// <summary>
+        /// Gets the instance of <see cref="ITaxonomy"/> representing the taxonomy to which the taxon field is bound to.
+        /// </summary>
+        protected virtual ITaxonomy Taxonomy
+        {
+            get
+            {
+                if (this.taxonomy == null)
+                {
+                    this.taxonomy = this.CurrentTaxonomyManager.GetTaxonomy(this.TaxonomyId);
+                }
+                return this.taxonomy;
+            }
+        }
+
+        #endregion
+
+        #region Private fields and constants
+        private TaxonomyManager taxonomyManager;
+        private Type taxonomyContentType;
+        private ITaxonomy taxonomy;
+        #endregion
+    }
+}
