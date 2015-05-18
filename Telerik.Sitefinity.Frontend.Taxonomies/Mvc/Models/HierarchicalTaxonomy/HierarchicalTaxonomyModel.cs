193f0cbc41c834e8196fdcfeaaf0f78e939ad192
GeorgiMateev <georgi.mateev@telerik.com>
2015-05-18 14:38:19 +0300

    Added hierarchical taxonomy model

---
+++ Telerik.Sitefinity.Frontend.Taxonomies/Mvc/Models/HierarchicalTaxonomy/HierarchicalTaxonomyModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Telerik.Sitefinity.Frontend.Taxonomies.Mvc.Models.HierarchicalTaxonomy
+{
+    /// <summary>
+    /// The hierarchical taxonomy model.
+    /// </summary>
+    public class HierarchicalTaxonomyModel : TaxonomyModel, IHierarchicalTaxonomyModel
+    {
+        /// <summary>
+        /// Determines what taxa will be displayed by the widget/
+        /// </summary>
+        /// <value>The taxa to display.</value>
+        public TaxaToDisplay TaxaToDisplay { get; set; }
+
+        /// <summary>
+        /// Determines how many levels from the hierarchy to include.
+        /// </summary>
+        /// <value>The levels.</value>
+        public int Levels { get; set; }
+
+        /// <summary>
+        /// Gets or sets the root category which children will be displayed as a top level in the widget.
+        /// Used only if this display mode is selected.
+        /// </summary>
+        /// <value>The parent category.</value>
+        public Guid RootCategory { get; set; }
+    }
+}
---
+++ Telerik.Sitefinity.Frontend.Taxonomies/Mvc/Models/HierarchicalTaxonomy/IHierarchicalTaxonomyModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Telerik.Sitefinity.Frontend.Taxonomies.Mvc.Models.HierarchicalTaxonomy
+{
+    /// <summary>
+    /// The public interface of the hierarchical taxonomy widget't model.
+    /// </summary>
+    public interface IHierarchicalTaxonomyModel : ITaxonomyModel
+    {
+        /// <summary>
+        /// Determines what taxa will be displayed by the widget/
+        /// </summary>
+        /// <value>The taxa to display.</value>
+        TaxaToDisplay TaxaToDisplay { get; set; }
+
+        /// <summary>
+        /// Determines how many levels from the hierarchy to include.
+        /// </summary>
+        /// <value>The levels.</value>
+        int Levels { get; set; }
+
+        /// <summary>
+        /// Gets or sets the root category which children will be displayed as a top level in the widget.
+        /// Used only if this display mode is selected.
+        /// </summary>
+        /// <value>The parent category.</value>
+        Guid RootCategory { get; set; }
+    }
+}
