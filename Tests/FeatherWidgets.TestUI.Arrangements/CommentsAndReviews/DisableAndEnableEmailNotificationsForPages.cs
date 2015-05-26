ca21633e00b07933410800dcdad11b61aa148f1b
VenetaBogoeva <veneta.bogoeva@telerik.com>
2015-05-26 13:40:54 +0300

    Added test for disable and enable email notifications

---
+++ Tests/FeatherWidgets.TestUI.Arrangements/CommentsAndReviews/DisableAndEnableEmailNotificationsForPages.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FeatherWidgets.TestUtilities.CommonOperations;
+using Telerik.Sitefinity.TestUI.Arrangements.Framework;
+using Telerik.Sitefinity.TestUI.Arrangements.Framework.Attributes;
+using Telerik.Sitefinity.TestUtilities.CommonOperations;
+
+namespace FeatherWidgets.TestUI.Arrangements
+{
+    /// <summary>
+    /// DisableAndEnableEmailNotificationsForPages arrangement class.
+    /// </summary>
+    public class DisableAndEnableEmailNotificationsForPages : ITestArrangement
+    {
+        /// <summary>
+        /// Server side set up.
+        /// </summary>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Globalization", "CA1303:Do not pass literals as localized parameters", MessageId = "Telerik.Sitefinity.TestUtilities.CommonOperations.CommentOperations.CreateComment(System.String,System.String,System.Guid,System.String,System.String,System.String,System.Boolean)"), ServerSetUp]
+        public void SetUp()
+        {
+            ServerOperations.Comments().AllowComments(ThreadType, true);
+            Guid templateId = Telerik.Sitefinity.TestUtilities.CommonOperations.ServerOperations.Templates().GetTemplateIdByTitle(PageTemplateName);
+            Guid pageId = Telerik.Sitefinity.TestUtilities.CommonOperations.ServerOperations.Pages().CreatePage(PageName, templateId);
+            pageId = ServerOperations.Pages().GetPageNodeId(pageId);
+            ServerOperationsFeather.Pages().AddCommentsWidgetToPage(pageId, "Contentplaceholder1");
+            var groupKey = ServerOperations.Comments().GetCurrentSiteId.ToString();
+            ServerOperations.Comments().CreateComment(groupKey, ThreadType, pageId, PageName, CommentToPage, "published", false);
+        }
+
+        /// <summary>
+        /// Disable email notifications for pages
+        /// </summary>
+        [ServerArrangement]
+        public void DisableEmailNotifications()
+        {
+            ServerOperations.Comments().EnableEmailSubscription(ThreadType, false);
+        }
+
+        /// <summary>
+        /// Enable email notifications for pages
+        /// </summary>
+        [ServerArrangement]
+        public void EnableEmailNotifications()
+        {
+            ServerOperations.Comments().EnableEmailSubscription(ThreadType, true);
+        }
+
+        /// <summary>
+        /// Tears down.
+        /// </summary>
+        [ServerTearDown]
+        public void TearDown()
+        {
+            ServerOperations.Pages().DeleteAllPages();
+            var siteID = ServerOperations.Comments().GetCurrentSiteId.ToString();
+            ServerOperations.Comments().DeleteAllComments(siteID);
+            ServerOperations.Comments().AllowComments(ThreadType, false);
+            ServerOperations.Comments().EnableEmailSubscription(ThreadType, false);
+        }
+
+        private const string PageName = "CommentsPage";
+        private const string PageTemplateName = "Bootstrap.default";
+        private const string ThreadType = "Telerik.Sitefinity.Pages.Model.PageNode";
+        private const string CommentToPage = "Comment to page ";
+    }
+}
