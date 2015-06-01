28906cf41afe172aab7cdb577ea0a3c65b9615dc
GeorgiMateev <georgi.mateev@telerik.com>
2015-06-01 18:49:10 +0300

    Added test for retrieving categories from content item

--- Tests/FeatherWidgets.TestIntegration/Taxonomies/HierarchicalTaxonomyTests.cs
+++ Tests/FeatherWidgets.TestIntegration/Taxonomies/HierarchicalTaxonomyTests.cs
@@ -6,6 +6,7 @@ using MbUnit.Framework;
 using Telerik.Sitefinity;
 using Telerik.Sitefinity.Data;
 using Telerik.Sitefinity.Frontend.Taxonomies.Mvc.Models.HierarchicalTaxonomy;
+using Telerik.Sitefinity.Model;
 using Telerik.Sitefinity.Modules.News;
 using Telerik.Sitefinity.News.Model;
 using Telerik.Sitefinity.Taxonomies;
@@ -200,7 +201,7 @@ namespace FeatherWidgets.TestIntegration.Taxonomies
         [Test]
         [Author(TestAuthor.Team7)]
         [Description("Verifies that only taxa that is assigned to a content item is shown when ShowEmptyTaxa is disabled.")]
-        public void Categories_VerifyEmptyTaxaIsNotShownRetrieved()
+        public void Categories_VerifyEmptyTaxaIsNotRetrieved()
         {
             var model = new HierarchicalTaxonomyModel();
             model.TaxaToDisplay = HierarchicalTaxaToDisplay.All;
@@ -218,6 +219,30 @@ namespace FeatherWidgets.TestIntegration.Taxonomies
 
             var lastActual = viewModel.Taxa[1];
             Assert.AreEqual(lastTaxonName, lastActual.Title);
+        }
+
+        [Test]
+        [Author(TestAuthor.Team7)]
+        [Description("Verifies that only taxa from a specific content item is shown when ContentId is set.")]
+        public void Categories_VerifyTaxaFromContentItemIsRetrieved()
+        {
+            var item = NewsManager.GetManager().GetNewsItems().FirstOrDefault();
+
+            var model = new HierarchicalTaxonomyModel();
+            model.ContentId = item.Id;
+
+            var viewModel = model.CreateViewModel();
+
+            var itemTaxa = (IList<Guid>)item.GetValue("Category");
+
+            Assert.AreEqual(itemTaxa.Count, viewModel.Taxa.Count);
+
+            var taxonomyManager = TaxonomyManager.GetManager();
+            for (var i = 0; i < itemTaxa.Count; i++)
+            {
+                var taxon = taxonomyManager.GetTaxa<HierarchicalTaxon>().FirstOrDefault(t => t.Id == itemTaxa[i]);
+                Assert.AreEqual(taxon.Title.ToString(), viewModel.Taxa[i].Title);
+            }
         }
 
         private void DeleteAllCategories()
