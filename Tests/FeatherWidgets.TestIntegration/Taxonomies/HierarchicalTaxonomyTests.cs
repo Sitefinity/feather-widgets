a9baa8df3fd7566d429c6107d7c6b49c5b6ee47c
GeorgiMateev <georgi.mateev@telerik.com>
2015-06-01 17:02:26 +0300

    Added three integration tests for the hierarchical model

--- Tests/FeatherWidgets.TestIntegration/Taxonomies/HierarchicalTaxonomyTests.cs
+++ Tests/FeatherWidgets.TestIntegration/Taxonomies/HierarchicalTaxonomyTests.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Web.Script.Serialization;
 using MbUnit.Framework;
+using Telerik.Sitefinity.Data;
 using Telerik.Sitefinity.Frontend.Taxonomies.Mvc.Models.HierarchicalTaxonomy;
 using Telerik.Sitefinity.Taxonomies;
+using Telerik.Sitefinity.Taxonomies.Model;
 using Telerik.Sitefinity.TestUtilities;
 using Telerik.Sitefinity.TestUtilities.CommonOperations;
 
@@ -18,18 +21,24 @@ namespace FeatherWidgets.TestIntegration.Taxonomies
         [FixtureSetUp]
         public void FixtureSetUp()
         {
-            var taxonomyOperations = new TaxonomiesOperations();
-
-            foreach (var taxon in this.taxaNames)
+            using (new ElevatedModeRegion(TaxonomyManager.GetManager()))
             {
-                taxonomyOperations.CreateHierarchicalTaxon(TaxonomyManager.CategoriesTaxonomyId, taxon);
+                var taxonomyOperations = new TaxonomiesOperations();
+
+                foreach (var taxon in this.taxaNames)
+                {
+                    string parentName;
+                    string taxonName;
+
+                    this.GetTaxonNameAndParentName(taxon, out parentName, out taxonName);
+
+                    taxonomyOperations.CreateHierarchicalTaxon(taxonName, parentName, "Categories");
+                }
+
+                var newsOperations = new NewsOperations();
+                var newsItemsId = Guid.NewGuid();
+                newsOperations.CreateNewsItem("n1", newsItemsId);
             }
-
-            var newsOperations = new NewsOperations();
-            var newsItemsId = Guid.NewGuid();
-            newsOperations.CreateNewsItem("n1", newsItemsId);
-
-            taxonomyOperations.AddTaxonsToNews(newsItemsId, this.taxaNames.Take(1), new List<string>());
         }
 
         [FixtureTearDown]
@@ -38,8 +47,7 @@ namespace FeatherWidgets.TestIntegration.Taxonomies
             var newsOperations = new NewsOperations();
             newsOperations.DeleteAllNews();
 
-            var taxonomyOperations = new TaxonomiesOperations();
-            taxonomyOperations.DeleteCategories(this.taxaNames.ToArray());
+            this.DeleteAllCategories();
         }
 
         [Test]
@@ -49,18 +57,100 @@ namespace FeatherWidgets.TestIntegration.Taxonomies
         {
             var model = new HierarchicalTaxonomyModel();
             model.TaxaToDisplay = HierarchicalTaxaToDisplay.All;
+            model.ShowEmptyTaxa = true;
 
             var viewModel = model.CreateViewModel();
 
+            Assert.AreEqual(this.taxaNames.Count, viewModel.Taxa.Count);
+
             for (int i = 0; i < viewModel.Taxa.Count; i++)
             {
-                var expected = this.taxaNames[i];
+                string parentName;
+                string taxonName;
+                this.GetTaxonNameAndParentName(this.taxaNames[i], out parentName, out taxonName);
+
+                var actual = viewModel.Taxa[i];
+
+                Assert.AreEqual(taxonName, actual.Title);
+            }
+        }
+
+        [Test]
+        [Author(TestAuthor.Team7)]
+        [Description("Verifies that if the option to show specific taxa is set, the exactly same taxa will be retrieved.")]
+        public void Categories_VerifySpecificTaxaIsRetrieved()
+        {
+            var model = new HierarchicalTaxonomyModel();
+            model.TaxaToDisplay = HierarchicalTaxaToDisplay.Selected;
+            model.ShowEmptyTaxa = true;
+
+            var selectedTaxaIds = TaxonomyManager.GetManager()
+                .GetTaxa<HierarchicalTaxon>()
+                .Where(t => this.taxaNames[1] == t.Title.ToString() || this.taxaNames[2] == t.Title.ToString())
+                .Select(t => t.Id);
+
+            var serializer = new JavaScriptSerializer();
+            model.SerializedSelectedTaxaIds = serializer.Serialize(selectedTaxaIds);
+
+            var viewModel = model.CreateViewModel();
+
+            Assert.AreEqual(2, viewModel.Taxa.Count);
+
+            for (int i = 0; i < viewModel.Taxa.Count; i++)
+            {
+                var expected = this.taxaNames[i + 1];
                 var actual = viewModel.Taxa[i];
 
                 Assert.AreEqual(expected, actual.Title);
             }
         }
 
-        private List<string> taxaNames = new List<string>() { "c1", "c2", "c3" };
+        [Test]
+        [Author(TestAuthor.Team7)]
+        [Description("Verifies that only top level taxa is retrieved if that option is selected.")]
+        public void Categories_VerifyTopLevelTaxaIsRetrieved()
+        {
+            var model = new HierarchicalTaxonomyModel();
+            model.TaxaToDisplay = HierarchicalTaxaToDisplay.TopLevel;
+            model.ShowEmptyTaxa = true;
+
+            var viewModel = model.CreateViewModel();
+
+            var topLevelNames = this.taxaNames.Where(tn => !tn.Contains('|')).ToList();
+
+            Assert.AreEqual(topLevelNames.Count, viewModel.Taxa.Count);
+
+            for (int i = 0; i < viewModel.Taxa.Count; i++)
+            {
+                var expected = topLevelNames[i];
+                var actual = viewModel.Taxa[i];
+
+                Assert.AreEqual(expected, actual.Title);
+            }
+        }
+
+        private void DeleteAllCategories()
+        {
+            var manager = TaxonomyManager.GetManager();
+            using (new ElevatedModeRegion(manager))
+            {
+                var taxa = manager.GetTaxa<HierarchicalTaxon>();
+                foreach (var taxon in taxa)
+                {
+                    manager.DeleteItem(taxon);
+                }
+
+                manager.SaveChanges();
+            }
+        }
+
+        private void GetTaxonNameAndParentName(string taxon, out string parentName, out string taxonName)
+        {
+            var parts = taxon.Split('|');
+            parentName = parts.Length == 2 ? parts[0] : string.Empty;
+            taxonName = parts.Length == 2 ? parts[1] : parts[0];
+        }
+
+        private List<string> taxaNames = new List<string>() { "c1", "c2", "c3", "c3|c3c1", "c3|c3c2", "c3c2|c3c2c1" };
     }
 }
