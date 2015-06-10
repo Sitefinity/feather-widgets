59b6841e8566ca798e893c113be74e475926ce80
manev <kaloyan.manev@telerik.com>
2015-06-10 13:08:39 +0300

    hide widget when feeds related module is not active

---
+++ Telerik.Sitefinity.Frontend.Publishing/Helpers/PublishingWidgetExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using Telerik.Sitefinity.Publishing;
+using Telerik.Sitefinity.Services;
+
+namespace Telerik.Sitefinity.Frontend.Publishing.Helpers
+{
+    public static class PublishingWidgetExtensions
+    {
+        /// <summary>
+        /// Determines whether the publishing module is activated.
+        /// </summary>
+        /// <returns></returns>
+        public static bool IsModuleActivated()
+        {
+            var module = SystemManager.GetModule(PublishingModule.ModuleName);
+
+            return module != null;
+        }
+    }
+}
