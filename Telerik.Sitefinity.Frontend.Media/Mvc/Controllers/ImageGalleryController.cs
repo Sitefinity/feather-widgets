169f3878755916b58fa0b8101f34f7da438ce6e0
GeorgiMateev <georgi.mateev@telerik.com>
2015-06-10 15:15:24 +0300

    Fixed comments and some errors in the code

--- Telerik.Sitefinity.Frontend.Media/Mvc/Controllers/ImageGalleryController.cs
+++ Telerik.Sitefinity.Frontend.Media/Mvc/Controllers/ImageGalleryController.cs
@@ -159,27 +159,30 @@ namespace Telerik.Sitefinity.Frontend.Media.Mvc.Controllers
         }
 
         /// <summary>
-        /// Action that returns view with the related items of the given item.
+        /// Action that returns a list view with the related items of the given item.
         /// </summary>
         /// <param name="relatedItem">The related item.</param>
-        /// <param name="detailPageId">The id of the page where the detail of the item will be shown.</param>
-        /// <param name="openInSamePage">Determines whether the detail of the item will be on the same page or in a other one.</param>
-        /// <param name="templateName">Name of the template.</param>
+        /// <param name="templateName">Name of the template that will show the list view.</param>
         /// <param name="relatedDataViewModel">The related data view model. Contains settings needed to retrieve related data.</param>
         /// <param name="settingsViewModel">The settings view model. The widget's model will be initialized with them.</param>
         /// <param name="page">The current page.</param>
+        /// <param name="detailPageId">The id of the page where the detail of the item will be shown.</param>
+        /// <param name="openInSamePage">Determines whether the detail of the item will be on the same page or in another one.</param>
         /// <returns></returns>
         public ActionResult RelatedData(
             IDataItem relatedItem,
-            Guid detailPageId,
-            bool openInSamePage,
             string templateName,
             RelatedDataViewModel relatedDataViewModel,
             ContentListSettingsViewModel settingsViewModel,
-            int? page)
+            int? page,
+            Guid? detailPageId,
+            bool? openInSamePage)
         {
-            this.DetailsPageId = detailPageId;
-            this.OpenInSamePage = openInSamePage;
+            if (detailPageId.HasValue)
+                this.DetailsPageId = detailPageId.Value;
+
+            if (openInSamePage.HasValue)
+                this.OpenInSamePage = openInSamePage.Value;
 
             this.SetPagingRedirectTemplateInViewBag();
 
