0c522ea79c327118fcb1ed4eae5e153aa14a5aea
manev <kaloyan.manev@telerik.com>
2015-06-15 11:06:38 +0300

    Added email campaingns project

---
+++ Telerik.Sitefinity.Frontend.EmailCampaingns/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using Telerik.Sitefinity.Frontend.Mvc.Infrastructure.Controllers.Attributes;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Telerik.Sitefinity.Frontend.EmailCampaingns")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5674dc18-b37d-4a3b-bd2a-ec40fe8b83e8")]
+
+[assembly: ControllerContainer]
+
