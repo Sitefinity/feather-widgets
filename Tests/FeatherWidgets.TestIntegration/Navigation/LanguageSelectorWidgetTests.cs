7bc56d4881eaab08f00e98fd4729236b82cd8fb0
Slaveya Shtereva <slaveya.shtereva@telerik.com>
2015-06-19 15:50:37 +0300

    Correct test LanguageSelectorWidget_RedirectToHomePageOfTheMissingTranslations

--- Tests/FeatherWidgets.TestIntegration/Navigation/LanguageSelectorWidgetTests.cs
+++ Tests/FeatherWidgets.TestIntegration/Navigation/LanguageSelectorWidgetTests.cs
@@ -234,7 +234,10 @@ namespace FeatherWidgets.TestIntegration.Navigation
                 { this.sitefinityLanguages["Bulgarian"], this.GetPageUrlOfNotTranslatedPage(PageName + currentPage.Value.Name, this.sitefinityLanguages["Bulgarian"]) }
             };
 
-            var notExpectedLinks = new Dictionary<CultureInfo, string>();
+            var notExpectedLinks = new Dictionary<CultureInfo, string>()
+            {
+                { this.sitefinityLanguages["English"], this.GetPageUrl(PageName, this.sitefinityLanguages["English"], true) }
+            };
 
             this.AssertLanguageLinks(pageContent, expectedLinks, notExpectedLinks);
         }
@@ -414,12 +417,12 @@ namespace FeatherWidgets.TestIntegration.Navigation
                             break;
                         }
 
-                            Assert.IsFalse(
+                        Assert.IsFalse(
                             string.IsNullOrEmpty(foundlanguageCulture.Name),
                             string.Format(CultureInfo.InvariantCulture, "Current link {0} is not expected.", linkOnClickAttribute));
                         
                         links.Remove(foundlanguageCulture);
-                        }
+                    }
                     else if (chunk.TagName.Equals("input") && chunk.GetParamValue("type") == "hidden" && chunk.GetParamValue("value") != null && chunk.GetParamValue("value").StartsWith("http://", StringComparison.Ordinal))
                     {
                         var dataSfRole = chunk.GetParamValue("data-sf-role");
