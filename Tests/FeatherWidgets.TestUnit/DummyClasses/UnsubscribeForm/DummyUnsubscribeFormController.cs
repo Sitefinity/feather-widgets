dacfce9946748ad62f60309e76fcd17421008b04
manev <kaloyan.manev@telerik.com>
2015-06-19 14:02:21 +0300

    unsubscribe controller unit test added

---
+++ Tests/FeatherWidgets.TestUnit/DummyClasses/UnsubscribeForm/DummyUnsubscribeFormController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using Telerik.Sitefinity.Frontend.EmailCampaigns.Mvc.Controllers;
+using Telerik.Sitefinity.Frontend.EmailCampaigns.Mvc.Models.UnsubscribeForm;
+
+namespace FeatherWidgets.TestUnit.DummyClasses.Media.DocumentsList
+{
+    public class DummyUnsubscribeFormController : UnsubscribeFormController
+    {
+        private readonly bool isLicensed;
+        private readonly bool isModuleActivated;
+
+        public override string LicensingMessage
+        {
+            get
+            {
+                return "No valid license";
+            }
+        }
+
+        public override string NewslettersModuleDeactivatedMessage
+        {
+            get
+            {
+                return "Not installed module";
+            }
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1026:DefaultParametersShouldNotBeUsed")]
+        public DummyUnsubscribeFormController(bool isLicensed = true, bool isModuleActivated = true)
+        {
+            this.isLicensed = isLicensed;
+            this.isModuleActivated = isModuleActivated;
+        }
+
+        protected override bool IsNewslettersModuleActivated()
+        {
+            return this.isModuleActivated;
+        }
+
+        public override bool IsLicensed
+        {
+            get
+            {
+                return this.isLicensed;
+            }
+        }
+
+        public IUnsubscribeFormModel MockModel { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Globalization", "CA1303:Do not pass literals as localized parameters", MessageId = "FeatherWidgets.TestUnit.DummyClasses.Media.DummyUnsubscribeFormModel.#ctor(System.String,System.String,System.String)")]
+        public override IUnsubscribeFormModel Model
+        {
+            get
+            {
+                return this.MockModel ?? new DummyUnsubscribeFormModel { ListId = new Guid("51C6563A-C165-40B0-B950-E3F99CF1ED98") };
+            }
+        }
+    }
+}
---
+++ Tests/FeatherWidgets.TestUnit/DummyClasses/UnsubscribeForm/DummyUnsubscribeFormModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using Telerik.Sitefinity.Frontend.EmailCampaigns.Mvc.Models;
+using Telerik.Sitefinity.Frontend.EmailCampaigns.Mvc.Models.UnsubscribeForm;
+
+namespace FeatherWidgets.TestUnit.DummyClasses.Media
+{
+    public class DummyUnsubscribeFormModel : IUnsubscribeFormModel
+    {
+        public Guid ListId { get; set; }
+
+        public string ProviderName { get; set; }
+
+        public string WidgetTitle { get; set; }
+
+        public string WidgetDescription { get; set; }
+
+        public string Message { get; set; }
+
+        public UnsubscribeMode UnsubscribeMode { get; set; }
+
+        public Guid PageId { get; set; }
+
+        public SuccessfullySubmittedForm SuccessfullySubmittedForm { get; set; }
+
+        public string LinkCssClass { get; set; }
+
+        public string EmailAddressCssClass { get; set; }
+
+        public UnsubscribeFormViewModel CreateViewModel()
+        {
+            return new UnsubscribeFormViewModel();
+        }
+
+        public void ExecuteAction(string subscriberId, string issueId, bool shouldSubscribe)
+        {
+        }
+
+        public bool Unsubscribe(UnsubscribeFormViewModel viewModel, out string error)
+        {
+            error = string.Empty;
+            return true;
+        }
+    }
+}
