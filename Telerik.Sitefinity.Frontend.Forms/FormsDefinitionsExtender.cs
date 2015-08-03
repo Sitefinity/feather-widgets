f4412e0576418b3d8dce4492ef095ab5485ccb3a
dzhenko <dzhenko@gmail.com>
2015-08-03 18:05:38 +0300

    Framework setting definition now via Featherwidget

---
+++ Telerik.Sitefinity.Frontend.Forms/FormsDefinitionsExtender.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.UI;
+using Telerik.Sitefinity.Modules.Pages;
+using Telerik.Sitefinity.Web.UI.ContentUI;
+using Telerik.Sitefinity.Web.UI.ContentUI.Contracts;
+using Telerik.Sitefinity.Web.UI.ContentUI.Views.Backend.Detail.Contracts;
+using Telerik.Sitefinity.Web.UI.Extenders.Definitions;
+using Telerik.Sitefinity.Web.UI.Fields;
+using Telerik.Sitefinity.Web.UI.Fields.Definitions;
+using Telerik.Sitefinity.Web.UI.Fields.Enums;
+
+internal class FormsDefinitionsExtender : IControlDefinitionExtender
+{
+    public const string BackendInsertViewName = "FormsBackendInsert";
+
+    public void ExtendDefinition(IContentViewControlDefinition contentViewControlDefinition)
+    {
+        var backendInsertViewDefinition = contentViewControlDefinition.Views.FirstOrDefault(v => v.ViewName == BackendInsertViewName) as IDetailFormViewDefinition;
+
+        if (backendInsertViewDefinition != null)
+        {
+            var frameworkSectionDefinition = this.BuildContentViewSectionDefinition();
+            var frameworkSectionChoiceFieldDefinition = this.BuildFrameworkChoiceFieldDefinition();
+            frameworkSectionDefinition.Fields.Add(frameworkSectionChoiceFieldDefinition);
+
+            ((IList<IContentViewSectionDefinition>)backendInsertViewDefinition.Sections).Add(frameworkSectionDefinition);
+        }
+    }
+
+    private ContentViewSectionDefinition BuildContentViewSectionDefinition()
+    {
+        var frameworkSection = new ContentViewSectionDefinition();
+
+        frameworkSection.ExpandableDefinition = new ExpandableControlDefinition();
+        frameworkSection.ExpandableDefinition.Expanded = false;
+
+        frameworkSection.CssClass = "sfExpandableForm";
+        frameworkSection.Title = "Framework Settings";
+
+        return frameworkSection;
+    }
+
+    private ChoiceFieldDefinition BuildFrameworkChoiceFieldDefinition()
+    {
+        var frameworkField = new ChoiceFieldDefinition()
+        {
+            ID = "FormFramework",
+            DataFieldName = "Framework",
+            Title = "WebFrameworkTitle",
+            Description = "WebFrameworkDescription",
+            DisplayMode = FieldDisplayMode.Write,
+            WrapperTag = HtmlTextWriterTag.Li,
+            MutuallyExclusive = true,
+            ResourceClassId = typeof(PageResources).Name,
+            RenderChoiceAs = RenderChoicesAs.RadioButtons,
+            FieldType = typeof(ChoiceField)
+        };
+
+        frameworkField.Choices.Add(new ChoiceDefinition()
+        {
+            Text = "MVCOnly",
+            ResourceClassId = typeof(PageResources).Name,
+            Value = "1"
+        });
+
+        frameworkField.Choices.Add(new ChoiceDefinition()
+        {
+            Text = "WebFormsOnly",
+            ResourceClassId = typeof(PageResources).Name,
+            Value = "0"
+        });
+
+        return frameworkField;
+    }
+}
--- Telerik.Sitefinity.Frontend.Forms/Initializer.cs
+++ Telerik.Sitefinity.Frontend.Forms/Initializer.cs
@@ -1,4 +1,7 @@
-﻿using Telerik.Sitefinity.Abstractions.VirtualPath;
+﻿using Telerik.Microsoft.Practices.Unity;
+using Telerik.Sitefinity.Abstractions;
+using Telerik.Sitefinity.Abstractions.VirtualPath;
+using Telerik.Sitefinity.Web.UI.ContentUI;
 
 namespace Telerik.Sitefinity.Frontend.Forms
 {
@@ -7,6 +10,7 @@ namespace Telerik.Sitefinity.Frontend.Forms
         public static void Initialize()
         {
             VirtualPathManager.AddVirtualFileResolver<FormsVirtualRazorResolver>(FormsVirtualRazorResolver.Path + "*", "MvcFormsResolver");
+            ObjectFactory.Container.RegisterInstance<IControlDefinitionExtender>("FormsDefinitionsExtender", new FormsDefinitionsExtender(), new ContainerControlledLifetimeManager());
         }
     }
 }
