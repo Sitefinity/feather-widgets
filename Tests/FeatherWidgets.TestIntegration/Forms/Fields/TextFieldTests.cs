39d09cdd415a412c47ad1f46727a1efeed5fbc83
ElenaGaneva <Elena.Ganeva@telerik.com>
2015-08-07 11:46:05 +0300

    Added test for text field

---
+++ Tests/FeatherWidgets.TestIntegration/Forms/Fields/TextFieldTests.cs
@@ -0,0 +1,58 @@
+﻿using System.Linq;
+using FeatherWidgets.TestUtilities.CommonOperations;
+using MbUnit.Framework;
+using Telerik.Sitefinity.Frontend.Forms.Mvc.Controllers;
+using Telerik.Sitefinity.Frontend.Mvc.Infrastructure.Controllers;
+using Telerik.Sitefinity.Frontend.TestUtilities;
+using Telerik.Sitefinity.Frontend.TestUtilities.CommonOperations;
+using Telerik.Sitefinity.Modules.Pages;
+using Telerik.Sitefinity.Mvc.Proxy;
+using Telerik.Sitefinity.TestIntegration.SDK.DevelopersGuide.SitefinityEssentials.Modules.Forms;
+
+namespace FeatherWidgets.TestIntegration.Forms.Fields
+{
+    /// <summary>
+    /// This class contains ensures TextField functionalities work correctly.
+    /// </summary>
+    [TestFixture]
+    public class TextFieldTests
+    {
+        /// <summary>
+        /// Ensures that when a text field widget is added to form the default value is presented in the page markup.
+        /// </summary>
+        [Test]
+        [Category(TestCategories.Forms)]
+        [Author(FeatherTeams.FeatherTeam)]
+        [Description("Ensures that when a text field widget is added to form the default value is presented in the page markup.")]
+        public void TextField_EditDefaultValue_MarkupIsCorrect()
+        {
+            var control = new MvcWidgetProxy();
+            control.ControllerName = typeof(TextFieldController).FullName;
+            var controller = new TextFieldController();
+            controller.MetaField.DefaultValue = "My default text";
+            control.Settings = new ControllerSettings(controller);
+
+            var formId = ServerOperationsFeather.Forms().CreateFormWithWidget(control);
+
+            var pageManager = PageManager.GetManager();
+
+            try
+            {
+                var template = pageManager.GetTemplates().FirstOrDefault(t => t.Name == "SemanticUI.default" && t.Title == "default");
+                Assert.IsNotNull(template, "Template was not found");
+
+                var pageId = FeatherServerOperations.Pages().CreatePageWithTemplate(template, "TextFieldValueTest", "text-field-value-test");
+                ServerOperationsFeather.Forms().AddFormControlToPage(pageId, formId);
+
+                var pageContent = FeatherServerOperations.Pages().GetPageContent(pageId);
+
+                Assert.IsTrue(pageContent.Contains("My default text"), "Form did not render the default text in the text field.");
+            }
+            finally
+            {
+                Telerik.Sitefinity.TestUtilities.CommonOperations.ServerOperations.Pages().DeleteAllPages();
+                FormsModuleCodeSnippets.DeleteForm(formId);
+            }
+        }
+    }
+}
