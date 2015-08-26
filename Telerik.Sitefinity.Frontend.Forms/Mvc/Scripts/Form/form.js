aa2664096db4fc5131d4924c2f4ada10c4cdcaea
dzhenko <dzhenko@gmail.com>
2015-08-26 12:48:18 +0300

    Client side adding of submit button

--- Telerik.Sitefinity.Frontend.Forms/Mvc/Scripts/Form/form.js
+++ Telerik.Sitefinity.Frontend.Forms/Mvc/Scripts/Form/form.js
@@ -1,5 +1,40 @@
 ﻿(function ($) {
+    var findSubmitButtonDock = function (wrapperDockingZones) {
+        if (wrapperDockingZones && wrapperDockingZones.length) {
+            var allDocks = [];
+
+            wrapperDockingZones.forEach(function (dockingZoneId) {
+                allDocks.push.apply(allDocks, $find(dockingZoneId).get_docks());
+            });
+
+            var submitButtonDocks = allDocks.filter(function (dock) {
+                var dockProperties = JSON.parse($(dock.get_element()).attr('parameters') || null);
+                if (dockProperties && dockProperties.length) {
+                    return dockProperties.some(function (prop) {
+                        return prop.Key === 'ControllerName' && prop.Value === 'Telerik.Sitefinity.Frontend.Forms.Mvc.Controllers.SubmitButtonController'
+                    });
+                }
+            });
+
+            return submitButtonDocks[0];
+        }
+    };
+
     $(function () {
-        console.log('loaded');
+        $(document).on('sf-zone-editor-item-dropped', function (e) {
+            if (e.args.ControlType === 'Telerik.Sitefinity.Mvc.Proxy.MvcControllerProxy') {
+                var droppedMvcFields = $('#RadDockZoneBody').children('[behaviourobjecttype]');
+                if (droppedMvcFields.length) {
+                    var hasSubmitButton = Array.prototype.some.call(droppedMvcFields, function (el) {
+                        return el.attributes.behaviourobjecttype.value === 'Telerik.Sitefinity.Frontend.Forms.Mvc.Controllers.SubmitButtonController';
+                    });
+
+                    if (!hasSubmitButton) {
+                        var submitButtonDock = findSubmitButtonDock(e.sender.get_toolboxDockingZones());
+                        e.sender._dropSubmitButton(submitButtonDock, e.args.DockId);
+                    }
+                }
+            }
+        });
     });
 }(jQuery));
