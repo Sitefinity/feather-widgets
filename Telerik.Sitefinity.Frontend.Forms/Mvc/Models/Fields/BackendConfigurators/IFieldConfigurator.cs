045df3a22bc5a84e3b07d2f4181fddf4651c01d9
ElenaGaneva <Elena.Ganeva@telerik.com>
2015-09-14 17:31:58 +0300

    Added missing comments

--- Telerik.Sitefinity.Frontend.Forms/Mvc/Models/Fields/BackendConfigurators/IFieldConfigurator.cs
+++ Telerik.Sitefinity.Frontend.Forms/Mvc/Models/Fields/BackendConfigurators/IFieldConfigurator.cs
@@ -7,8 +7,16 @@ using Telerik.Sitefinity.Web.UI.Fields;
 
 namespace Telerik.Sitefinity.Frontend.Forms.Mvc.Models.Fields.BackendConfigurators
 {
+    /// <summary>
+    /// Defines API for field configuration.
+    /// </summary>
     public interface IFieldConfigurator
     {
+        /// <summary>
+        /// Configures the specified backend control.
+        /// </summary>
+        /// <param name="backendControl">The backend control.</param>
+        /// <param name="formFieldController">The form field controller.</param>
         void Configure(FieldControl backendControl, IFormFieldController<IFormFieldModel> formFieldController);
     }
 }
