145e3525d32bfa4472b32dac0cb72fae2b2ad7e6
ElenaGaneva <Elena.Ganeva@telerik.com>
2015-10-02 15:57:28 +0300

    Added html-field in SectionHeader element designer
    Due to UX requirements

--- Telerik.Sitefinity.Frontend.Forms/Mvc/Models/Fields/SectionHeader/ISectionHeaderModel.cs
+++ Telerik.Sitefinity.Frontend.Forms/Mvc/Models/Fields/SectionHeader/ISectionHeaderModel.cs
@@ -9,27 +9,11 @@ namespace Telerik.Sitefinity.Frontend.Forms.Mvc.Models.Fields.SectionHeader
     public interface ISectionHeaderModel : IFormElementModel
     {
         /// <summary>
-        /// Gets or sets the placeholder text.
-        /// </summary>
-        /// <value>
-        /// The placeholder text.
-        /// </value>
-        string PlaceholderText { get; set; }
-
-        /// <summary>
         /// Gets or sets the text.
         /// </summary>
         /// <value>
         /// The text.
         /// </value>
         string Text { get; set; }
-
-        /// <summary>
-        /// Gets or sets the type of the heading.
-        /// </summary>
-        /// <value>
-        /// The type of the heading.
-        /// </value>
-        HeadingType HeadingType { get; set; }
     }
 }
