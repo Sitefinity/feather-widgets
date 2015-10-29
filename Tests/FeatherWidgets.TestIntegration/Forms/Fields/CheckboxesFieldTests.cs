04cce77746b9f774bc45a0bd0f067ac9b224df40
ElenaGaneva <Elena.Ganeva@telerik.com>
2015-10-29 14:53:09 +0200

    Added checkboxes integration test

---
+++ Tests/FeatherWidgets.TestIntegration/Forms/Fields/CheckboxesFieldTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FeatherWidgets.TestUtilities.CommonOperations;
+using MbUnit.Framework;
+using Telerik.Sitefinity.Frontend.Forms.Mvc.Controllers;
+using Telerik.Sitefinity.Frontend.Forms.Mvc.StringResources;
+using Telerik.Sitefinity.Frontend.Mvc.Infrastructure.Controllers;
+using Telerik.Sitefinity.Frontend.TestUtilities;
+using Telerik.Sitefinity.Frontend.TestUtilities.CommonOperations;
+using Telerik.Sitefinity.Localization;
+using Telerik.Sitefinity.Model;
+using Telerik.Sitefinity.Modules.Forms;
+using Telerik.Sitefinity.Modules.Pages;
+using Telerik.Sitefinity.Mvc.Proxy;
+using Telerik.Sitefinity.TestIntegration.SDK.DevelopersGuide.SitefinityEssentials.Modules.Forms;
+
+namespace FeatherWidgets.TestIntegration.Forms.Fields
+{
+    /// <summary>
+    /// This class contains ensures checkboxes field functionalities work correctly.
+    /// </summary>
+    [TestFixture]
+    public class CheckboxesFieldTests
+    {
+        /// <summary>
+        /// Ensures that when a checkboxes field is added to form the choices are presented in the page markup.
+        /// </summary>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Maintainability", "CA1506:AvoidExcessiveClassCoupling"), Test]
+        [Category(TestCategories.Forms)]
+        [Author(FeatherTeams.FeatherTeam)]
+        [Description("Ensures that when a checkboxes field is added to form the choices are presented in the page markup.")]
+        public void CheckboxesFieldTests_EditDefaultChoices_MarkupIsCorrect()
+        {
+            var choice1 = "Choice1";
+            var choice2 = "Choice2";
+            var choice3 = "Choice3";
+
+            var controller = new CheckboxesFieldController();
+            controller.Model.SerializedChoices = "[\"{0}\",\"{1}\",\"{2}\"]".Arrange(choice1, choice2, choice3);
+            controller.Model.HasOtherChoice = true;
+
+            var control = new MvcWidgetProxy();
+            control.Settings = new ControllerSettings(controller);
+            control.ControllerName = typeof(CheckboxesFieldController).FullName;
+
+            var formId = ServerOperationsFeather.Forms().CreateFormWithWidget(control);
+
+            var pageManager = PageManager.GetManager();
+
+            try
+            {
+                var template = pageManager.GetTemplates().FirstOrDefault(t => t.Name == "SemanticUI.default" && t.Title == "default");
+                Assert.IsNotNull(template, "Template was not found");
+
+                var pageId = FeatherServerOperations.Pages().CreatePageWithTemplate(template, "CheckboxesFieldDefaultValueTest", "checkboxes-field-value-test");
+                ServerOperationsFeather.Forms().AddFormControlToPage(pageId, formId);
+
+                var pageContent = FeatherServerOperations.Pages().GetPageContent(pageId);
+                Assert.IsTrue(pageContent.Contains(choice1), "Form did not render the first choice in the checkboxes field.");
+                Assert.IsTrue(pageContent.Contains(choice2), "Form did not render the second choice in the checkboxes field.");
+                Assert.IsTrue(pageContent.Contains(choice3), "Form did not render the third choice in the checkboxes field.");
+                Assert.IsTrue(pageContent.Contains(Res.Get<FieldResources>().Other), "Form did not render the 'Other' choice in the checkboxes field.");
+            }
+            finally
+            {
+                Telerik.Sitefinity.TestUtilities.CommonOperations.ServerOperations.Pages().DeleteAllPages();
+                FormsModuleCodeSnippets.DeleteForm(formId);
+            }
+        }
+    }
+}
