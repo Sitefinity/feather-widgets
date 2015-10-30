6a4eb7ac37ae149f92d7c00e8cb6fb1d5497e462
ElenaGaneva <Elena.Ganeva@telerik.com>
2015-10-30 15:09:18 +0200

    Added method for creating mvc form

--- Tests/FeatherWidgets.TestUtilities/CommonOperations/Forms/FormsOperations.cs
+++ Tests/FeatherWidgets.TestUtilities/CommonOperations/Forms/FormsOperations.cs
@@ -4,6 +4,7 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
@@ -16,6 +17,7 @@ using Telerik.Sitefinity.Modules.Forms;
 using Telerik.Sitefinity.Modules.Forms.Web.UI.Fields;
 using Telerik.Sitefinity.Modules.Pages;
 using Telerik.Sitefinity.Mvc.Proxy;
+using Telerik.Sitefinity.Mvc.TestUtilities.CommonOperations;
 using Telerik.Sitefinity.Services;
 using Telerik.Sitefinity.TestIntegration.Data.Content;
 using Telerik.Sitefinity.TestIntegration.SDK.DevelopersGuide.SitefinityEssentials.Modules.Forms;
@@ -28,6 +30,11 @@ namespace FeatherWidgets.TestUtilities.CommonOperations.Forms
     /// </summary>
     public class FormsOperations
     {
+        /// <summary>
+        /// Adds the form control to page.
+        /// </summary>
+        /// <param name="pageId">The page identifier.</param>
+        /// <param name="formId">The form identifier.</param>
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Globalization", "CA1303:Do not pass literals as localized parameters", MessageId = "Telerik.Sitefinity.TestIntegration.Data.Content.PageContentGenerator.AddControlToPage(System.Guid,System.Web.UI.Control,System.String,System.String,System.Action<Telerik.Sitefinity.Pages.Model.PageDraftControl>)"), System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA2204:Literals should be spelled correctly", MessageId = "TestForm")]
         public void AddFormControlToPage(Guid pageId, Guid formId)
         {
@@ -43,6 +50,11 @@ namespace FeatherWidgets.TestUtilities.CommonOperations.Forms
             PageContentGenerator.AddControlToPage(pageId, mvcProxy, "TestForm", "Contentplaceholder1");
         }
 
+        /// <summary>
+        /// Creates the form with widget.
+        /// </summary>
+        /// <param name="widget">The widget.</param>
+        /// <returns></returns>
         public Guid CreateFormWithWidget(Control widget)
         {
             var formId = Guid.NewGuid();
@@ -52,7 +64,7 @@ namespace FeatherWidgets.TestUtilities.CommonOperations.Forms
             var formControls = new List<Control>();
             formControls.Add(widget);
 
-            Telerik.Sitefinity.TestUtilities.CommonOperations.ServerOperations.Forms().CreateForm(formId, "form_" + formId.ToString("N"), formId.ToString("N"), formSuccessMessage, formControls, FormFramework.Mvc);
+            this.CreateForm(formId, "form_" + formId.ToString("N"), formId.ToString("N"), formSuccessMessage, formControls);
 
             SystemManager.ClearCurrentTransactions();
             SystemManager.RestartApplication(false);
@@ -61,6 +73,11 @@ namespace FeatherWidgets.TestUtilities.CommonOperations.Forms
             return formId;
         }
 
+        /// <summary>
+        /// Creates the form with widgets.
+        /// </summary>
+        /// <param name="widgets">The widgets.</param>
+        /// <returns></returns>
         public Guid CreateFormWithWidgets(IEnumerable<Control> widgets)
         {
             var formId = Guid.NewGuid();
@@ -73,7 +90,7 @@ namespace FeatherWidgets.TestUtilities.CommonOperations.Forms
                 formControls.Add(widget);
             }
 
-            Telerik.Sitefinity.TestUtilities.CommonOperations.ServerOperations.Forms().CreateForm(formId, "form_" + formId.ToString("N"), formId.ToString("N"), formSuccessMessage, formControls, FormFramework.Mvc);
+            this.CreateForm(formId, "form_" + formId.ToString("N"), formId.ToString("N"), formSuccessMessage, formControls);
 
             SystemManager.ClearCurrentTransactions();
             SystemManager.RestartApplication(false);
@@ -82,6 +99,11 @@ namespace FeatherWidgets.TestUtilities.CommonOperations.Forms
             return formId;
         }
 
+        /// <summary>
+        /// Adds the form widget.
+        /// </summary>
+        /// <param name="formId">The form identifier.</param>
+        /// <param name="widget">The widget.</param>
         public void AddFormWidget(Guid formId, Control widget)
         {
             var formManager = FormsManager.GetManager();
@@ -115,6 +137,14 @@ namespace FeatherWidgets.TestUtilities.CommonOperations.Forms
             return fieldName;
         }
 
+        /// <summary>
+        /// Submits the field.
+        /// </summary>
+        /// <param name="fieldName">Name of the field.</param>
+        /// <param name="submittedValue">The submitted value.</param>
+        /// <param name="pageManager">The page manager.</param>
+        /// <param name="pageId">The page identifier.</param>
+        /// <returns></returns>
         public ActionResult SubmitField(string fieldName, string submittedValue, PageManager pageManager, Guid pageId)
         {
             var pageNode = pageManager.GetPageNode(pageId);
@@ -135,6 +165,58 @@ namespace FeatherWidgets.TestUtilities.CommonOperations.Forms
             var result = formController.Index(formCollection);
 
             return result;
+        }
+
+        /// <summary>
+        /// Creates a new form in Content -> Forms
+        /// </summary>
+        /// <param name="formId">Form ID</param>
+        /// <param name="formName">Form name</param>
+        /// <param name="formTitle">Form title</param>
+        /// <param name="formSuccessMessage">Success message after the form is submitted</param>
+        /// <param name="formControls">Form widgets like text boxes and buttons</param>
+        public void CreateForm(Guid formId, string formName, string formTitle, string formSuccessMessage, List<Control> formControls)
+        {
+            FormsManager formManager = FormsManager.GetManager();
+            var form = formManager.GetForms().SingleOrDefault(f => f.Id == formId);
+            Guid siblingId = Guid.Empty;
+
+            if (form == null)
+            {
+                form = formManager.CreateForm(formName, formId);
+                form.Framework = FormFramework.Mvc;
+                form.Title = formTitle;
+                form.UrlName = Regex.Replace(form.Name.ToLower(), ArrangementConstants.UrlNameCharsToReplace, ArrangementConstants.UrlNameReplaceString);
+                form.SuccessMessage = formSuccessMessage;
+
+                var draft = formManager.EditForm(form.Id);
+                var master = formManager.Lifecycle.CheckOut(draft);
+
+                if (master != null)
+                {
+                    if (formControls != null && formControls.Any())
+                    {
+                        int controlsCounter = 0;
+                        foreach (var control in formControls)
+                        {
+                            controlsCounter++;
+                            control.ID = string.Format(formName + "_C" + controlsCounter.ToString().PadLeft(3, '0'));
+                            var formControl = formManager.CreateControl<FormDraftControl>(control, "Body");
+
+                            formControl.SiblingId = siblingId;
+                            formControl.Caption = control.GetType().Name;
+                            siblingId = formControl.Id;
+
+                            master.Controls.Add(formControl);
+                        }
+                    }
+
+                    master = formManager.Lifecycle.CheckIn(master);
+                    formManager.Lifecycle.Publish(master);
+
+                    formManager.SaveChanges(true);
+                }
+            }
         }
 
         private static Guid GetLastControlInPlaceHolder(FormDescription form, string placeHolder)
