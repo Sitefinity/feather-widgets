72f3504c8c28e457aea4274e0864f9e96872094e
Ferdi Nebiev <ferdi.nebiev@gmail.com>
2015-11-20 14:50:57 +0200

    Added integration test for NavigationFieldController.

--- Tests/FeatherWidgets.TestIntegration/Forms/Fields/NavigationFieldTests.cs
+++ Tests/FeatherWidgets.TestIntegration/Forms/Fields/NavigationFieldTests.cs
@@ -72,7 +72,7 @@ namespace FeatherWidgets.TestIntegration.Forms.Fields
         [Author(FeatherTeams.SitefinityTeam6)]
         [Multilingual(MultilingualExecutionMode.MultiAndMonoLingual)]
         [Description("Ensures that when a Navigation field widget is added to form with page break widget, page titles are presented in the page markup.")]
-        public void Navigation_FieldIsCorrectlyInitialized()
+        public void Navigation_FieldIsCorrectlyInitializedOnReload()
         {
             var controller = new PageBreakController();
             var control = new MvcWidgetProxy();
@@ -117,6 +117,79 @@ namespace FeatherWidgets.TestIntegration.Forms.Fields
                     });
                 
                 formsManager.SaveChanges();
+
+                var masterForm = formsManager.Lifecycle.CheckIn(tempForm, culture);
+                formsManager.Lifecycle.Publish(masterForm, culture);
+                formsManager.SaveChanges();
+
+                var template = pageManager.GetTemplates().FirstOrDefault(t => t.Name == "SemanticUI.default" && t.Title == "default");
+                Assert.IsNotNull(template, "Template was not found");
+
+                var pageId = FeatherServerOperations.Pages().CreatePageWithTemplate(template, "NavigationFieldInitializationTest", "section-header-field-value-test");
+                ServerOperationsFeather.Forms().AddFormControlToPage(pageId, formId);
+
+                var pageContent = FeatherServerOperations.Pages().GetPageContent(pageId);
+
+                var pageName1 = Res.Get<FieldResources>().PageName + "1";
+                var pageName2 = Res.Get<FieldResources>().PageName + "2";
+                Assert.IsTrue(pageContent.Contains(pageName1), "Form did not render navigation field pages");
+                Assert.IsTrue(pageContent.Contains(pageName2), "Form did not render navigation field pages");
+            }
+            finally
+            {
+                Telerik.Sitefinity.TestUtilities.CommonOperations.ServerOperations.Pages().DeleteAllPages();
+                FormsModuleCodeSnippets.DeleteForm(formId);
+            }
+        }
+
+        /// <summary>
+        /// Ensures that when a Navigation field widget is added to form with page break widget, page titles are presented in the page markup.
+        /// </summary>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Maintainability", "CA1506:AvoidExcessiveClassCoupling"), Test]
+        [Category(TestCategories.Forms)]
+        [Author(FeatherTeams.SitefinityTeam6)]
+        [Multilingual(MultilingualExecutionMode.MultiAndMonoLingual)]
+        [Description("Ensures that when a Navigation field widget is added to form with page break widget, page titles are presented in the page markup.")]
+        public void Navigation_FieldIsCorrectlyInitializedOnNewWidgetAdded()
+        {
+            var controller = new PageBreakController();
+            var control = new MvcWidgetProxy();
+            control.ControllerName = typeof(PageBreakController).FullName;
+            control.Settings = new ControllerSettings(controller);
+
+            var controls = new List<Control>() { control };
+            var formId = ServerOperationsFeather.Forms().CreateFormWithWidgets(controls);
+
+            var pageManager = PageManager.GetManager();
+            var formsManager = FormsManager.GetManager();
+            try
+            {
+                var culture = SystemManager.CurrentContext.AppSettings.DefaultFrontendLanguage;
+                var tempForm = formsManager.EditForm(formId, culture);
+                formsManager.SaveChanges();
+
+                var zoneEditorService = new ZoneEditorService();
+                var parameters = new Dictionary<string, string>();
+                parameters["ControllerName"] = typeof(NavigationFieldController).FullName;
+
+                zoneEditorService.UpdateControlState(
+                    new Telerik.Sitefinity.Web.UI.ZoneEditorWebServiceArgs()
+                    {
+                        ControlType = typeof(MvcControllerProxy).FullName,
+                        PageId = tempForm.Id,
+                        PlaceholderId = "Body",
+                        Parameters = parameters,
+                        MediaType = DesignMediaType.Form,
+                        Attributes = new Dictionary<string, string>(),
+                        CommandName = "new",
+                        Url = "/Sitefinity/Forms/" + tempForm.Name
+                    });
+
+                var tempFormId = tempForm.Id;
+                
+                SystemManager.ClearCurrentTransactions();
+                formsManager = FormsManager.GetManager();
+                tempForm = formsManager.GetDraft(tempFormId);
 
                 var masterForm = formsManager.Lifecycle.CheckIn(tempForm, culture);
                 formsManager.Lifecycle.Publish(masterForm, culture);
