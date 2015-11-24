2c20968e78281e5bd1ab76a6cbf2e57aacb94a58
dzhenko <dzhenko@gmail.com>
2015-11-24 10:34:10 +0200

    Updated utilities dll

--- Tests/FeatherWidgets.TestIntegration/Common/ModuleUnloadTests.cs
+++ Tests/FeatherWidgets.TestIntegration/Common/ModuleUnloadTests.cs
@@ -1,13 +1,9 @@
-﻿using System.IO;
-using System.Net;
-using System.Threading;
-using System.Web;
+﻿using System;
 using FeatherWidgets.TestUtilities.CommonOperations;
 using MbUnit.Framework;
 using Telerik.Sitefinity.Frontend.ContentBlock.Mvc.Controllers;
 using Telerik.Sitefinity.Frontend.TestUtilities;
 using Telerik.Sitefinity.Mvc.Proxy;
-using Telerik.Sitefinity.Services;
 using Telerik.Sitefinity.Web;
 
 namespace FeatherWidgets.TestIntegration.Common
@@ -25,14 +21,13 @@ namespace FeatherWidgets.TestIntegration.Common
         [Description("Checks whether after deactivating Feather the Sitefinity pages doesn't throw errors on frontend.")]
         public void DeactivatingFeather_WidgetOnPage_VerifyFrontend()
         {
-            var featherWasEnabled = this.IsFeatherEnabled();
+            var moduleOperations = new Telerik.Sitefinity.Frontend.TestUtilities.CommonOperations.FeatherModuleOperations();
             var pageOperations = new PagesOperations();
 
+            moduleOperations.EnsureFeatherEnabled();
+            
             try
             {
-                if (!featherWasEnabled)
-                    this.ActivateFeatherFromDeactivatedState();
-
                 var mvcProxy = new MvcControllerProxy();
                 mvcProxy.ControllerName = typeof(ContentBlockController).FullName;
                 var contentBlockController = new ContentBlockController();
@@ -48,7 +43,7 @@ namespace FeatherWidgets.TestIntegration.Common
                 Assert.IsTrue(responseContent.Contains(ModuleUnloadTests.CbContent), "Content was not found!");
                 Assert.IsTrue(responseContentInEdit.Contains(ModuleUnloadTests.CbContent), "Content was not found!");
 
-                this.DeactivateFeather();
+                moduleOperations.DeactivateFeather();
 
                 responseContent = PageInvoker.ExecuteWebRequest(url);
                 responseContentInEdit = PageInvoker.ExecuteWebRequest(url + "/Action/Edit");
@@ -60,58 +55,11 @@ namespace FeatherWidgets.TestIntegration.Common
             finally
             {
                 pageOperations.DeletePages();
-
-                if (featherWasEnabled)
-                    this.ActivateFeatherFromDeactivatedState();
+                moduleOperations.ActivateFeatherFromDeactivatedState();
             }
         }
 
-        private bool IsFeatherEnabled()
-        {
-            return SystemManager.GetModule("Feather") != null;
-        }
-
-        private void ActivateFeatherFromDeactivatedState()
-        {
-            var installOperationEndpoint = UrlPath.ResolveUrl(ModuleUnloadTests.FeatherActivateFromDeactivatedStateUrl, true);
-            this.MakePutRequest(installOperationEndpoint, JsonRequestPayload);
-
-            // Give time for events to fire
-            Thread.Sleep(5000);
-        }
-
-        private void DeactivateFeather()
-        {
-            if (!this.IsFeatherEnabled())
-                return;
-
-            var uninstallOperationEndpoint = UrlPath.ResolveUrl(ModuleUnloadTests.FeatherDeactivateUrl, true);
-            this.MakePutRequest(uninstallOperationEndpoint, JsonRequestPayload);
-
-            // Give time for events to fire
-            Thread.Sleep(5000);
-        }
-
-        private void MakePutRequest(string url, string payload)
-        {
-            var httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
-            httpWebRequest.CookieContainer = new CookieContainer();
-            httpWebRequest.Headers["Authorization"] = HttpContext.Current.Request.Headers["Authorization"];
-            httpWebRequest.ContentType = "text/json";
-            httpWebRequest.Method = "PUT";
-
-            using (var writer = new StreamWriter(httpWebRequest.GetRequestStream()))
-            {
-                writer.Write(payload);
-            }
-
-            httpWebRequest.GetResponse();
-        }
-
-        private const string JsonRequestPayload = "{\"ClientId\":\"Feather\",\"Description\":\"Modern, intuitive, convention based, mobile-first UI for Telerik Sitefinity\",\"ErrorMessage\":\"\",\"IsModuleLicensed\":true,\"IsSystemModule\":false,\"Key\":\"Feather\",\"ModuleId\":\"00000000-0000-0000-0000-000000000000\",\"ModuleType\":0,\"Name\":\"Feather\",\"ProviderName\":\"\",\"StartupType\":3,\"Status\":1,\"Title\":\"Feather\",\"Type\":\"Telerik.Sitefinity.Frontend.FrontendModule, Telerik.Sitefinity.Frontend\",\"Version\":{\"_Build\":390,\"_Major\":1,\"_Minor\":4,\"_Revision\":0}}";
         private const string CbContent = "Initial CB content";
-        private const string FeatherDeactivateUrl = "/Sitefinity/Services/ModulesService/modules?operation=3";
-        private const string FeatherActivateFromDeactivatedStateUrl = "/Sitefinity/Services/ModulesService/modules?operation=2";
         private string pageNamePrefix = "CBPage";
         private string pageTitlePrefix = "CB";
         private string urlNamePrefix = "content-block";
