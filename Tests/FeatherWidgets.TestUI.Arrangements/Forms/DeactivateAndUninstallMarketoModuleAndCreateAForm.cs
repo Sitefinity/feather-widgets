b0b4bd8a545ee9ee06002b26a7b9ad02ebc71d58
VenetaBogoeva <veneta.bogoeva@telerik.com>
2016-02-03 16:00:36 +0200

    Added new UI tests LanguageSelectorXssAttack and DeactivateAndUninstallMarketoModuleAndCreateAForm

---
+++ Tests/FeatherWidgets.TestUI.Arrangements/Forms/DeactivateAndUninstallMarketoModuleAndCreateAForm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FeatherWidgets.TestUtilities.CommonOperations;
+using FeatherWidgets.TestUtilities.CommonOperations.Forms;
+using Telerik.Sitefinity.Data;
+using Telerik.Sitefinity.Publishing;
+using Telerik.Sitefinity.TestArrangementService.Attributes;
+using Telerik.Sitefinity.TestUI.Arrangements.Framework;
+using Telerik.Sitefinity.TestUtilities.CommonOperations;
+using Telerik.Sitefinity.Web.Services;
+
+namespace FeatherWidgets.TestUI.Arrangements
+{
+    /// <summary>
+    /// DeactivateAndUninstallMarketoModuleAndCreateAForm arrangement class.
+    /// </summary>
+    public class DeactivateAndUninstallMarketoModuleAndCreateAForm : TestArrangementBase
+    {
+        [ServerArrangement]
+        public void DeactivateMarketoModule()
+        {
+            ServerOperations.StaticModules().DeactivateModule(ModuleName);
+        }
+
+        [ServerArrangement]
+        public void ActivateMarketoModule()
+        {
+            ServerOperations.StaticModules().ActivateModule(ModuleName);
+        }
+
+        /// <summary>
+        /// Tears down.
+        /// </summary>
+        [ServerTearDown]
+        public void TearDown()
+        {
+            ServerOperations.StaticModules().ActivateModule(ModuleName);
+        }
+
+        private const string ModuleName = "MarketoConnector";
+    }
+}
