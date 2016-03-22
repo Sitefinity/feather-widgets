09839f6fbeb98bdfcc22ebab24d907193c8eecef
Slaveya Shtereva <slaveya.shtereva@telerik.com>
2016-03-22 14:48:08 +0200

    Add new category for test purpuse

--- Tests/FeatherWidgets.TestUI.TestCases/FeatherTestCategories.cs
+++ Tests/FeatherWidgets.TestUI.TestCases/FeatherTestCategories.cs
@@ -36,6 +36,11 @@ namespace FeatherWidgets.TestUI.TestCases
         public const string ContentBlock3 = "ContentBlock3";
 
         /// <summary>
+        /// Content block category
+        /// </summary>
+        public const string ContentBlock4 = "ContentBlock4";
+
+        /// <summary>
         /// Navigation category
         /// </summary>
         public const string Navigation = "Navigation";
