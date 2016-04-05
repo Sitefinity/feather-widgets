2668422ad0e677653a504918cf6c226cffd65305
Boyko Karadzhov <bokovskii@gmail.com>
2016-04-05 11:55:51 +0300

    fix controller dependency

--- Telerik.Sitefinity.Frontend.Events/Mvc/Controllers/EventsController.cs
+++ Telerik.Sitefinity.Frontend.Events/Mvc/Controllers/EventsController.cs
@@ -1,11 +1,10 @@
 ﻿using System.Web.Mvc;
-using Telerik.Sitefinity.Modules.Events;
 using Telerik.Sitefinity.Modules.Pages.Configuration;
 using Telerik.Sitefinity.Mvc;
 
 namespace Telerik.Sitefinity.Frontend.Events.Mvc.Controllers
 {
-    [ControllerToolboxItem(Name = "Events_MVC", Title = "Events", SectionName = ToolboxesConfig.ContentToolboxSectionName, ModuleName = EventsModule.ModuleName, CssClass = EventsController.WidgetIconCssClass)]
+    [ControllerToolboxItem(Name = "Events_MVC", Title = "Events", SectionName = ToolboxesConfig.ContentToolboxSectionName, ModuleName = "Events", CssClass = EventsController.WidgetIconCssClass)]
     public class EventsController : Controller
     {
         public ActionResult Index()
