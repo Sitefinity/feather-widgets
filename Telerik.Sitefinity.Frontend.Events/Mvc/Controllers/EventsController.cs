437374cc99eea70a6a746d339b4556914ed5e51d
Boyko Karadzhov <bokovskii@gmail.com>
2016-04-05 11:37:12 +0300

    initial checkin events widget

---
+++ Telerik.Sitefinity.Frontend.Events/Mvc/Controllers/EventsController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+using Telerik.Sitefinity.Modules.Events;
+using Telerik.Sitefinity.Modules.Pages.Configuration;
+using Telerik.Sitefinity.Mvc;
+
+namespace Telerik.Sitefinity.Frontend.Events.Mvc.Controllers
+{
+    [ControllerToolboxItem(Name = "Events_MVC", Title = "Events", SectionName = ToolboxesConfig.ContentToolboxSectionName, ModuleName = EventsModule.ModuleName, CssClass = EventsController.WidgetIconCssClass)]
+    public class EventsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return this.Content("This widget is under construction!");
+        }
+
+        private const string WidgetIconCssClass = "sfEventsViewIcn sfMvcIcn";
+    }
+}
