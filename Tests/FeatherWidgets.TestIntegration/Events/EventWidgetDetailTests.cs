95248d054e0fdfff4dce4216a73f1cf8b71485fd
ElenaGaneva <Elena.Ganeva@telerik.com>
2016-04-11 14:40:40 +0300

    Added file for integration test for detail functionality of events

---
+++ Tests/FeatherWidgets.TestIntegration/Events/EventWidgetDetailTests.cs
@@ -0,0 +1,61 @@
+﻿using MbUnit.Framework;
+using Telerik.Sitefinity.Frontend.TestUtilities;
+
+namespace FeatherWidgets.TestIntegration.Events
+{
+    /// <summary>
+    /// This is class contains test for event widget details functionality.
+    /// </summary>
+    [TestFixture]
+    [Description("This is class contains test for event widget details functionality.")]
+    public class EventWidgetDetailTests
+    {
+        /// <summary>
+        /// Event widget - test whether single item view is displayed in the same page.
+        /// </summary>
+        [Test]
+        [Category(TestCategories.Events)]
+        [Author(FeatherTeams.FeatherTeam)]
+        [Description("Verifies that open single item in the same page functionality resolves the correct page.")]
+        public void EventWidget_VerifyOpenSingleItemInSamePage()
+        {
+            
+        }
+
+        /// <summary>
+        /// Events widget - verify open single item in custom page functionality.
+        /// </summary>
+        [Test]
+        [Category(TestCategories.Events)]
+        [Author(FeatherTeams.FeatherTeam)]
+        [Description("Verifies that open single item in the existing page functionality resolves the correct page.")]
+        public void EventWidget_VerifyOpenSingleItemInCustomPage()
+        {
+            
+        }
+
+        /// <summary>
+        /// Verifies that selected templates is used in Details mode.
+        /// </summary>
+        [Test]
+        [Category(TestCategories.Events)]
+        [Author(FeatherTeams.FeatherTeam)]
+        [Description("Verifies that selected templates is used in Details mode.")]
+        public void EventWidget_SelectDetailTemplate()
+        {
+            
+        }
+
+        /// <summary>
+        /// Verifies that links for exporting events are available when export is enabled.
+        /// </summary>
+        [Test]
+        [Category(TestCategories.Events)]
+        [Author(FeatherTeams.FeatherTeam)]
+        [Description("Verifies that links for exporting events are available when export is enabled.")]
+        public void EventWidget_AllowCalendarExport()
+        {
+
+        }
+    }
+}
