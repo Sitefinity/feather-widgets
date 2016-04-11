702b937900ed5e7b1ed3bf8383891a60decaec38
ElenaGaneva <Elena.Ganeva@telerik.com>
2016-04-11 14:04:52 +0300

    Added empty integration tests for events widget

---
+++ Tests/FeatherWidgets.TestIntegration/Events/EventWidgetFilteringTests.cs
@@ -0,0 +1,66 @@
+﻿using MbUnit.Framework;
+using Telerik.Sitefinity.Frontend.TestUtilities;
+
+namespace FeatherWidgets.TestIntegration.Events
+{
+    /// <summary>
+    /// This is a test fixture with tests for filtering and narrow selection options of Event widget.
+    /// </summary>
+    [TestFixture]
+    public class EventWidgetFilteringTests
+    {
+        [Test]
+        [Category(TestCategories.Events)]
+        [Author(FeatherTeams.FeatherTeam)]
+        [Description("Verifies that events widget is filtering events by tags.")]
+        public void EventWidget_AllEvents_FilterByTags()
+        {
+            
+        }
+
+        [Test]
+        [Category(TestCategories.Events)]
+        [Author(FeatherTeams.FeatherTeam)]
+        [Description("Verifies that event widget is filtering events by categories.")]
+        public void EventWidget_AllEvents_FilterByCategories()
+        {
+            
+        }
+
+        [Test]
+        [Category(TestCategories.Events)]
+        [Author(FeatherTeams.FeatherTeam)]
+        [Description("Verifies that event widget is filtering events by calendar.")]
+        public void EventWidget_AllEvents_FilterByCalendar()
+        {
+
+        }
+
+        [Test]
+        [Category(TestCategories.Events)]
+        [Author(FeatherTeams.FeatherTeam)]
+        [Description("Verifies that event widget is displaying only past events.")]
+        public void EventWidget_FilterByDate_DisplayPastOnly()
+        {
+
+        }
+
+        [Test]
+        [Category(TestCategories.Events)]
+        [Author(FeatherTeams.FeatherTeam)]
+        [Description("Verifies that event widget is displaying only current events.")]
+        public void EventWidget_FilterByDate_DisplayCurrentOnly()
+        {
+
+        }
+
+        [Test]
+        [Category(TestCategories.Events)]
+        [Author(FeatherTeams.FeatherTeam)]
+        [Description("Verifies that event widget is displaying only upcoming events.")]
+        public void EventWidget_FilterByDate_DisplayUpcomingOnly()
+        {
+
+        }
+    }
+}
--- Tests/FeatherWidgets.TestIntegration/Events/EventWidgetTests.cs
+++ Tests/FeatherWidgets.TestIntegration/Events/EventWidgetTests.cs
@@ -1,15 +1,37 @@
-﻿using System;
+﻿using MbUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Telerik.Sitefinity.Frontend.TestUtilities;
 using Telerik.Sitefinity.Modules.Events;
 using Telerik.Sitefinity.TestUtilities.CommonOperations;
 
 namespace FeatherWidgets.TestIntegration.Events
 {
+    /// <summary>
+    /// This class contains test related to basic functionality of Event Widget.
+    /// </summary>
+    [TestFixture]
     public class EventWidgetTests
     {
+        [Test]
+        [Category(TestCategories.Events)]
+        [Author(FeatherTeams.FeatherTeam)]
+        [Description("Add Event widget to a page and display selected events only.")]
+        public void EventWidget_DisplaySelectedEventsOnly()
+        {
+        }
+
+        [Test]
+        [Category(TestCategories.Events)]
+        [Author(FeatherTeams.FeatherTeam)]
+        [Description("Add Event widget to a page and create events(past, current, upcoming, all day, repeat) with published and draft posts in order to verify that ")]
+        public void EventWidget_DisplayAllPublishedEvents()
+        {
+        }
+
         public void EventWidget_DisplaysCurrentEvents()
         {
             // ServerOperations.Events().CreateAllDayEvent("")
