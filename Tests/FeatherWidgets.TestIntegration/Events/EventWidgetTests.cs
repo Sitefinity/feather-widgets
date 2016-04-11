69d051b568ed43b982c66605fb236b6753ad3ea2
Boyko Karadzhov <bokovskii@gmail.com>
2016-04-11 11:47:13 +0300

    incomplete tests setup for event widget

---
+++ Tests/FeatherWidgets.TestIntegration/Events/EventWidgetTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telerik.Sitefinity.Modules.Events;
+using Telerik.Sitefinity.TestUtilities.CommonOperations;
+
+namespace FeatherWidgets.TestIntegration.Events
+{
+    public class EventWidgetTests
+    {
+        public void EventWidget_DisplaysCurrentEvents()
+        {
+            // ServerOperations.Events().CreateAllDayEvent("")
+        }
+    }
+}
