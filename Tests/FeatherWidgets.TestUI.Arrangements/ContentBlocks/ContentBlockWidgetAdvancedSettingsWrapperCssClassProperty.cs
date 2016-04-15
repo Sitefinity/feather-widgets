f27dbacad1795b1d6d48f92cec3d5c9a78e4e3e1
assenova <stoyanka.assenova@telerik.com>
2016-04-15 18:47:05 +0300

    Added ui test for bugID 144077 and bugID 176774

---
+++ Tests/FeatherWidgets.TestUI.Arrangements/ContentBlocks/ContentBlockWidgetAdvancedSettingsWrapperCssClassProperty.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using FeatherWidgets.TestUtilities.CommonOperations;
+using Telerik.Sitefinity.Frontend.TestUtilities.CommonOperations;
+using Telerik.Sitefinity.TestArrangementService.Attributes;
+using Telerik.Sitefinity.TestUI.Arrangements.Framework;
+using Telerik.Sitefinity.TestUI.Arrangements.Framework.Server;
+using Telerik.Sitefinity.TestUtilities.CommonOperations;
+
+namespace FeatherWidgets.TestUI.Arrangements
+{
+    /// <summary>
+    /// Arrangement methods for ContentBlockWidgetAdvancedSettingsWrapperCssClassProperty
+    /// </summary>
+    public class ContentBlockWidgetAdvancedSettingsWrapperCssClassProperty : TestArrangementBase
+    {
+        /// <summary>
+        /// Server side set up.
+        /// </summary>
+        [ServerSetUp]
+        public void SetUp()
+        {
+            AuthenticationHelper.AuthenticateUser(AdminUserName, AdminPass, true);
+
+            var pageId = ServerOperations.Pages().CreatePage(PageTitle);
+            ServerOperationsFeather.Pages().AddContentBlockWidgetToPage(pageId, ContentBlockText);
+
+            Guid templateId = Telerik.Sitefinity.TestUtilities.CommonOperations.ServerOperations.Templates().GetTemplateIdByTitle(PageTemplateName);
+            Guid pageId1 = Telerik.Sitefinity.TestUtilities.CommonOperations.ServerOperations.Pages().CreatePage(PageTitle1, templateId);
+            pageId1 = ServerOperations.Pages().GetPageNodeId(pageId1);
+            ServerOperationsFeather.Pages().AddContentBlockWidgetToPage(pageId1, ContentBlockText1, PlaceHolderId);
+        }
+
+        /// <summary>
+        /// Tears down.
+        /// </summary>
+        [ServerTearDown]
+        public void TearDown()
+        {
+            ServerOperations.Pages().DeleteAllPages();
+        }
+
+        private const string AdminUserName = "admin";
+        private const string AdminPass = "admin@2";
+        private const string PageTitle = "TestPageHybrid";
+        private const string ContentBlockText = "TestContent";
+        private const string PageTitle1 = "FeatherBootstrapPage";
+        private const string ContentBlockText1 = "TestContent1";
+        private const string PageTemplateName = "Bootstrap.default";
+        private const string PlaceHolderId = "Contentplaceholder1";
+    }
+}
