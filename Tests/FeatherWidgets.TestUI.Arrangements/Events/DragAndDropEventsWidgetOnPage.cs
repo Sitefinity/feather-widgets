156664593d5c2a0a6ac016e1e36f32c1c6bd88ca
dzhenko <dzhenko@gmail.com>
2016-04-22 13:49:15 +0300

    UI Tests progress

---
+++ Tests/FeatherWidgets.TestUI.Arrangements/Events/DragAndDropEventsWidgetOnPage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Telerik.Sitefinity.TestArrangementService.Attributes;
+using Telerik.Sitefinity.TestUI.Arrangements.Framework.Server;
+using Telerik.Sitefinity.TestUtilities.CommonOperations;
+
+namespace FeatherWidgets.TestUI.Arrangements.Events
+{
+    /// <summary>
+    /// DragAndDropEventsWidgetOnPage arrangement class.
+    /// </summary>
+    public class DragAndDropEventsWidgetOnPage : TestArrangementBase
+    {
+        /// <summary>
+        /// Server side set up.
+        /// </summary>
+        [ServerSetUp]
+        public void SetUp()
+        {
+            string templateName = ServerArrangementContext.GetCurrent().Values["templateName"];
+
+            Guid templateId = ServerOperations.Templates().GetTemplateIdByTitle(templateName);
+            ServerOperations.Pages().CreatePage(PageName, templateId);
+            ServerOperations.News().CreatePublishedNewsItem(EventsTitle);
+        }
+
+        /// <summary>
+        /// Tears down.
+        /// </summary>
+        [ServerTearDown]
+        public void TearDown()
+        {
+            ServerOperations.Pages().DeleteAllPages();
+            ServerOperations.News().DeleteAllNews();
+        }
+
+        private const string PageName = "Events";
+        private const string EventsContent = "Events content";
+        private const string EventsTitle = "EventsTitle";
+    }
+}
---
+++ Tests/FeatherWidgets.TestUI.TestCases/Events/DragAndDropEventsWidgetOnPage.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Feather.Widgets.TestUI.Framework;
+
+namespace FeatherWidgets.TestUI.TestCases.Events
+{
+    /// <summary>
+    /// DragAndDropEventsWidgetOnPage test class.
+    /// </summary>
+    [TestClass]
+    public class DragAndDropEventsWidgetOnPage_ : FeatherTestCase
+    {
+        /// <summary>
+        /// UI test DragAndDropNewsWidgetOnPage
+        /// </summary>
+        [TestMethod,
+        Owner(FeatherTeams.SitefinityTeam7),
+        TestCategory(FeatherTestCategories.PagesAndContent),
+        TestCategory(FeatherTestCategories.News),
+        TestCategory(FeatherTestCategories.Bootstrap)]
+        public void DragAndDropNewsWidgetOnPage()
+        {
+            this.pageTemplateName = "Bootstrap.default";
+
+            BAT.Macros().User().EnsureAdminLoggedIn();
+            BAT.Arrange(this.ArrangementClass).AddParameter("templateName", this.pageTemplateName).ExecuteSetUp();
+
+            BAT.Macros().NavigateTo().Pages(this.Culture);
+            BAT.Wrappers().Backend().Pages().PagesWrapper().OpenPageZoneEditor(PageName);
+            BATFeather.Wrappers().Backend().Pages().PageZoneEditorWrapper().AddWidgetToPlaceHolderPureMvcMode(WidgetName);
+            BAT.Wrappers().Backend().Pages().PageZoneEditorWrapper().CheckWidgetContent(WidgetName, EventsTitle);
+            BAT.Wrappers().Backend().Pages().PageZoneEditorWrapper().PublishPage();
+            this.VerifyNewsOnTheFrontend();
+        }
+
+        /// <summary>
+        /// UI test DragAndDropNewsWidgetOnPageBasedOnFoundationTemplate
+        /// </summary>
+        [TestMethod,
+        Owner(FeatherTeams.FeatherTeam),
+        TestCategory(FeatherTestCategories.PagesAndContent),
+        TestCategory(FeatherTestCategories.News),
+        TestCategory(FeatherTestCategories.Foundation)]
+        public void DragAndDropNewsWidgetOnPageBasedOnFoundationTemplate()
+        {
+            this.pageTemplateName = "Foundation.default";
+
+            BAT.Macros().User().EnsureAdminLoggedIn();
+            BAT.Arrange(this.ArrangementClass).AddParameter("templateName", this.pageTemplateName).ExecuteSetUp();
+
+            BAT.Macros().NavigateTo().Pages(this.Culture);
+            BAT.Wrappers().Backend().Pages().PagesWrapper().OpenPageZoneEditor(PageName);
+            BATFeather.Wrappers().Backend().Pages().PageZoneEditorWrapper().AddWidgetToPlaceHolderPureMvcMode(WidgetName);
+            BAT.Wrappers().Backend().Pages().PageZoneEditorWrapper().CheckWidgetContent(WidgetName, EventsTitle);
+            BAT.Wrappers().Backend().Pages().PageZoneEditorWrapper().PublishPage();
+            this.VerifyNewsOnTheFrontend();
+        }
+
+        /// <summary>
+        /// UI test DragAndDropNewsWidgetOnPageBasedOnSemanticUITemplate
+        /// </summary>
+        [TestMethod,
+        Owner(FeatherTeams.FeatherTeam),
+        TestCategory(FeatherTestCategories.PagesAndContent),
+        TestCategory(FeatherTestCategories.News),
+        TestCategory(FeatherTestCategories.SemanticUI)]
+        public void DragAndDropNewsWidgetOnPageBasedOnSemanticUITemplate()
+        {
+            this.pageTemplateName = "SemanticUI.default";
+
+            BAT.Macros().User().EnsureAdminLoggedIn();
+            BAT.Arrange(this.ArrangementClass).AddParameter("templateName", this.pageTemplateName).ExecuteSetUp();
+
+            BAT.Macros().NavigateTo().Pages(this.Culture);
+            BAT.Wrappers().Backend().Pages().PagesWrapper().OpenPageZoneEditor(PageName);
+            BATFeather.Wrappers().Backend().Pages().PageZoneEditorWrapper().AddWidgetToPlaceHolderPureMvcMode(WidgetName);
+            BAT.Wrappers().Backend().Pages().PageZoneEditorWrapper().CheckWidgetContent(WidgetName, EventsTitle);
+            BAT.Wrappers().Backend().Pages().PageZoneEditorWrapper().PublishPage();
+            this.VerifyNewsOnTheFrontend();
+        }
+
+        /// <summary>
+        /// Verify news widget on the frontend
+        /// </summary>
+        public void VerifyNewsOnTheFrontend()
+        {
+            BAT.Macros().NavigateTo().CustomPage("~/" + PageName.ToLower(), true, this.Culture);
+            Assert.IsTrue(BATFeather.Wrappers().Frontend().News().NewsWrapper().IsNewsTitlesPresentOnThePageFrontend(this.newsTitles));
+        }
+
+        /// <summary>
+        /// Performs clean up and clears all data created by the test.
+        /// </summary>
+        protected override void ServerCleanup()
+        {
+            BAT.Arrange(this.ArrangementClass).ExecuteTearDown();
+        }
+
+        private string ArrangementClass
+        {
+            get { return ArrangementClassName; }
+        }
+
+        private const string PageName = "Events";
+        private const string WidgetName = "Events";
+        private const string EventsTitle = "EventsTitle";
+        private readonly string[] newsTitles = new string[] { EventsTitle };
+        private string pageTemplateName;
+        private const string ArrangementClassName = "DragAndDropEventsWidgetOnPage";
+    }
+}
