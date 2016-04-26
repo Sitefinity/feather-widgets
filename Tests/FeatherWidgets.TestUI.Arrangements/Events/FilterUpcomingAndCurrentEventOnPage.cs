3437bca3b0e0e092aa4c00fc238db677a00bdc0e
ElenaGaneva <Elena.Ganeva@telerik.com>
2016-04-26 17:01:15 +0300

    Added UI test for upcoming and current events

---
+++ Tests/FeatherWidgets.TestUI.Arrangements/Events/FilterUpcomingAndCurrentEventOnPage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using FeatherWidgets.TestUI.Arrangements.Events;
+using FeatherWidgets.TestUtilities.CommonOperations;
+using Telerik.Sitefinity.TestArrangementService.Attributes;
+using Telerik.Sitefinity.TestUtilities.CommonOperations;
+
+namespace FeatherWidgets.TestUI.Arrangements
+{
+    /// <summary>
+    /// FilterUpcomingAndCurrentEventOnPage arrangement class.
+    /// </summary>
+    public class FilterUpcomingAndCurrentEventOnPage : TestArrangementBase
+    {
+        /// <summary>
+        /// Server side set up.
+        /// </summary>
+        [ServerSetUp]
+        public void SetUp()
+        {
+            Guid pageId = ServerOperations.Pages().CreatePage(PageName);
+            EventsTestsCommon.CreateEvents();
+            ServerOperationsFeather.Pages().AddEventsWidgetToPage(pageId);
+        }
+
+        /// <summary>
+        /// Tears down.
+        /// </summary>
+        [ServerTearDown]
+        public void TearDown()
+        {
+            ServerOperations.Pages().DeletePage(PageName);
+            ServerOperations.Events().DeleteAllEvents();
+        }
+
+        private const string PageName = "EventsPage";
+    }
+}
