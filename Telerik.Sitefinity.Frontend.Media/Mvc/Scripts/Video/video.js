14e8a16a25f52409745b7ecd71c4cc5cad13497b
adimanova <addidimanova@gmail.com>
2016-10-07 12:57:11 +0300

    Add DEC tracking for feather MediaPlayer widget as in web forms counterpart #1968
    Digital Experience Cloud connector tracks when a video is played through the web forms widget, but does not for feather registration widget.
    
    Reviewer: @todorm85

---
+++ Telerik.Sitefinity.Frontend.Media/Mvc/Scripts/Video/video.js
@@ -0,0 +1,27 @@
+﻿(function ($) {
+    $(function () {
+        $('[data-sf-role=playVideo]').on('play', function () {
+            var video = $(this);
+            var videoSrc = video.attr('src');
+            sendSentence(videoSrc);
+        });
+
+        function sendSentence(itemSrc) {
+            if (window.DataIntelligenceSubmitScript) {
+                DataIntelligenceSubmitScript._client.sentenceClient.writeSentence({
+                    predicate: "Play video",
+                    object: itemSrc,
+                    objectMetadata: [{
+                        'K': 'PageTitle',
+                        'V': document.title
+                    },
+                    {
+                        'K': 'PageUrl',
+                        'V': location.href
+                    }
+                    ]
+                });
+            }
+        }
+    });
+}(jQuery));
