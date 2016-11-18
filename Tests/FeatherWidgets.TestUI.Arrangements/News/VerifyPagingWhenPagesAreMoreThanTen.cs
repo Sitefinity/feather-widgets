300b0773555a8f59b17ef194a2986e1912bf5e47
Kandzhikova <Galya.Kandzhikova@telerik.com>
2016-11-18 09:11:48 +0200

    Added new test VerifyPagingWhenPagesAreMoreThanTen

---
+++ Tests/FeatherWidgets.TestUI.Arrangements/News/VerifyPagingWhenPagesAreMoreThanTen.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using FeatherWidgets.TestUtilities.CommonOperations;
+using Telerik.Sitefinity.TestArrangementService.Attributes;
+using Telerik.Sitefinity.TestUtilities.CommonOperations;
+
+namespace FeatherWidgets.TestUI.Arrangements
+{
+    /// <summary>
+    /// VerifyPagingWhenPagesAreMoreThanTen arrangement class. 
+    /// </summary>
+    public class VerifyPagingWhenPagesAreMoreThanTen : TestArrangementBase
+    {
+        /// <summary>
+        /// Server side set up.
+        /// </summary>
+        [ServerSetUp]
+        public void SetUp()
+        {
+            Guid templateId = ServerOperations.Templates().GetTemplateIdByTitle(PageTemplateName);
+            Guid pageId = ServerOperations.Pages().CreatePage(PageName, templateId);
+
+            for (int i = 1; i < 12; i++)
+            {
+                ServerOperationsFeather.NewsOperations().CreatePublishedNewsItem(NewsTitle + i, NewsContent, NewsAuthor, NewsSource, null, null, null);
+            }
+
+            Guid pageNodeId = ServerOperations.Pages().GetPageNodeId(pageId);
+            ServerOperationsFeather.Pages().AddNewsWidgetToPage(pageNodeId, "Contentplaceholder1");
+        }
+
+        /// <summary>
+        /// Tears down.
+        /// </summary>
+        [ServerTearDown]
+        public void TearDown()
+        {
+            ServerOperations.Pages().DeleteAllPages();
+            ServerOperations.News().DeleteAllNews();
+        }
+
+        private const string PageTemplateName = "Bootstrap.default";
+        private const string PageName = "NewsPage";
+        private const string NewsContent = "News content";
+        private const string NewsTitle = "NewsTitle";
+        private const string NewsAuthor = "AuthorName";
+        private const string NewsSource = "SourceName";
+    }
+}
