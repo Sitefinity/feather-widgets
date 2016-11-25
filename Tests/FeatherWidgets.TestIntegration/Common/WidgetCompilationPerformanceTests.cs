ed1b0e45ccd02f0b45ab383353c70170a31626fd
ElenaGaneva <Elena.Ganeva@telerik.com>
2016-11-25 17:24:39 +0200

    Added test for profiling executions on page template hierarchy

--- Tests/FeatherWidgets.TestIntegration/Common/WidgetCompilationPerformanceTests.cs
+++ Tests/FeatherWidgets.TestIntegration/Common/WidgetCompilationPerformanceTests.cs
@@ -1,15 +1,15 @@
 ﻿using System;
 using System.IO;
-using System.Reflection;
-using System.Web.Compilation;
 using FeatherWidgets.TestUtilities.CommonOperations;
 using MbUnit.Framework;
+using Telerik.Sitefinity.Frontend.Blogs.Mvc.Controllers;
 using Telerik.Sitefinity.Frontend.ContentBlock.Mvc.Controllers;
+using Telerik.Sitefinity.Frontend.News.Mvc.Controllers;
 using Telerik.Sitefinity.Frontend.TestUtilities;
 using Telerik.Sitefinity.Frontend.TestUtilities.CommonOperations;
 using Telerik.Sitefinity.Modules.Pages;
+using Telerik.Sitefinity.Mvc.TestUtilities.Data;
 using Telerik.Sitefinity.Pages.Model;
-using Telerik.Sitefinity.Restriction;
 using Telerik.Sitefinity.TestUtilities.CommonOperations;
 using Telerik.Sitefinity.TestUtilities.Modules.Diagnostics;
 using Telerik.Sitefinity.Web;
@@ -26,6 +26,8 @@ namespace FeatherWidgets.TestIntegration.Common
     public class WidgetCompilationPerformanceTests : ProfilingTestBase
     {
         #region Tests
+
+        #region Widget on page
 
         /// <summary>
         /// Verifies that when widget template is edited and page is requested the execution and the compilation of the MVC widgets is logged correctly.
@@ -172,6 +174,79 @@ namespace FeatherWidgets.TestIntegration.Common
 
         #endregion
 
+        #region Widget on template
+
+        /// <summary>
+        /// Verifies that executions are logged for every MVC widget placed on the page or on its parent templates.
+        /// </summary>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Globalization", "CA1303:Do not pass literals as localized parameters", MessageId = "FeatherWidgets.TestUtilities.CommonOperations.Templates.TemplateOperations.AddControlToTemplate(System.Guid,System.Web.UI.Control,System.String,System.String)"), System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Globalization", "CA1303:Do not pass literals as localized parameters", MessageId = "Telerik.Sitefinity.TestUtilities.CommonOperations.WidgetOperations.AddContentBlockToPage(System.Guid,System.String,System.String,System.String)"), Test]
+        [Author(FeatherTeams.FeatherTeam)]
+        [Description("Verifies that executions are logged for every MVC widget placed on the page or on its parent templates.")]
+        public void TemplateHierarchy_RequestPage_ShouldLogExecutions()
+        {
+            Guid templateId1 = default(Guid);
+            Guid templateId2 = default(Guid);
+
+            PageNode pageNode = null;
+            try
+            {
+                this.EnableProfiler("HttpRequestsProfiler");
+                this.EnableProfiler("WidgetExecutionsProfiler");
+                this.EnableProfiler("RazorViewCompilationsProfiler");
+
+                pageNode = this.CreatePageTemplateHierarchy(ref templateId1, ref templateId2);
+                
+                var fullPageUrl = RouteHelper.GetAbsoluteUrl(pageNode.GetUrl());
+
+                // Request page
+                this.ClearData();
+                this.ExecuteAuthenticatedRequest(fullPageUrl);
+                this.FlushData();
+
+                this.AssertWidgetExecutionCount(3);
+            }
+            finally
+            {
+                this.DeletePages(pageNode);
+                Telerik.Sitefinity.TestUtilities.CommonOperations.ServerOperations.Templates().DeletePageTemplate(templateId2);
+                Telerik.Sitefinity.TestUtilities.CommonOperations.ServerOperations.Templates().DeletePageTemplate(templateId1);
+            }
+        }
+
+        private PageNode CreatePageTemplateHierarchy(ref Guid templateId1, ref Guid templateId2)
+        {
+            var widgetName = "ContentBlock";
+            var widgetName1 = "News widget";
+            var widgetName2 = "Blog post widget";
+            var template1Name = "Template1";
+            var template2Name = "Template2";
+
+            templateId1 = ServerOperationsFeather.TemplateOperations().DuplicatePageTemplate(PageTemplateName, template1Name);
+            var mvcWidget1 = new Telerik.Sitefinity.Mvc.Proxy.MvcControllerProxy();
+            mvcWidget1.ControllerName = typeof(NewsController).FullName;
+            mvcWidget1.Settings = new Telerik.Sitefinity.Mvc.Proxy.ControllerSettings(new NewsController());
+            ServerOperationsFeather.TemplateOperations().AddControlToTemplate(templateId1, mvcWidget1, BootstrapPlaceholder, widgetName1);
+
+            templateId2 = ServerOperationsFeather.TemplateOperations().CreatePageTemplate(template2Name, templateId1);
+            var mvcWidget2 = new Telerik.Sitefinity.Mvc.Proxy.MvcControllerProxy();
+            mvcWidget2.ControllerName = typeof(BlogPostController).FullName;
+            mvcWidget2.Settings = new Telerik.Sitefinity.Mvc.Proxy.ControllerSettings(new BlogPostController());
+            mvcWidget2.ID = widgetName2.Replace(" ", string.Empty);
+            ServerOperationsFeather.TemplateOperations().AddControlToTemplate(templateId2, mvcWidget2, BootstrapPlaceholder, widgetName2);
+
+            var pageId = ServerOperations.Pages().CreatePage("TestPage1", templateId2);
+            var pageNodeId = ServerOperations.Pages().GetPageNodeId(pageId);
+            var pageManager = Telerik.Sitefinity.Modules.Pages.PageManager.GetManager();
+            var pageNode = pageManager.GetPageNode(pageNodeId);
+            ServerOperationsFeather.Pages().AddContentBlockWidgetToPage(pageNodeId, widgetName, BootstrapPlaceholder);
+
+            return pageNode;
+        }
+
+        #endregion
+
+        #endregion
+
         #region Fields and Constants
 
         private struct ResourcePackages
@@ -181,6 +256,7 @@ namespace FeatherWidgets.TestIntegration.Common
 
         private const string WidgetViewPathFormat = "";
         private const string PageTemplateName = "Bootstrap.default";
+        private const string BootstrapPlaceholder = "Contentplaceholder1";
 
         #endregion
     }
