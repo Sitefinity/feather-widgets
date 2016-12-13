9e00a3a8bf30180def193fa05d3a11bc2f63f00a
vassildinev <vassildinev@gmail.com>
2016-12-13 15:07:36 +0200

    Added more integration tests for testing the functionality of the Diagnostics module regarding the widget executions and widget view compilations profiling (layouts, dynamic widgets, custom widgets, built-in widgets)

---
+++ Tests/FeatherWidgets.TestIntegration/Mvc/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+
+namespace FeatherWidgets.TestIntegration.Mvc.Controllers
+{
+    /// <summary>
+    /// This <see cref="Controller"/> represents a custom widget for testing purposes.
+    /// </summary>
+    /// <seealso cref="System.Web.Mvc.Controller" />
+    public class TestController : Controller
+    {
+        /// <summary>
+        /// The default action of the custom widget controller.
+        /// </summary>
+        /// <returns>The default view of the custom widget controller.</returns>
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
