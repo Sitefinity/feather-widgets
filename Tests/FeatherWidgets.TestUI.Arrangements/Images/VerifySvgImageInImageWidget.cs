271b1922d37bade979b52eb4631da0e72e23dbfa
gvalova <Gergana.Valova@telerik.com>
2017-03-15 13:24:38 +0200

    Add arrangement for SVG test for Image Widget

---
+++ Tests/FeatherWidgets.TestUI.Arrangements/Images/VerifySvgImageInImageWidget.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using FeatherWidgets.TestUtilities.CommonOperations;
+using MbUnit.Framework;
+using Telerik.Sitefinity.Taxonomies;
+using Telerik.Sitefinity.Taxonomies.Model;
+using Telerik.Sitefinity.TestArrangementService.Attributes;
+using Telerik.Sitefinity.TestUI.Arrangements.Framework;
+using Telerik.Sitefinity.TestUI.Arrangements.Framework.Server;
+using Telerik.Sitefinity.TestUtilities.CommonOperations;
+
+namespace FeatherWidgets.TestUI.Arrangements
+{
+    /// <summary>
+    /// Upload Svg Image, create MVC page with Image widget
+    /// </summary>
+    public class VerifySvgImageInImageWidget : TestArrangementBase
+    {
+        /// <summary>
+        /// Server side set up.
+        /// </summary>
+        [ServerSetUp]
+        public void SetUp()
+        {
+            ServerOperations.Images().EnableSvgFileExtension(enableSvgSupport);
+            Guid pageId = ServerOperations.Pages().CreatePage(PageName);
+            ServerOperationsFeather.Pages().AddImageWidgetToPage(pageId);
+
+            var parentId = ServerOperations.Images().CreateLibrary(ImageLibraryTitle);
+            ServerOperations.Images().CreateFolder(ChildLibraryTitle, parentId);
+        }
+
+        /// <summary>
+        /// Tears down.
+        /// </summary>
+        [ServerTearDown]
+        public void TearDown()
+        {
+            ServerOperations.Pages().DeleteAllPages();
+            ServerOperations.Images().DeleteAllLibrariesExceptDefaultOne();
+
+            ////Disable Svg Support
+            ServerOperations.Images().EnableSvgFileExtension();
+        }
+
+        /// Gets the current libraries provider Url name.
+        /// </summary>
+        [ServerArrangement]
+        public void GetCurrentProviderUrlName()
+        {
+            string urlName = ServerOperations.Media().GetCurrentProviderUrlName;
+
+            ServerArrangementContext.GetCurrent().Values.Add("CurrentProviderUrlName", urlName);
+        }
+
+        private static bool enableSvgSupport = true;
+        private const string PageName = "PageWithSvgImage";
+        private const string ImageLibraryTitle = "TestImageLibrary";
+        private const string ChildLibraryTitle = "ChildImageLibrary";
+    }
+}
